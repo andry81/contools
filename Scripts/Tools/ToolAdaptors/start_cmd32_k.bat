527b61c2cd4bd973fe6c685183fb0d06070aa2a3
andry81 <andry@inbox.ru>
2017-03-10 14:42:18 +0000

    ToolAdaptors refactor
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@26 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tools/ToolAdaptors/start_cmd32_k.bat
+++ Scripts/Tools/ToolAdaptors/start_cmd32_k.bat
@@ -4,23 +4,27 @@ rem Author:   Andrey Dibrov (andry at inbox dot ru)
 
 rem Description:
 rem   Script tryes to call x32 cmd interpreter under any process mode otherwise
-rem   it calls a cmd interpreter under the same process mode
+rem   it calls a cmd interpreter under current process mode
 rem   (x32 under x32 or x64 under x64).
 
-if "%~1" == "" exit /b -1
+rem   If current process mode is not the x32 process mode, then the cmd.exe
+rem   calls with the /K flag.
+rem   Waits only console process.
 
 if "%PROCESSOR_ARCHITECTURE%" == "AMD64" goto X64
 rem in case of wrong PROCESSOR_ARCHITECTURE value
 if "%PROCESSOR_ARCHITEW6432%" == "" goto X64
 
-call %%*
+if "%~1" == "" exit /b -1
+
+start "" /B /WAIT %*
 rem Exit with current error level.
 goto :EOF
 
 :X64
 rem just in case
-if exist "%SystemRoot%\Syswow64" (
-  "%SystemRoot%\Syswow64\cmd.exe" /c %*
+if exist "%SystemRoot%\Syswow64\" (
+  "%SystemRoot%\Syswow64\cmd.exe" /K %*
 ) else (
-  "%SystemRoot%\System32\cmd.exe" /c %*
+  "%SystemRoot%\System32\cmd.exe" /K %*
 )
