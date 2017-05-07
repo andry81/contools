0558768241b3c81c5ee3e03cb7e5a52e9862f563
andry81 <andry@inbox.ru>
2017-05-07 20:38:34 +0000

    * workaround: suppressed "FINDSTR: Line NNN is too long" message
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@56 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tools/head.bat
+++ Scripts/Tools/head.bat
@@ -1,5 +1,18 @@
 @echo off
 
+rem Author:  Andrey Dibrov (andry at inbox dot ru)
+rem
+rem Description:
+rem   Script prints N first lines from the input stream. If N is not set then all.
+rem
+rem Features:
+rem   * Prints all characters including control characters like !%^&|> and so.
+rem   * Does not consume empty lines.
+rem
+rem Issues:
+rem   * findstr truncates lines longer than 8180 characters ("FINDSTR: Line NNN is too long" message)
+rem   * Not so fast, prints ~2000 lines about 8 seconds on 3.2GHz AMD processor
+
 setlocal DISABLEDELAYEDEXPANSION
 
 set "NUM=%~1"
@@ -7,7 +20,7 @@ if "%NUM%" == "" set NUM=0
 
 set LINE_INDEX=0
 
-for /F "usebackq delims=" %%i in (`findstr /B /N /R /C:".*"`) do (
+for /F "usebackq delims=" %%i in (`findstr /B /N /R /C:".*" 2^>nul`) do (
   set LINE_STR=%%i
   call :IF_OR_PRINT %%NUM%% NEQ 0 if %%LINE_INDEX%% GEQ %%NUM%% && exit /b 0
   set /A LINE_INDEX+=1
