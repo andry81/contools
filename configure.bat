c949762867abb1148a139330a134679e48ed7547
andry81 <andry@inbox.ru>
2017-05-19 12:29:33 +0000

    * fixed: configure.bat
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@80 699b1267-f50e-4533-8e1b-323e5631ef88

--- configure.bat
+++ configure.bat
@@ -5,7 +5,7 @@ setlocal
 set "CONFIGURE_ROOT=%~dp0"
 set "CONFIGURE_ROOT=%CONFIGURE_ROOT:~0,-1%"
 
-if exist "%CONFIGURE_ROOT%/Scripts/__init__.bat" exit /b 1
+if exist "%CONFIGURE_ROOT%/Scripts/Tools/scm/svn/__init__.bat" exit /b 1
 
 rem generate __init__.bat in "Tools/scm/svn"
 (
