816c6de576dbf8d9d9c92a76d0dc6a4d60eb5061
andry81 <andry@inbox.ru>
2017-06-03 17:13:44 +0000

    * fixed: tests run
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@97 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tests/batscripts/__init__.bat
+++ Scripts/Tests/batscripts/__init__.bat
@@ -11,25 +11,8 @@ set "TESTS_ROOT=%~dp0"
 set "TESTS_ROOT=%TESTS_ROOT:\=/%"
 if "%TESTS_ROOT:~-1%" == "/" set "TESTS_ROOT=%TESTS_ROOT:~0,-1%"
 
-if "%CONTOOLS_ROOT%" == "" set "CONTOOLS_ROOT=%~dp0..\..\tools"
-set "CONTOOLS_ROOT=%CONTOOLS_ROOT:\=/%"
-if "%CONTOOLS_ROOT:~-1%" == "/" set "CONTOOLS_ROOT=%CONTOOLS_ROOT:~0,-1%"
-
-if "%GNUWIN32_ROOT%" == "" set "GNUWIN32_ROOT=%CONTOOLS_ROOT%/gnuwin32"
-set "GNUWIN32_ROOT=%GNUWIN32_ROOT:\=/%"
-if "%GNUWIN32_ROOT:~-1%" == "/" set "GNUWIN32_ROOT=%GNUWIN32_ROOT:~0,-1%"
-
-if "%SVNCMD_TOOLS_ROOT%" == "" set "SVNCMD_TOOLS_ROOT=%CONTOOLS_ROOT%/scm/svn"
-set "SVNCMD_TOOLS_ROOT=%SVNCMD_TOOLS_ROOT:\=/%"
-if "%SVNCMD_TOOLS_ROOT:~-1%" == "/" set "SVNCMD_TOOLS_ROOT=%SVNCMD_TOOLS_ROOT:~0,-1%"
-
-if "%XML_TOOLS_ROOT%" == "" set "XML_TOOLS_ROOT=%CONTOOLS_ROOT%/xml"
-set "XML_TOOLS_ROOT=%XML_TOOLS_ROOT:\=/%"
-if "%XML_TOOLS_ROOT:~-1%" == "/" set "XML_TOOLS_ROOT=%XML_TOOLS_ROOT:~0,-1%"
-
-if "%VARS_ROOT%" == "" set "VARS_ROOT=%CONTOOLS_ROOT%/vars"
-set "VARS_ROOT=%VARS_ROOT:\=/%"
-if "%VARS_ROOT:~-1%" == "/" set "VARS_ROOT=%VARS_ROOT:~0,-1%"
+rem initialize Tools "module"
+call "%%TESTS_ROOT%%/../../Tools/__init__.bat" || goto :EOF
 
 set "TEST_SRC_BASE_DIR=%~dp0"
 set "TEST_SRC_BASE_DIR=%TEST_SRC_BASE_DIR:~0,-1%"
