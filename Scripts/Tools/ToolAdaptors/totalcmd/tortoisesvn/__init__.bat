18d5f3db0cc69b61513b4f76de3f225c63e3036a
andry81 <andry@inbox.ru>
2017-06-08 11:31:08 +0000

    * missed change
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@106 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tools/ToolAdaptors/totalcmd/tortoisesvn/__init__.bat
+++ Scripts/Tools/ToolAdaptors/totalcmd/tortoisesvn/__init__.bat
@@ -1,3 +1,6 @@
 @echo off
 
+if exist "%~dp0..\configure.user.bat" ( call "%~dp0..\configure.user.bat" || goto :EOF )
+if not "%CONTOOLS_ROOT_COPY%" == "" set "CONTOOLS_ROOT=%CONTOOLS_ROOT_COPY%"
+
 call "%%~dp0..\Tools\__init__.bat" || goto :EOF
