9ce5ca0d67a2594cc2bc693056acde3b80ac48d4
andry81 <andry@inbox.ru>
2017-08-12 19:22:53 +0000

    * batscript tests minor update
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@163 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tests/batscripts/.test_strlen/impl.bat
+++ Scripts/Tests/batscripts/.test_strlen/impl.bat
@@ -11,7 +11,7 @@ if %TESTLIB__OVERALL_TESTS% GTR %TESTLIB__CURRENT_TESTS% (
 
 if %STRING_LEN% EQU %ERRORLEVEL_RETURNED% (
   rem print string containing __STRING__ environment variable value which may hold batch control characters
-  "%CONTOOLS_ROOT%/printf.exe" "PASSED: %TEST_REPORT_PREFIX_STR%: (%ERRORLEVEL_RETURNED% == %STRING_LEN%) STRING=`${__STRING__}`"
+  "%CONTOOLS_ROOT%/printf.exe" "PASSED: %TEST_REPORT_PREFIX_STR%: STRING_LEN=%STRING_LEN% STRING=`${__STRING__}`"
   exit /b 0
 )
 
