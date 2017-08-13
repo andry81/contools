6790fcc76d7c768acd0621fea5a1e50ca51fa033
andry81 <andry@inbox.ru>
2017-08-13 14:35:13 +0000

    * configuration script unification for multiple projects
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@165 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tools/__init__.bat
+++ Scripts/Tools/__init__.bat
@@ -1,37 +1,25 @@
 @echo off
 
-if %__CONTOOLS_INIT__%0 NEQ 0 exit /b 0
-
-if not defined CONTOOLS_ROOT set "CONTOOLS_ROOT=%~dp0"
+if "%CONTOOLS_ROOT%" == "" set "CONTOOLS_ROOT=w:/Work/MyProjects/_contools/sf~contools/Scripts/Tools"
 set "CONTOOLS_ROOT=%CONTOOLS_ROOT:\=/%"
 if "%CONTOOLS_ROOT:~-1%" == "/" set "CONTOOLS_ROOT=%CONTOOLS_ROOT:~0,-1%"
 
-if not defined BUILD_TOOLS_ROOT set "BUILD_TOOLS_ROOT=%CONTOOLS_ROOT%/build"
-set "BUILD_TOOLS_ROOT=%BUILD_TOOLS_ROOT:\=/%"
-if "%BUILD_TOOLS_ROOT:~-1%" == "/" set "BUILD_TOOLS_ROOT=%BUILD_TOOLS_ROOT:~0,-1%"
-
-if not defined GNUWIN32_ROOT set "GNUWIN32_ROOT=%CONTOOLS_ROOT%/gnuwin32"
+if "%GNUWIN32_ROOT%" == "" set "GNUWIN32_ROOT=%CONTOOLS_ROOT%/gnuwin32"
 set "GNUWIN32_ROOT=%GNUWIN32_ROOT:\=/%"
 if "%GNUWIN32_ROOT:~-1%" == "/" set "GNUWIN32_ROOT=%GNUWIN32_ROOT:~0,-1%"
 
-if not defined SVNCMD_TOOLS_ROOT set "SVNCMD_TOOLS_ROOT=%CONTOOLS_ROOT%/scm/svn"
+if "%SVNCMD_TOOLS_ROOT%" == "" set "SVNCMD_TOOLS_ROOT=%CONTOOLS_ROOT%/scm/svn"
 set "SVNCMD_TOOLS_ROOT=%SVNCMD_TOOLS_ROOT:\=/%"
 if "%SVNCMD_TOOLS_ROOT:~-1%" == "/" set "SVNCMD_TOOLS_ROOT=%SVNCMD_TOOLS_ROOT:~0,-1%"
 
-if not defined SQLITE_TOOLS_ROOT set "SQLITE_TOOLS_ROOT=%CONTOOLS_ROOT%/sqlite"
+if "%SQLITE_TOOLS_ROOT%" == "" set "SQLITE_TOOLS_ROOT=%CONTOOLS_ROOT%/sqlite"
 set "SQLITE_TOOLS_ROOT=%SQLITE_TOOLS_ROOT:\=/%"
 if "%SQLITE_TOOLS_ROOT:~-1%" == "/" set "SQLITE_TOOLS_ROOT=%SQLITE_TOOLS_ROOT:~0,-1%"
 
-if not defined XML_TOOLS_ROOT set "XML_TOOLS_ROOT=%CONTOOLS_ROOT%/xml"
+if "%XML_TOOLS_ROOT%" == "" set "XML_TOOLS_ROOT=%CONTOOLS_ROOT%/xml"
 set "XML_TOOLS_ROOT=%XML_TOOLS_ROOT:\=/%"
 if "%XML_TOOLS_ROOT:~-1%" == "/" set "XML_TOOLS_ROOT=%XML_TOOLS_ROOT:~0,-1%"
 
-if not defined HASHDEEP_ROOT set "HASHDEEP_ROOT=%CONTOOLS_ROOT%/hash/hashdeep"
-set "HASHDEEP_ROOT=%HASHDEEP_ROOT:\=/%"
-if "%HASHDEEP_ROOT:~-1%" == "/" set "HASHDEEP_ROOT=%HASHDEEP_ROOT:~0,-1%"
-
-if not defined VARS_ROOT set "VARS_ROOT=%CONTOOLS_ROOT%/vars"
+if "%VARS_ROOT%" == "" set "VARS_ROOT=%CONTOOLS_ROOT%/vars"
 set "VARS_ROOT=%VARS_ROOT:\=/%"
 if "%VARS_ROOT:~-1%" == "/" set "VARS_ROOT=%VARS_ROOT:~0,-1%"
-
-set __CONTOOLS_INIT__=1
