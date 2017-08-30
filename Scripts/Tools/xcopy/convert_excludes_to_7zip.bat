ca5ea804c9af1929b7f413f78f38bba0812300bc
andry81 <andry@inbox.ru>
2017-08-30 19:36:59 +0000

    * missed changes
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@193 699b1267-f50e-4533-8e1b-323e5631ef88

---
+++ Scripts/Tools/xcopy/convert_excludes_to_7zip.bat
@@ -0,0 +1,61 @@
+@echo off
+
+rem Author:   Andrey Dibrov (andry at inbox dot ru)
+
+rem Description:
+rem   Script converts unform file/directory excludes list in format of
+rem     `exclude1|..|excludeN` ,where
+rem       `exclude` can be an exclude wildcard file name or @-prefixed path to
+rem       text file with list of file name wildcards.
+rem   into 7zip preformatted command line of excludes.
+
+rem Examples:
+rem   1. call convert_excludes_to_7zip.bat "@excludes_file_list.lst|*.pdb|*.ilk|*.map" "@excludes_dir_list.lst|dir1|dir2"
+
+rem Drop return value
+set "RETURN_VALUE="
+
+setlocal
+
+set "?~n0=%~n0"
+
+set "ARCHIVE_EXCLUDE_FILES_LIST=%~1"
+set "ARCHIVE_EXCLUDE_DIRS_LIST=%~2"
+
+if not defined ARCHIVE_EXCLUDE_FILES_LIST if not defined ARCHIVE_EXCLUDE_DIRS_LIST exit /b 1
+
+if defined ARCHIVE_EXCLUDE_FILES_LIST ( call :PROCESS_EXCLUDES_LIST "%%ARCHIVE_EXCLUDE_FILES_LIST%%" || goto :EOF )
+if defined ARCHIVE_EXCLUDE_DIRS_LIST ( call :PROCESS_EXCLUDES_LIST "%%ARCHIVE_EXCLUDE_DIRS_LIST%%" || goto :EOF )
+
+(
+  endlocal
+  set RETURN_VALUE=%RETURN_VALUE%
+)
+
+exit /b 0
+
+:PROCESS_EXCLUDES_LIST
+set "ARCHIVE_EXCLUDES_LIST=%~1"
+
+set INDEX=1
+
+:ARCHIVE_EXCLUDES_LIST_LOOP
+set "FILE="
+for /F "eol=	 tokens=%INDEX% delims=|" %%i in ("%ARCHIVE_EXCLUDES_LIST%") do set "FILE=%%i"
+if not defined FILE exit /b 0
+
+if "%FILE:~0,1%" == "@" (
+  set RETURN_VALUE=%RETURN_VALUE% -xr@"%FILE:~1%"
+) else (
+  call :PROCESS_WILDCARD
+)
+
+set /A INDEX+=1
+
+goto ARCHIVE_EXCLUDES_LIST_LOOP
+
+:PROCESS_WILDCARD
+for /F "eol=	 tokens=* delims=" %%i in ("%FILE%") do (
+  set RETURN_VALUE=%RETURN_VALUE% -xr!"%%i"
+)
+exit /b 0
---
+++ Scripts/Tools/xcopy/convert_excludes_to_robocopy.bat
@@ -0,0 +1,70 @@
+@echo off
+
+rem Author:   Andrey Dibrov (andry at inbox dot ru)
+
+rem Description:
+rem   Script converts unform file/directory excludes list in format of
+rem     `exclude1|..|excludeN` ,where
+rem       `exclude` can be an exclude wildcard file name or @-prefixed path to
+rem       text file with list of file name wildcards.
+rem   into robocopy preformatted command line of excludes.
+
+rem Examples:
+rem   1. call convert_excludes_to_robocopy.bat "@excludes_file_list.lst|*.pdb|*.ilk|*.map" "@excludes_dir_list.lst|dir1|dir2"
+
+rem Drop return value
+set "RETURN_VALUE="
+
+setlocal
+
+set "?~n0=%~n0"
+
+set "ROBOCOPY_EXCLUDE_FILES_LIST=%~1"
+set "ROBOCOPY_EXCLUDE_DIRS_LIST=%~2"
+
+if not defined ROBOCOPY_EXCLUDE_FILES_LIST if not defined ROBOCOPY_EXCLUDE_DIRS_LIST exit /b 1
+
+if defined ROBOCOPY_EXCLUDE_FILES_LIST ( call :PROCESS_EXCLUDES_LIST "%%ROBOCOPY_EXCLUDE_FILES_LIST%%" XF || goto :EOF )
+if defined ROBOCOPY_EXCLUDE_DIRS_LIST ( call :PROCESS_EXCLUDES_LIST "%%ROBOCOPY_EXCLUDE_DIRS_LIST%%" XD || goto :EOF )
+
+(
+  endlocal
+  set RETURN_VALUE=%RETURN_VALUE%
+)
+
+exit /b 0
+
+:PROCESS_EXCLUDES_LIST
+set "ROBOCOPY_EXCLUDES_LIST=%~1"
+set "ROBOCOPY_EXCLUDES_FLAG_PREFIX=%~2"
+
+set INDEX=1
+
+:ROBOCOPY_EXCLUDES_LIST_LOOP
+set "FILE="
+for /F "eol=	 tokens=%INDEX% delims=|" %%i in ("%ROBOCOPY_EXCLUDES_LIST%") do set "FILE=%%i"
+if not defined FILE exit /b 0
+
+if "%FILE:~0,1%" == "@" (
+  set "FILE=%FILE:~1%"
+  call :PROCESS_EXCLUDES_LIST_FILE
+) else (
+  call :PROCESS_WILDCARD
+)
+
+set /A INDEX+=1
+
+goto ROBOCOPY_EXCLUDES_LIST_LOOP
+
+:PROCESS_EXCLUDES_LIST_FILE
+for /F "usebackq eol=	 tokens=* delims=" %%i in ("%FILE%") do (
+  set "FILE=%%i"
+  call :PROCESS_WILDCARD || goto :EOF
+)
+exit /b 0
+
+:PROCESS_WILDCARD
+for /F "eol=	 tokens=* delims=" %%i in ("%FILE%") do (
+  set RETURN_VALUE=%RETURN_VALUE% %ROBOCOPY_EXCLUDES_FLAG_PREFIX% "%%i"
+)
+exit /b 0
