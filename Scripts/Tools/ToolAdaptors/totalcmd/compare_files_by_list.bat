e07fe3e77c84873a0eda70d74653d4cc4c319021
andry81 <andry@inbox.ru>
2017-11-02 10:56:21 +0000

    * new: implementation through the file list from a text file
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@197 699b1267-f50e-4533-8e1b-323e5631ef88

---
+++ Scripts/Tools/ToolAdaptors/totalcmd/compare_files_by_list.bat
@@ -0,0 +1,72 @@
+@echo off
+
+setlocal
+
+rem script flags
+set FLAG_WAIT_EXIT=0
+
+:FLAGS_LOOP
+
+rem flags always at first
+set "FLAG=%~1"
+
+if defined FLAG ^
+if not "%FLAG:~0,1%" == "-" set "FLAG="
+
+if defined FLAG (
+  if "%FLAG%" == "-wait" (
+    set FLAG_WAIT_EXIT=1
+    shift
+  )
+
+  rem read until no flags
+  goto FLAGS_LOOP
+)
+
+call "%%~dp0loadvars.bat" "%%~dp0profile.vars"
+
+set "PWD=%~1"
+shift
+
+set "FILES_LIST="
+set NUM_FILES=0
+
+if not defined PWD goto NOPWD
+( %PWD:~0,2% && cd "%PWD%" ) || exit /b 1
+
+:NOPWD
+
+rem read selected file paths from file
+for /F "usebackq eol=	 delims=" %%i in ("%~1") do (
+  set FILE_PATH=%%i
+  call :PROCESS_FILE_PATH || goto PROCESS_COMPARE
+)
+
+rem only 2 first files from the list are accepted
+exit /b 0
+
+:PROCESS_FILE_PATH
+if not defined FILE_PATH exit /b 0
+rem must be files, not sub directories
+if exist "%FILE_PATH%\" exit /b 0
+set FILES_LIST=%FILES_LIST% "%FILE_PATH%"
+
+set /A NUM_FILES+=1
+
+if %NUM_FILES% GEQ 2 exit /b 1
+
+exit /b 0
+
+:PROCESS_COMPARE
+
+if %FLAG_WAIT_EXIT% NEQ 0 (
+  call :CMD start /B /WAIT "" "%%COMPARE_TOOL%%" /wait %%FILES_LIST%%
+) else (
+  call :CMD start /B "" "%%COMPARE_TOOL%%" /nowait %%FILES_LIST%%
+)
+
+exit /b 0
+
+:CMD
+echo.^>%*
+(%*)
