46a15d3d533acaf76f9bc5e13366d1e5d9d86ff1
andry81 <andry@inbox.ru>
2018-12-12 10:51:09 +0000

    2018.12.12: * fixed: Scripts/Tools/touch.bat: not fully correct implementation
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@226 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tools/touch.bat
+++ Scripts/Tools/touch.bat
@@ -1,3 +1,11 @@
 @echo off
 
-type nul >> %*
+rem based on: https://superuser.com/questions/10426/windows-equivalent-of-the-linux-command-touch/764725#764725
+rem
+
+setlocal
+
+if not exist "%~1" type nul >> "%~1" & goto :EOF
+
+set _ATTRIBUTES=%~a1
+if "%~a1" == "%_ATTRIBUTES:r=%" (copy "%~1"+,, > nul) else attrib -r "%~1" & copy "%~1"+,, > nul & attrib +r "%~1"
