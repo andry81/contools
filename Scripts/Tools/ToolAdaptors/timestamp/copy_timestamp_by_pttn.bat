09b59d0c4053be828ff10b2c8fcfd15632dd2eb4
andry81 <andry@inbox.ru>
2018-12-15 13:50:34 +0000

    2018.12.15: * new: Utilities: touch utility w/o sources (taken from Steve Miller's `Win32 Console Toolbox`, see for details: https://stevemiller.net/apps/) * new: Scripts/Tools/ToolAdaptors: file timestamp copy script `copy_timestamp_by_pttn.bat` to copy timestamps from one file to another by file path pattern
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@230 699b1267-f50e-4533-8e1b-323e5631ef88

---
+++ Scripts/Tools/ToolAdaptors/timestamp/copy_timestamp_by_pttn.bat
@@ -0,0 +1,53 @@
+@echo off
+
+rem Description:
+rem   Copies file time stamps from one file to another by input path pattern.
+rem
+
+setlocal
+
+set "INPUT_PATH_PTTN=%~1"
+set "OUTPUT_DIR=%~2"
+set "OUTPUT_EXT=%~3"
+
+if not defined INPUT_PATH_PTTN (
+  echo.%~nx0: error: INPUT_PATH_PTTN input file path pattern is not set.
+  exit /b 1
+) >&2
+
+if not defined OUTPUT_DIR (
+  echo.%~nx0: error: OUTPUT_DIR output directory path is not set.
+  exit /b 2
+) >&2
+
+if not exist "%OUTPUT_DIR%\" (
+  echo.%~nx0: error: OUTPUT_DIR output directory path does not exist: "%OUTPUT_DIR%".
+  exit /b 3
+) >&2
+
+if not defined OUTPUT_EXT (
+  echo.%~nx0: error: OUTPUT_EXT output file extension is not set.
+  exit /b 4
+) >&2
+
+if "%OUTPUT_EXT:~0,1%" == "." (
+  echo.%~nx0: error: OUTPUT_EXT should not begins by dot: "%OUTPUT_EXT%".
+  exit /b 5
+) >&2
+
+set "INPUT_DIR_PATH=%~dp1"
+
+chcp 1251
+
+for /F "usebackq eol=| tokens=* delims=" %%i in (`dir /A:-D /B "%INPUT_PATH_PTTN%" 2^> nul`) do (
+  set "INPUT_FILE_NAME=%%i"
+  call :PROCESS_INPUT_FILE_PATH "%%INPUT_FILE_NAME%%" || exit /b
+)
+
+exit /b 0
+
+:PROCESS_INPUT_FILE_PATH
+set "INPUT_FILE_PATH=%INPUT_DIR_PATH%%INPUT_FILE_NAME%"
+set "OUTPUT_FILE_PATH=%OUTPUT_DIR%\%~n1.%OUTPUT_EXT%"
+
+touch.exe /c /m /a /r "%INPUT_FILE_PATH%" "%OUTPUT_FILE_PATH%"
