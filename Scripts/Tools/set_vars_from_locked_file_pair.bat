0dac873fb13db615d8e9c6d08fd363e91e5b6406
andry81 <andry@inbox.ru>
2019-02-22 04:49:21 +0000

    2019.02.22: * fixed: Scripts/Tools: `set_vars_from_locked_file_pair.bat` variables setup after the lock release instead of under the lock
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@253 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tools/set_vars_from_locked_file_pair.bat
+++ Scripts/Tools/set_vars_from_locked_file_pair.bat
@@ -16,9 +16,6 @@ set "PRINT_VARS_SET=%~4"
 
 set "FILE_LOCK_DIR=%~d1"
 
-rem drop last error
-type nul>nul
-
 rem the lock file directory must already exist
 if not exist "%FILE_LOCK_DIR%" (
   echo.%~nx0: error: FILE_LOCK_DIR does not exist: "%FILE_LOCK_DIR%"
@@ -32,57 +29,53 @@ if not exist "%FILE_VAR_NAMES_PATH%" (
 
 if not exist "%FILE_VAR_VALUES_PATH%" (
   echo.%~nx0: error: FILE_VAR_VALUES_PATH does not exist: "%FILE_VAR_VALUES_PATH%"
-  exit /b 2
+  exit /b 3
 ) >&2
 
-rem The `del` on the same line to avoid interference with the endlocal:
-rem expression with the lock file path variable substitution should be evaluated before the endlocal
-call :MAIN & del /F /Q /A:-D "%FILE_LOCK_PATH%"
-exit /b
+rem The endlocal works only in the same call context
+endlocal
 
-:MAIN
 rem exclusive acquire of the lock file
 :REPEAT_LOCK_LOOP
 
-rem lock via redirection to file
-set LOCK_FILE0_ACQUIRE=0
 (
   (
     rem if lock is acquired, then we are in...
+    call :MAIN "%%~2" "%%~3" "%%~4"
+    call set "LASTERROR=%%ERRORLEVEL%%"
 
-    rem Drop error level to 0 to avoid interference with the error level from the redirection command below.
-    type nul>nul
-  ) 9> "%FILE_LOCK_PATH%" && set LOCK_FILE0_ACQUIRE=1
+    rem exit with return code from the MAIN
+  ) 9> "%~1" && (del /F /Q /A:-D "%~1" & goto EXIT)
 ) 2>nul
-
-rem has exclusive lock been acquired?
-if %LOCK_FILE0_ACQUIRE% NEQ 0 goto LOCK_LOOP_END
 
 rem Busy wait: with external call significally reduces CPU consumption while in a waiting state
 pathping localhost -n -q 1 -p 20 >nul 2>&1
 goto REPEAT_LOCK_LOOP
 
-:LOCK_LOOP_END
+:EXIT
+exit /b %LASTERROR%
 
-if %PRINT_VARS_SET%0 NEQ 0 goto SET_WITH_PRINT
+:MAIN
+rem drop last error
+type nul>nul
+
+if %~30 NEQ 0 goto SET_WITH_PRINT
 
 rem trick with simultaneous iteration over 2 lists in the same time
 (
-  rem endlocal on the same line to drop the `FILE_VAR_NAMES_PATH` variable only after the line evaluation
-  endlocal & for /f "usebackq eol=# tokens=* delims=" %%i in ("%FILE_VAR_NAMES_PATH%") do (
+  for /f "usebackq eol=# tokens=* delims=" %%i in ("%~1") do (
     set /p "%%i="
   )
-) < "%FILE_VAR_VALUES_PATH%"
+) < "%~2"
 
 exit /b
 
 :SET_WITH_PRINT
 rem trick with simultaneous iteration over 2 lists in the same time
 (
-  rem endlocal on the same line to drop the `FILE_VAR_NAMES_PATH` variable only after the line evaluation
-  endlocal & for /f "usebackq eol=# tokens=* delims=" %%i in ("%FILE_VAR_NAMES_PATH%") do (
+  for /f "usebackq eol=# tokens=* delims=" %%i in ("%~1") do (
     set /p "%%i="
     rem to filter out wrong matches of a variable from the `set "%%i"`
     for /f "usebackq eol=# tokens=1,* delims==" %%j in (`set "%%i"`) do if "%%j" == "%%i" echo.%%i=%%k
   )
-) < "%FILE_VAR_VALUES_PATH%"
+) < "%~2"
