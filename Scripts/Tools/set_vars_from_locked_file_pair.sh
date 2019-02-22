d37a50102f00fc97be6d8aba1d48f143d51b28ed
andry81 <andry@inbox.ru>
2019-02-22 05:18:00 +0000

    2019.02.22: * new: Scripts/Tools: `set_vars_from_locked_file_pair.sh` script to set variables from 2 files after a file lock acquire (through `source` command)
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@254 699b1267-f50e-4533-8e1b-323e5631ef88

---
+++ Scripts/Tools/set_vars_from_locked_file_pair.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# Another variant of a configuration file variables read and set script.
+# The script must stay as simple as possible, so for this task it uses these parameters:
+# 1. path where to lock a lock file
+# 2. path where to read a file with variable names (each per line)
+# 3. path where to read a file with variable values (each per line, must be the same quantity of lines with the variable names file)
+
+# Script can be ONLY included by "source" command.
+if [[ -n "$BASH" && (-z "$BASH_LINENO" || ${BASH_LINENO[0]} -gt 0) ]]; then 
+
+function set_vars_from_locked_file_pair()
+{
+  # the lock file directory must already exist
+  if [[ ! -d "${1%[/\\]*}" ]]; then
+    echo "$0: error: lock file directory does not exist: \`${1%[/\\]*}\`" >&2
+    return 1
+  fi
+
+  if [[ ! -f "${2//\\//}" ]]; then
+    echo "$0: error: variable names file does not exist: \`$2\`" >&2
+    return 2
+  fi
+
+  if [[ ! -f "${3//\\//}" ]]; then
+    echo "$0: error: variable values file does not exist: \`$3\`" >&2
+    return 3
+  fi
+
+  function LocalMain()
+  {
+    # open file for direct reading by the `read` in the same shell process
+    exec 7<> "$2"
+    exec 8<> "$3"
+
+    # cleanup on return
+    trap "exec 8>&-; exec 7>&-; trap - RETURN" RETURN
+
+    local __VarName
+    local __VarValue
+
+    # exclusive acquire of the lock file
+    while :; do
+      # lock via redirection to file
+      {
+        # simultaneous iteration over 2 lists in the same time
+        while read -r -u 7 __VarName; do
+          read -r -u 8 __VarValue
+          # drop line returns
+          __VarName="${__VarName//[$'\r\n']}"
+          __VarValue="${__VarValue//[$'\r\n']}"
+          declare -gx $__VarName="$__VarValue"
+          (( $4 )) && echo "$__VarName=\`$__VarValue\`"
+        done
+
+        break
+
+        # return with code from the LocalRead
+      } 9> "$1" # has exclusive lock been acquired?
+
+      # busy wait
+      sleep 0.02
+    done 2> /dev/null
+  }
+
+  LocalMain "${1//\\//}" "${2//\\//}" "${3//\\//}" "${4:-0}"
+}
+
+fi
