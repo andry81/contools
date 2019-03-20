2b75b5229ba313c3665eaa6f6df532e7e68aa6be
andry81 <andry@inbox.ru>
2019-03-20 03:28:09 +0000

    2019.03.20: * fixed: Scripts/Tools/admin/Telemetry: more junk disabler scripts
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@261 699b1267-f50e-4533-8e1b-323e5631ef88

---
+++ Scripts/Tools/admin/Telemetry/disable_telemetry_junk_win7_4.bat
@@ -0,0 +1,47 @@
+@echo off
+
+setlocal
+
+rem related to this article:
+rem   `Telemetry and Data Collection are coming to Windows 7 and Windows 8 too`,
+rem   https://winaero.com/blog/telemetry-and-data-collection-are-coming-to-windows-7-and-windows-8-too/
+rem
+
+wusa /uninstall /kb:915597 /quiet /norestart
+wusa /uninstall /kb:958830 /quiet /norestart
+wusa /uninstall /kb:2505438 /quiet /norestart
+wusa /uninstall /kb:2670838 /quiet /norestart
+wusa /uninstall /kb:2976978 /quiet /norestart
+wusa /uninstall /kb:2977759 /quiet /norestart
+wusa /uninstall /kb:3046480 /quiet /norestart
+wusa /uninstall /kb:3050267 /quiet /norestart
+wusa /uninstall /kb:3058168 /quiet /norestart
+wusa /uninstall /kb:3064683 /quiet /norestart
+wusa /uninstall /kb:3065988 /quiet /norestart
+wusa /uninstall /kb:3072318 /quiet /norestart
+wusa /uninstall /kb:3081424 /quiet /norestart
+wusa /uninstall /kb:3083325 /quiet /norestart
+wusa /uninstall /kb:3083711 /quiet /norestart
+wusa /uninstall /kb:3102429 /quiet /norestart
+wusa /uninstall /kb:3112343 /quiet /norestart
+wusa /uninstall /kb:3115858 /quiet /norestart
+wusa /uninstall /kb:3118401 /quiet /norestart
+wusa /uninstall /kb:3121255 /quiet /norestart
+wusa /uninstall /kb:3122648 /quiet /norestart
+wusa /uninstall /kb:3122656 /quiet /norestart
+wusa /uninstall /kb:3123862 /quiet /norestart
+wusa /uninstall /kb:3124280 /quiet /norestart
+wusa /uninstall /kb:3126446 /quiet /norestart
+wusa /uninstall /kb:3126593 /quiet /norestart
+wusa /uninstall /kb:3127220 /quiet /norestart
+wusa /uninstall /kb:3127229 /quiet /norestart
+wusa /uninstall /kb:3134214 /quiet /norestart
+wusa /uninstall /kb:3134814 /quiet /norestart
+wusa /uninstall /kb:3150513 /quiet /norestart
+wusa /uninstall /kb:3161102 /quiet /norestart
+wusa /uninstall /kb:3163245 /quiet /norestart
+wusa /uninstall /kb:3168965 /quiet /norestart
+wusa /uninstall /kb:3170106 /quiet /norestart
+wusa /uninstall /kb:3170735 /quiet /norestart
+wusa /uninstall /kb:3170455 /quiet /norestart
+wusa /uninstall /kb:3172605 /quiet /norestart
