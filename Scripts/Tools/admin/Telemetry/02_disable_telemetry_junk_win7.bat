cbbb76a56233e7fd601798618972270f6ec8ccd3
andry81 <andry@inbox.ru>
2019-07-02 15:04:31 +0000

    2019.07.02: * changed: Scripts/Tools/admin/Telemetry: add interation with the user to ask for quiet uninstall (now is not by default, because Event Viewer can contain errors after unsuccessful uninstall and user might not to known that)
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@287 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tools/admin/Telemetry/02_disable_telemetry_junk_win7.bat
+++ Scripts/Tools/admin/Telemetry/02_disable_telemetry_junk_win7.bat
@@ -7,7 +7,11 @@ rem   `Telemetry and Data Collection are coming to Windows 7 and Windows 8 too`,
 rem   https://winaero.com/blog/telemetry-and-data-collection-are-coming-to-windows-7-and-windows-8-too/
 rem
 
-wusa.exe /uninstall /kb:3068708 /quiet /norestart
-wusa.exe /uninstall /kb:3022345 /quiet /norestart
-wusa.exe /uninstall /kb:3075249 /quiet /norestart
-wusa.exe /uninstall /kb:3080149 /quiet /norestart
+set "CMD_QUIET="
+set /P "IS_QUIET=Quiet uninstall? [y/N] "
+if "%IS_QUIET%" == "y" set "CMD_QUIET=/quiet"
+
+wusa.exe /uninstall /kb:3068708 %CMD_QUIET% /norestart
+wusa.exe /uninstall /kb:3022345 %CMD_QUIET% /norestart
+wusa.exe /uninstall /kb:3075249 %CMD_QUIET% /norestart
+wusa.exe /uninstall /kb:3080149 %CMD_QUIET% /norestart
--- Scripts/Tools/admin/Telemetry/04_disable_telemetry_junk_win7.bat
+++ Scripts/Tools/admin/Telemetry/04_disable_telemetry_junk_win7.bat
@@ -7,41 +7,45 @@ rem   `Telemetry and Data Collection are coming to Windows 7 and Windows 8 too`,
 rem   https://winaero.com/blog/telemetry-and-data-collection-are-coming-to-windows-7-and-windows-8-too/
 rem
 
-wusa /uninstall /kb:915597 /quiet /norestart
-wusa /uninstall /kb:958830 /quiet /norestart
-wusa /uninstall /kb:2505438 /quiet /norestart
-wusa /uninstall /kb:2670838 /quiet /norestart
-wusa /uninstall /kb:2976978 /quiet /norestart
-wusa /uninstall /kb:2977759 /quiet /norestart
-wusa /uninstall /kb:3046480 /quiet /norestart
-wusa /uninstall /kb:3050267 /quiet /norestart
-wusa /uninstall /kb:3058168 /quiet /norestart
-wusa /uninstall /kb:3064683 /quiet /norestart
-wusa /uninstall /kb:3065988 /quiet /norestart
-wusa /uninstall /kb:3072318 /quiet /norestart
-wusa /uninstall /kb:3081424 /quiet /norestart
-wusa /uninstall /kb:3083325 /quiet /norestart
-wusa /uninstall /kb:3083711 /quiet /norestart
-wusa /uninstall /kb:3102429 /quiet /norestart
-wusa /uninstall /kb:3112343 /quiet /norestart
-wusa /uninstall /kb:3115858 /quiet /norestart
-wusa /uninstall /kb:3118401 /quiet /norestart
-wusa /uninstall /kb:3121255 /quiet /norestart
-wusa /uninstall /kb:3122648 /quiet /norestart
-wusa /uninstall /kb:3122656 /quiet /norestart
-wusa /uninstall /kb:3123862 /quiet /norestart
-wusa /uninstall /kb:3124280 /quiet /norestart
-wusa /uninstall /kb:3126446 /quiet /norestart
-wusa /uninstall /kb:3126593 /quiet /norestart
-wusa /uninstall /kb:3127220 /quiet /norestart
-wusa /uninstall /kb:3127229 /quiet /norestart
-wusa /uninstall /kb:3134214 /quiet /norestart
-wusa /uninstall /kb:3134814 /quiet /norestart
-wusa /uninstall /kb:3150513 /quiet /norestart
-wusa /uninstall /kb:3161102 /quiet /norestart
-wusa /uninstall /kb:3163245 /quiet /norestart
-wusa /uninstall /kb:3168965 /quiet /norestart
-wusa /uninstall /kb:3170106 /quiet /norestart
-wusa /uninstall /kb:3170735 /quiet /norestart
-wusa /uninstall /kb:3170455 /quiet /norestart
-wusa /uninstall /kb:3172605 /quiet /norestart
+set "CMD_QUIET="
+set /P "IS_QUIET=Quiet uninstall? [y/N] "
+if "%IS_QUIET%" == "y" set "CMD_QUIET=/quiet"
+
+wusa /uninstall /kb:915597 %CMD_QUIET% /norestart
+wusa /uninstall /kb:958830 %CMD_QUIET% /norestart
+wusa /uninstall /kb:2505438 %CMD_QUIET% /norestart
+wusa /uninstall /kb:2670838 %CMD_QUIET% /norestart
+wusa /uninstall /kb:2976978 %CMD_QUIET% /norestart
+wusa /uninstall /kb:2977759 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3046480 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3050267 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3058168 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3064683 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3065988 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3072318 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3081424 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3083325 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3083711 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3102429 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3112343 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3115858 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3118401 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3121255 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3122648 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3122656 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3123862 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3124280 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3126446 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3126593 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3127220 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3127229 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3134214 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3134814 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3150513 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3161102 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3163245 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3168965 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3170106 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3170735 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3170455 %CMD_QUIET% /norestart
+wusa /uninstall /kb:3172605 %CMD_QUIET% /norestart
