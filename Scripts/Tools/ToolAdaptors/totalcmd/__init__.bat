0da98e31b5bc70494307d79feffb24e009c18825
andry81 <andry@inbox.ru>
2019-11-04 14:14:24 +0000

    2019.11.04: * fixed: Scripts/Tools/ToolAdaptors/totalcmd: `create_empty_files_by_dir_list.bat` and `create_empty_files_by_path_list.bat` is missed to install * changed: Scripts/Tools/ToolAdaptors/totalcmd: `configure.bat` renamed into `_install.bat` * changed: Scripts/Tools/ToolAdaptors/totalcmd: `_install.bat` simplification, now the first parameter is always required to define the directory there to install * changed: Scripts/Tools/ToolAdaptors/totalcmd: install: scripts, button bars and configuration files moved into `tacklebar` subdirectory to avoid mix or overwrite standalone files in the same root directory * changed: Scripts/Tools/ToolAdaptors/totalcmd: readme update
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@347 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tools/ToolAdaptors/totalcmd/__init__.bat
+++ Scripts/Tools/ToolAdaptors/totalcmd/__init__.bat
@@ -1,13 +1,19 @@
 @echo off
 
-if %__TOTALCMD_INIT__%0 NEQ 0 exit /b 0
+if %__TACKLEBAR_INIT__%0 NEQ 0 exit /b 0
 
-set "TOTALCMD_ROOT=%~dp0"
-set "TOTALCMD_ROOT=%TOTALCMD_ROOT:\=/%"
-if "%TOTALCMD_ROOT:~-1%" == "/" set "TOTALCMD_ROOT=%TOTALCMD_ROOT:~0,-1%"
+if not defined COMMANDER_SCRIPTS_ROOT (
+  echo.%~nx0: error: COMMANDER_SCRIPTS_ROOT environment variable is not defined
+  exit /b 1
+) >&2
 
-call "%%TOTALCMD_ROOT%%/loadvars.bat" "%%TOTALCMD_ROOT%%/profile.vars" || exit /b
+if not exist "%COMMANDER_SCRIPTS_ROOT%\" (
+  echo.%~nx0: error: COMMANDER_SCRIPTS_ROOT directory does not exist: "%COMMANDER_SCRIPTS_ROOT%"
+  exit /b 2
+) >&2
+
+call "%%COMMANDER_SCRIPTS_ROOT%%/tacklebar/loadvars.bat" "%%COMMANDER_SCRIPTS_ROOT%%/tacklebar/profile.vars" || exit /b
 
 call "%%~dp0Tools\__init__.bat" || exit /b
 
-set __TOTALCMD_INIT__=1
+set __TACKLEBAR_INIT__=1
