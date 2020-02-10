2dfe51f48fa363390f11b61a08bc8dba2475b209
andry81 <andry@inbox.ru>
2020-02-10 15:08:01 +0000

    2020.02.10: * fixed: Scripts/Tools/ToolAdaptors: sh: bash_entry: always convert backend paths to native with forward slashes for `BASH_SOURCE_*` variables * new: preconfigure scripts
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@383 699b1267-f50e-4533-8e1b-323e5631ef88

---
+++ preconfigure.bat
@@ -0,0 +1,70 @@
+@echo off
+
+setlocal
+
+call "%%~dp0__init__.bat" || exit /b
+
+if %UAC_MODE%0 EQU 0 (
+  rem request admin permissions
+  set UAC_MODE=1
+  call :CMD "%%CONTOOLS_ROOT%%\ToolAdaptors\lnk\cmd_admin.lnk" /c "%%~dpf0" %%*
+  exit /b
+)
+
+echo.1. Download `https://sf.net/p/contools/3dparty`.
+echo.2. Read the instructions from the readme file in the downloaded project to checkout third party sources.
+echo 3. Press any key to continue and select the `_src` subdirectory in the project as a third party catalog.
+
+pause
+
+for /F "usebackq eol=	 tokens=* delims=" %%i in (`@"%UTILITY_ROOT%\wxFileDialog.exe" "" "%CONFIGURE_ROOT%" "Select the third party catalog to link with..." -de`) do set "_3DPARTY_ROOT=%%i"
+
+if not exist "%_3DPARTY_ROOT%" (
+  if not defined _3DPARTY_ROOT (
+    echo.error: %~nx0: third party catalog is not selected.
+  ) else (
+    echo.error: %~nx0: third party catalog does not exist: "%_3DPARTY_ROOT%".
+  )
+  pause
+  exit /b 255
+) >&2
+
+call :CREATE_DIR_LINK "%%~dp0_3dparty" "%%_3DPARTY_ROOT%%"
+
+call :CREATE_DIR_LINK "%%~dp0_scripts" "%%~dp0_3dparty\utility\tacklelib\tacklelib\_scripts"
+call :CREATE_DIR_LINK "%%~dp0cmake" "%%~dp0_3dparty\utility\tacklelib\tacklelib\cmake"
+
+pause
+
+exit /b
+
+:CREATE_DIR_LINK
+set "MKLINK_CMD=mklink /D"
+call :MKLINK %%*
+exit /b
+
+:CREATE_FILE_LINK
+set "MKLINK_CMD=mklink"
+call :MKLINK %%*
+exit /b
+
+:MKLINK
+set "LINK_FROM=%~f1"
+set "LINK_TO=%~f2"
+
+if exist "%LINK_FROM%" exit /b 0
+
+call :CMD %%MKLINK_CMD%% "%%LINK_FROM%%" "%%LINK_TO%%"
+if exist "%LINK_FROM%" (
+  echo."%LINK_FROM%" -^> "%LINK_TO%"
+) else (
+  echo.%~nx0%: error: could not create link: "%LINK_FROM%" -^> "%LINK_TO%"
+  exit /b 255
+) >&2
+
+exit /b 0
+
+:CMD
+echo.^>%*
+(%*)
+exit /b
---
+++ preconfigure.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Script ONLY for execution.
+if [[ -n "$BASH" && (-z "$BASH_LINENO" || ${BASH_LINENO[0]} -eq 0) ]]; then 
+
+source "/bin/bash_entry" || exit $?
+tkl_include "__init__.sh" || exit $?
+
+function Call()
+{
+  echo ">$@"
+  echo
+  "$@"
+  LastError=$?
+  return $LastError
+}
+
+function Pause()
+{
+  local key
+  read -n1 -r -p "Press any key to continue..." key
+  echo
+}
+
+case "$OSTYPE" in
+  mingw* | msys* | cygwin*)
+    Call "${COMSPEC//\\//}" /c "${BASH_SOURCE_DIR}/preconfigure.bat" $@
+    exit $?
+    ;;
+  *)
+    echo "1. Download `https://sf.net/p/tacklelib/3dparty`."
+    echo "2. Read the instructions from the readme file in the downloaded project to checkout third party sources."
+    echo "3. Press any key to continue and select the `_src` subdirectory in the project as a third party catalog."
+
+    Pause
+
+    _3DPARTY_ROOT=$("${UTILITY_ROOT}/wxFileDialog" "" "${CONFIGURE_ROOT}" "Select the third party catalog to link with..." -de)
+
+    if [[ ! -d "${_3DPARTY_ROOT}" ]]; then
+      if [[ -z "${_3DPARTY_ROOT}" ]]; then
+        echo "error: $0: third party catalog is not selected." >&2
+      else
+        echo "error: $0: third party catalog does not exist: `${_3DPARTY_ROOT}`" >&2
+      fi
+      exit 255
+    fi
+
+    Call ln -s "$BASH_SOURCE_DIR/_3dparty" "${_3DPARTY_ROOT}"
+
+    Call ln -s "$BASH_SOURCE_DIR/_3dparty/utility/tacklelib/tacklelib/_scripts" "$BASH_SOURCE_DIR/_scripts"
+    Call ln -s "$BASH_SOURCE_DIR/_3dparty/utility/tacklelib/tacklelib/cmake" "$BASH_SOURCE_DIR/cmake"
+    ;;
+esac
+
+fi
