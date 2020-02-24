dd66fe9106c29024618b22395528f4b7f836c8a3
andry81 <andry@inbox.ru>
2020-02-24 21:58:47 +0000

    2020.02.25: * changed: Scripts/Tools: `load_config.bat` improved to load compatible to the unix shell configuration `key=value` pairs: `key="value"` instead of `"key=value"` or `key=(value0 ... valueN)` for a list of values
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@392 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tools/load_config.bat
+++ Scripts/Tools/load_config.bat
@@ -22,19 +22,41 @@ if exist "%__CONFIG_DIR%\%__CONFIG_FILE%.in" (
 )
 
 rem load configuration files
-if exist "%__CONFIG_DIR%\%__CONFIG_FILE%" (
-  for /F "usebackq eol=# tokens=* delims=" %%i in ("%__CONFIG_DIR%\%__CONFIG_FILE%") do (
-    call set %%i
-  )
-) else (
+if not exist "%__CONFIG_DIR%\%__CONFIG_FILE%" (
   echo.%~nx0: error: config file is not found: "%__CONFIG_DIR%\%__CONFIG_FILE%".
   exit /b 3
 ) >&2
 
+for /F "usebackq eol=# tokens=* delims=" %%i in ("%__CONFIG_DIR%\%__CONFIG_FILE%") do for /F "eol=	 tokens=1,* delims==" %%j in ("%%i") do (
+  set __VAR=%%j
+  set __VALUE=%%k
+  call :PARSE
+)
+
 rem drop local variables
 (
+  set "__VAR="
+  set "__VALUE="
   set "__CONFIG_DIR="
   set "__CONFIG_FILE="
 )
 
 exit /b 0
+
+:PARSE
+if not defined __VALUE (
+  set "%__VAR%="
+  exit /b 0
+)
+
+setlocal ENABLEDELAYEDEXPANSION
+
+if ^" == ^%__VALUE:~0,1% (
+  for /F "eol=	 tokens=* delims=" %%i in ("!__VALUE:~1,-1!") do (
+    endlocal
+    set %__VAR%=%%i
+  )
+) else for /F "eol=	 tokens=* delims=" %%i in ("!__VALUE!") do (
+  endlocal
+  set %__VAR%=%%i
+)
