960b00755384c8071cc4c223e0ad35e9ef37280c
andry81 <andry@inbox.ru>
2020-07-18 21:55:58 +0000

    2020.07.18: * fixed: Scripts/Tests/batscripts: execution fixes * new: Scripts/Tools/std: `trim_var.bat` and `echo_var.bat` scripts including handle of system characters like `!`, `%`, `"`, "^" and so on * new: Scripts/Tests/batscripts: `trim_var.bat` script tests * new: Scripts/Tools/testlib: `endlocal.bat` script to use from a test script in case if the `setlocal` command call is a part of a test case
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@406 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tests/batscripts/__init__.bat
+++ Scripts/Tests/batscripts/__init__.bat
@@ -1,8 +1,6 @@
 @echo off
 
-set "TESTS_ROOT=%~dp0"
-set "TESTS_ROOT=%TESTS_ROOT:~0,-1%"
-set "TESTS_ROOT=%TESTS_ROOT:\=/%"
+call :CANONICAL_PATH TESTS_ROOT "%%~dp0"
 
 rem initialize Tools "module"
 call "%%TESTS_ROOT%%/../../Tools/__init__.bat" || exit /b
@@ -10,16 +8,13 @@ call "%%TESTS_ROOT%%/../../Tools/__init__.bat" || exit /b
 rem initialize testlib "module"
 call "%%CONTOOLS_ROOT%%/testlib/__init__.bat" || exit /b
 
-set "TEST_SRC_BASE_DIR=%TESTS_ROOT%"
-
-set "TEST_DATA_BASE_DIR=%TEST_SRC_BASE_DIR%/_testdata"
-set "TEST_TEMP_BASE_DIR=%TEST_SRC_BASE_DIR%/../../../Temp"
-
-call :GET_ABSOLUTE_PATH "%%TEST_DATA_BASE_DIR%%"
-set "TEST_DATA_BASE_DIR=%RETURN_VALUE%"
+call :CANONICAL_PATH TEST_SRC_BASE_DIR  "%%TESTS_ROOT%%"
+call :CANONICAL_PATH TEST_DATA_BASE_DIR "%%TEST_SRC_BASE_DIR%%/_testdata"
+call :CANONICAL_PATH TEST_TEMP_BASE_DIR "%%TEST_SRC_BASE_DIR%%/../../../Temp"
 
 exit /b 0
 
-:GET_ABSOLUTE_PATH
-set "RETURN_VALUE=%~dpf1"
+:CANONICAL_PATH
+set "%~1=%~dpf2"
+call set "%%~1=%%%~1:\=/%%"
 exit /b 0
