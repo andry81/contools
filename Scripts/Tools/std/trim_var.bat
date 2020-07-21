572ac8286a12683b0a452a7b94a2b632f7d25526
andry81 <andry@inbox.ru>
2020-07-21 13:16:15 +0000

    2020.07.21: * changed: Scripts/Tools/std: trim_var.bat: optimized, removed redundant `setlocal ENABLEDELAYEDEXPANSION` section
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@410 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tools/std/trim_var.bat
+++ Scripts/Tools/std/trim_var.bat
@@ -10,40 +10,20 @@ setlocal DISABLEDELAYEDEXPANSION
 rem Load and replace a value quote characters by the \x01 character.
 call set "RETURN_VALUE=%%%~1:"=%%"
 
-rem Encode value to remove exclamation characters.
-set "RETURN_VALUE=%RETURN_VALUE:?=?00%"
-set "RETURN_VALUE=%RETURN_VALUE:!=?01%"
-
-if not defined RETURN_VALUE exit /b 0
-
-rem safe to enable
-setlocal ENABLEDELAYEDEXPANSION
-
 :TRIM_LEFT_LOOP
-if not "!RETURN_VALUE:~0,1!" == " " if not "!RETURN_VALUE:~0,1!" == "	" goto TRIM_LEFT_LOOP_END
-set "RETURN_VALUE=!RETURN_VALUE:~1!"
+if not defined RETURN_VALUE exit /b 0
+if not ^%RETURN_VALUE:~0,1%/ == ^ / if not ^%RETURN_VALUE:~0,1%/ == ^	/ goto TRIM_RIGHT_LOOP
+set "RETURN_VALUE=%RETURN_VALUE:~1%"
+if not defined RETURN_VALUE exit /b 0
 goto TRIM_LEFT_LOOP
 
-:TRIM_LEFT_LOOP_END
-if not defined RETURN_VALUE exit /b 0
-
 :TRIM_RIGHT_LOOP
-if not "!RETURN_VALUE:~-1!" == " " if not "!RETURN_VALUE:~-1!" == "	" goto END
-set "RETURN_VALUE=!RETURN_VALUE:~0,-1!"
+if not defined RETURN_VALUE exit /b 0
+if not ^%RETURN_VALUE:~-1%/ == ^ / if not ^%RETURN_VALUE:~-1%/ == ^	/ goto TRIM_RIGHT_LOOP_END
+set "RETURN_VALUE=%RETURN_VALUE:~0,-1%"
 goto TRIM_RIGHT_LOOP
 
-:END
-if not defined RETURN_VALUE exit /b 0
-
-rem restore value
-(
-  endlocal
-  set "RETURN_VALUE=%RETURN_VALUE:?01=!%"
-)
-
-rem restore value
-set "RETURN_VALUE=%RETURN_VALUE:?00=?%"
-
+:TRIM_RIGHT_LOOP_END
 rem recode quote and exclamation characters
 set "__ESC__=^"
 set __QUOT__=^"
