fe7e4edb96dd79efe81f3825efd98b0c6bd87d64
andry81 <andry@inbox.ru>
2020-08-31 05:27:09 +0000

    2020.08.31: * fixed: Scripts/Tools/ToolAdapters/notepadplusplus: npplib.py: restore first active tab at the end of each tab manipulation script
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@432 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tools/ToolAdaptors/notepadpusplus/scripts/npplib.py
+++ Scripts/Tools/ToolAdaptors/notepadpusplus/scripts/npplib.py
@@ -2,22 +2,37 @@ from __future__ import print_function
 
 def toggle_readonly_flag_for_all_tabs():
   print('toggle_readonly_flag_for_all_tabs:')
+
+  active_file = notepad.getCurrentFilename()
+ 
   for f in notepad.getFiles():
     notepad.activateFile(f[0])
     notepad.menuCommand(MENUCOMMAND.EDIT_SETREADONLY)
     print("  - {}".format(f))
+
+  notepad.activateFile(active_file)
+
   print()
 
 def clear_readonly_flag_from_all_files():
   print('clear_readonly_flag_from_all_files:')
+
+  active_file = notepad.getCurrentFilename()
+
   for f in notepad.getFiles():
     notepad.activateFile(f[0])
     notepad.menuCommand(MENUCOMMAND.EDIT_CLEARREADONLY)
     print("  - {}".format(f))
+
+  notepad.activateFile(active_file)
+
   print()
 
 def reopen_all_files():
   print('reopen_all_files:')
+
+  active_file = notepad.getCurrentFilename()
+
   notepad.saveAllFiles()
   all_files = notepad.getFiles()
   notepad.closeAll()
@@ -25,4 +40,7 @@ def reopen_all_files():
   for f in all_files:
     notepad.open(f[0])
     print("  - {}".format(f[0]))
+
+  notepad.activateFile(active_file)
+
   print()
