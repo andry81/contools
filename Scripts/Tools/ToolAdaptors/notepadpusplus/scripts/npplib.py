715c70c69fd1e4f382a1ad5754586fee21d2f7f8
andry81 <andry@inbox.ru>
2020-08-31 06:44:32 +0000

    2020.08.31: * fixed: Scripts/Tools/ToolAdapters/notepadplusplus: npplib.py: reverse activation order before restore last active tab to retain selection order reverse to activation order
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@435 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tools/ToolAdaptors/notepadpusplus/scripts/npplib.py
+++ Scripts/Tools/ToolAdaptors/notepadpusplus/scripts/npplib.py
@@ -3,9 +3,10 @@ from __future__ import print_function
 def toggle_readonly_flag_for_all_tabs():
   print('toggle_readonly_flag_for_all_tabs:')
 
+  all_files = notepad.getFiles()
   active_file = notepad.getCurrentFilename()
- 
-  for f in notepad.getFiles():
+
+  for f in reversed(all_files):
     notepad.activateFile(f[0])
     notepad.menuCommand(MENUCOMMAND.EDIT_SETREADONLY)
     print("  - {}".format(f))
@@ -17,9 +18,10 @@ def toggle_readonly_flag_for_all_tabs():
 def clear_readonly_flag_from_all_files():
   print('clear_readonly_flag_from_all_files:')
 
+  all_files = notepad.getFiles()
   active_file = notepad.getCurrentFilename()
 
-  for f in notepad.getFiles():
+  for f in reversed(all_files):
     notepad.activateFile(f[0])
     notepad.menuCommand(MENUCOMMAND.EDIT_CLEARREADONLY)
     print("  - {}".format(f))
@@ -31,15 +33,19 @@ def clear_readonly_flag_from_all_files():
 def reopen_all_files():
   print('reopen_all_files:')
 
+  all_files = list(notepad.getFiles())
   active_file = notepad.getCurrentFilename()
 
   notepad.saveAllFiles()
-  all_files = notepad.getFiles()
   notepad.closeAll()
 
   for f in all_files:
     notepad.open(f[0])
     print("  - {}".format(f[0]))
+
+  # reactive in reverse order
+  for f in reversed(all_files):
+    notepad.activateFile(f[0])
 
   notepad.activateFile(active_file)
 
