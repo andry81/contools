843925ed710a71d8de8f610c4057b488640066bc
andry81 <andry@inbox.ru>
2021-01-09 14:35:10 +0000

    2021.01.09: * new: Scripts/Tools/std: `get_wmic_os_version.bat` script
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@514 699b1267-f50e-4533-8e1b-323e5631ef88

---
+++ Scripts/Tools/std/get_wmic_os_version.bat
@@ -0,0 +1,19 @@
+@echo off
+
+rem Description:
+rem   Independent to Windows OS version request.
+
+rem Drop last error level
+type nul>nul
+
+rem drop return value
+set "RETURN_VALUE="
+
+for /F "usebackq eol= tokens=1,2 delims==" %%i in (`wmic os get version /VALUE 2^>nul`) do if "%%i" == "Version" set "RETURN_VALUE=%%j"
+
+if defined RETURN_VALUE (
+  set "RETURN_VALUE=%RETURN_VALUE:~0,18%"
+  exit /b 0
+)
+
+exit /b 1
