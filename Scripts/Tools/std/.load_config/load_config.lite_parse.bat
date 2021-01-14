d8bb5bc2141af9fc4c235a0487988f33ea540eb0
andry81 <andry@inbox.ru>
2021-01-14 12:20:49 +0000

    2021.01.14: * fixed: Scripts/Tools/std: load_script.bat: minor fixup * new: Scripts/Tools/std: `gen_config.bat` script to generate config only (without load) * new: Scripts/Tools/std: load_script.bat: added 2 uniform variable parameters to a variable declaration (usage: <varname>:<param0>:<param1>=<value>) * new: Scripts/Tools/std: load_script.bat: `-noexpand` flag to explicitly avoid variable expansion on load as by default
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@523 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tools/std/.load_config/load_config.lite_parse.bat
+++ Scripts/Tools/std/.load_config/load_config.lite_parse.bat
@@ -1,17 +1,40 @@
 
 (
   endlocal
-  for /F "usebackq eol=# tokens=1,* delims==" %%i in ("%__?CONFIG_OUT_DIR%/%__?CONFIG_FILE%") do ( set "__?VALUE=%%j" & if defined __?VALUE ( call :PARSE_EXPR %%i ) else set "%%i=" )
-  set "__?VALUE="
+  for /F "usebackq eol=# tokens=1,* delims==" %%i in ("%__?CONFIG_OUT_DIR%/%__?CONFIG_FILE%") do ( set "__?VALUE=%%j" & call :PARSE "%%~1" "%%~2" "%%~3" %%i )
+  set "__?VALUE=" & set "__?VAR_EXPR=" & set "__?PARAM0=" & set "__?PARAM1=" & set "__?QUOT__=" & set "__?EXCL__=" & set "__?ESC__="
 )
-
 exit /b 0
 
+:PARSE
+set "__?VAR_EXPR=%~4"
+set "__?VAR_EXPR=%__?VAR_EXPR:::=:.:%"
+for /F "eol= tokens=1,2,* delims=:" %%i in ("%__?VAR_EXPR%") do ( call :PARSE_EXPR "%%~4" "%%~i" "%%~j" "%%~k" "%%~1" "%%~2" "%%~3" )
+exit /b
+
 :PARSE_EXPR
-if ^/ == ^%__?VALUE:~0,1%/ ( set "%~1=" & exit /b 0 )
-if ^/ == ^%__?VALUE:~1,1%/ ( set %~1=^%__?VALUE:~1,1%& exit /b 0 )
-if ^"/ == ^%__?VALUE:~0,1%/ (
-  if ^"/ == ^%__?VALUE:~-1%/ (
-    call set "%~1=%__?VALUE:~1,-1%"
-  ) else call set %%1=%__?VALUE%
-) else call set %%1=%__?VALUE%
+if not defined __?VALUE goto PARSE_VAR
+
+set "__?VALUE=%__?VALUE:"=%"
+
+if %~5 EQU 0 call set "__?VALUE=%__?VALUE%"
+
+if ^/ == ^%__?VALUE:~0,1%/ if ^/ == ^%__?VALUE:~-1%/ call set "__?VALUE=%__?VALUE:~1,-1%"
+
+:PARSE_VAR
+if not "%~3" =="." ( set "__?PARAM0=%~3" ) else set "__?PARAM0="
+if not "%~4" =="." ( set "__?PARAM1=%~4" ) else set "__?PARAM1="
+if "%~6" == "" ( if "%~7" == "" ( if /i not "%__?PARAM0%" == "" ( set "%~1=" ) else if /i not "%__?PARAM1%" == "" ( set "%~1=" ) else ( goto PARSE_VALUE )
+) else if /i not "%__?PARAM0%" == "" ( set "%~1=" ) else if /i "%__?PARAM1%" == "%~7" ( set "%~1=" & goto PARSE_VALUE ) else if /i not "%__?PARAM1%" == "" set "%~1="
+) else if "%~7" == "" ( if /i not "%__?PARAM1%" == "" ( set "%~1=" ) else if /i "%__?PARAM0%" == "%~6" ( set "%~1=" & goto PARSE_VALUE ) else if /i not "%__?PARAM0%" == "" set "%~1="
+) else if /i "%__?PARAM0%" == "%~6" if /i "%__?PARAM1%" == "%~7" ( set "%~1=" & goto PARSE_VALUE
+) else if /i not "%__?PARAM0%" == "" ( set "%~1=" ) else if /i not "%__?PARAM1%" == "" set "%~1="
+exit /b
+
+:PARSE_VALUE
+set __?QUOT__=^"
+set "__?EXCL__=!" & set "__?ESC__=^"
+set "__?VALUE=%__?VALUE:!=!__?EXCL__!%"
+set "__?VALUE=%__?VALUE:=!__?QUOT__!%"
+set "__?VALUE=%__?VALUE:^=!__?ESC__!%"
+setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("%__?VALUE%") do ( endlocal & set "%~2=%%i" )
