11794130b9060774d3c04a75c23f28913415ceb4
andry81 <andry@inbox.ru>
2021-01-15 14:34:50 +0000

    2021.01.15: * fixed: Scripts/Tools/std: chcp.bat, restorecp.bat: reimplemented to support invalid code page setup ignore and correct restore * changed: Scripts/Tools/std: chcp.bat, restorecp.bat, pause.bat: check the `chcp.com` existence before usage
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@524 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tools/std/chcp.bat
+++ Scripts/Tools/std/chcp.bat
@@ -12,6 +12,10 @@ setlocal
 rem drop last error level
 type nul>nul
 
+set "?~dp0=%~dp0"
+set "?~n0=%~n0"
+set "?~nx0=%~nx0"
+
 rem script flags
 set FLAG_PRINT=0
 
@@ -40,32 +44,38 @@ if defined FLAG (
 set "CODE_PAGE=%~1"
 
 if not defined CODE_PAGE (
-  echo.%~nx0: error: CODE_PAGE is not defined.
+  echo.%?~nx0%: error: CODE_PAGE is not defined.
   exit /b -255
 ) >&2
-if not defined CURRENT_CP goto INIT
+
+if not exist "%SystemRoot%\System32\chcp.com" (
+  echo.%?~nx0%: error: `chcp.com` is not found.
+  exit /b -255
+) >&2
+
+if not defined CP_HISTORY_LIST goto INIT
 
 set "LAST_CP=%CURRENT_CP%"
-if defined CP_HISTORY_LIST (
-  set "CP_HISTORY_LIST=%CURRENT_CP%|%CP_HISTORY_LIST%"
-) else (
-  set "CP_HISTORY_LIST=%CURRENT_CP%|"
-)
+if not defined LAST_CP for /F "usebackq eol= tokens=1,* delims=:" %%i in (`@"%%SystemRoot%%\System32\chcp.com" 2^>nul`) do set "LAST_CP=%%j"
+set "CP_HISTORY_LIST=%LAST_CP%|%CP_HISTORY_LIST%"
 set "CURRENT_CP=%CODE_PAGE%"
 
 goto UPDATECP
 
 :INIT
 set "LAST_CP="
-set "CURRENT_CP=%CODE_PAGE%"
-for /F "usebackq eol= tokens=1,* delims=:" %%i in (`chcp.com 2^>nul`) do set "LAST_CP=%%j"
+for /F "usebackq eol= tokens=1,* delims=:" %%i in (`@"%%SystemRoot%%\System32\chcp.com" 2^>nul`) do set "LAST_CP=%%j"
 if defined LAST_CP set "LAST_CP=%LAST_CP: =%"
-if defined LAST_CP set "CP_HISTORY_LIST=%LAST_CP%|"
+
+set "CURRENT_CP=%CODE_PAGE%"
+set "CP_HISTORY_LIST=%LAST_CP%|"
 
 :UPDATECP
-if not "%CURRENT_CP%" == "%LAST_CP%" if %FLAG_PRINT% NEQ 0 (
-  chcp.com %CURRENT_CP%
-) else chcp.com %CURRENT_CP% >nul
+if "%CURRENT_CP%" == "%LAST_CP%" goto EXIT
+
+if %FLAG_PRINT% NEQ 0 (
+  "%SystemRoot%\System32\chcp.com" %CURRENT_CP% || set "CURRENT_CP=%LAST_CP%"
+) else "%SystemRoot%\System32\chcp.com" %CURRENT_CP% >nul || set "CURRENT_CP=%LAST_CP%"
 
 :EXIT
 (
--- Scripts/Tools/std/pause.bat
+++ Scripts/Tools/std/pause.bat
@@ -3,8 +3,8 @@
 rem DESCRIPTION:
 rem   Sets the code page to the last known (`%LAST_CP%`) after a code page restore routine (basically after a call to the
 rem   `restorecp.bat` script). That is required in cases where the code page before a call to the `chcp.bat` was different
-rem   when the after and so the `pause` command does print unreadable text in the log. This is it, all output into a log
-rem   file must be made under the same code page!
+rem   when the after and so the `pause` command does print unreadable text in the log.
+rem   This is it, all output into a log file must be made under the same code page!
 rem
 rem NOTE:
 rem   Script does not change the error level because restores it internally
@@ -12,15 +12,18 @@ rem
 
 setlocal & set "LASTERROR=%ERRORLEVEL%"
 
+if not exist "%SystemRoot%\System32\chcp.com" goto PAUSE
+
 set "CURRENT_CP="
 for /F "usebackq eol= tokens=1,* delims=:" %%i in (`chcp.com 2^>nul`) do set "CURRENT_CP=%%j"
 
 if defined CURRENT_CP set "CURRENT_CP=%CURRENT_CP: =%"
 
 if exist "%SystemRoot%\System32\timeout.exe" (
-  if defined LAST_CP if not "%CURRENT_CP%" == "%LAST_CP%" ( chcp.com %LAST_CP% >nul & "%SystemRoot%\System32\timeout.exe" /T -1 & chcp.com %CURRENT_CP% >nul & exit /b %LASTERROR% )
-) else if defined LAST_CP if not "%CURRENT_CP%" == "%LAST_CP%" ( chcp.com %LAST_CP% >nul & pause & chcp.com %CURRENT_CP% >nul & exit /b %LASTERROR% )
+  if defined LAST_CP if not "%CURRENT_CP%" == "%LAST_CP%" ( "%SystemRoot%\System32\chcp.com" %LAST_CP% >nul & "%SystemRoot%\System32\timeout.exe" /T -1 & "%SystemRoot%\System32\chcp.com" %CURRENT_CP% >nul & exit /b %LASTERROR% )
+) else if defined LAST_CP if not "%CURRENT_CP%" == "%LAST_CP%" ( "%SystemRoot%\System32\chcp.com" %LAST_CP% >nul & pause & "%SystemRoot%\System32\chcp.com" %CURRENT_CP% >nul & exit /b %LASTERROR% )
 
+:PAUSE
 pause
 
 exit /b %LASTERROR%
