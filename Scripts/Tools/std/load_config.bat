c23dd102d38bd3064bfd39ba323a84b1ce17c8ea
andry81 <andry@inbox.ru>
2021-01-16 21:48:35 +0000

    2021.01.17: * fixed: Scripts/Tools/std: load_config.bat: minor fixup * new: Scripts/Tools/std: gen_config.bat: placeholder replace functionality * changed: Scripts/Tools/std: load_config.bat: `-gen_config` flag to explicitly generate the configuration file, the configuration file does not generate anymore by default
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@532 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tools/std/load_config.bat
+++ Scripts/Tools/std/load_config.bat
@@ -7,6 +7,7 @@ set "__?~n0=%~n0"
 set "__?~nx0=%~nx0"
 
 rem script flags
+set __?FLAG_GEN_CONFIG=0
 set __?FLAG_LITE_PARSE=0
 set __?FLAG_NO_EXPAND=0
 set __?FLAG_FULL_PARSE=0
@@ -21,7 +22,9 @@ if defined FLAG ^
 if not "%FLAG:~0,1%" == "-" set "FLAG="
 
 if defined FLAG (
-  if "%FLAG%" == "-lite_parse" (
+  if "%FLAG%" == "-gen_config" (
+    set __?FLAG_GEN_CONFIG=1
+  ) else if "%FLAG%" == "-lite_parse" (
     set __?FLAG_LITE_PARSE=1
   ) else if "%FLAG%" == "-noexpand" (
     set __?FLAG_NO_EXPAND=1
@@ -30,7 +33,7 @@ if defined FLAG (
   ) else if "%FLAG%" == "-allow_not_known_class_as_var_name" (
     set __?FLAG_ALLOW_NOT_KNOWN_CLASS_AS_VAR_NAME=1
   ) else (
-    echo.%?~nx0%: error: invalid flag: %FLAG%
+    echo.%__?~nx0%: error: invalid flag: %FLAG%
     exit /b -255
   ) >&2
 
@@ -47,40 +50,38 @@ set "__?PARAM0=%~4"
 set "__?PARAM1=%~5"
 
 if not defined __?CONFIG_IN_DIR (
-  echo.%?~nx0%: error: input config directory is not defined.
+  echo.%__?~nx0%: error: input config directory is not defined.
   exit /b 1
 ) >&2
 
 if not defined __?CONFIG_OUT_DIR (
-  echo.%?~nx0%: error: output config directory is not defined.
+  echo.%__?~nx0%: error: output config directory is not defined.
   exit /b 2
 ) >&2
 
-set "__?CONFIG_IN_DIR=%__?CONFIG_IN_DIR:\=/%"
-set "__?CONFIG_OUT_DIR=%__?CONFIG_OUT_DIR:\=/%"
-
-if "%__?CONFIG_IN_DIR:~-1%" == "/" set "__?CONFIG_IN_DIR=%__?CONFIG_IN_DIR:~0,-1%"
-if "%__?CONFIG_OUT_DIR:~-1%" == "/" set "__?CONFIG_OUT_DIR=%__?CONFIG_OUT_DIR:~0,-1%"
+for /F "eol= tokens=* delims=" %%i in ("%__?CONFIG_IN_DIR%\.") do set "__?CONFIG_IN_DIR=%%~fi"
+for /F "eol= tokens=* delims=" %%i in ("%__?CONFIG_OUT_DIR%\.") do set "__?CONFIG_OUT_DIR=%%~fi"
 
 if not exist "%__?CONFIG_IN_DIR%\" (
-  echo.%?~nx0%: error: input config directory does not exist: "%__?CONFIG_IN_DIR%".
+  echo.%__?~nx0%: error: input config directory does not exist: "%__?CONFIG_IN_DIR%".
   exit /b 10
 ) >&2
 
 if not exist "%__?CONFIG_OUT_DIR%\" (
-  echo.%?~nx0%: error: output config directory does not exist: "%__?CONFIG_OUT_DIR%".
+  echo.%__?~nx0%: error: output config directory does not exist: "%__?CONFIG_OUT_DIR%".
   exit /b 11
 ) >&2
 
-if not exist "%__?CONFIG_OUT_DIR%\%__?CONFIG_FILE%" ^
-if exist "%__?CONFIG_IN_DIR%/%__?CONFIG_FILE%.in" (
-  echo."%__?CONFIG_IN_DIR%/%__?CONFIG_FILE%.in" -^> "%__?CONFIG_OUT_DIR%/%__?CONFIG_FILE%"
-  type "%__?CONFIG_IN_DIR:/=\%\%__?CONFIG_FILE%.in" > "%__?CONFIG_OUT_DIR%/%__?CONFIG_FILE%"
+if %__?FLAG_GEN_CONFIG% NEQ 0 (
+  if not exist "%__?CONFIG_OUT_DIR%\%__?CONFIG_FILE%" if exist "%__?CONFIG_IN_DIR%\%__?CONFIG_FILE%.in" (
+    echo."%__?CONFIG_IN_DIR%\%__?CONFIG_FILE%.in" -^> "%__?CONFIG_OUT_DIR%\%__?CONFIG_FILE%"
+    type "%__?CONFIG_IN_DIR%\%__?CONFIG_FILE%.in" > "%__?CONFIG_OUT_DIR%\%__?CONFIG_FILE%"
+  )
 )
 
 rem load configuration files
-if not exist "%__?CONFIG_OUT_DIR%/%__?CONFIG_FILE%" (
-  echo.%?~nx0%: error: config file is not found: "%__?CONFIG_OUT_DIR%/%__?CONFIG_FILE%".
+if not exist "%__?CONFIG_OUT_DIR%\%__?CONFIG_FILE%" (
+  echo.%__?~nx0%: error: config file is not found: "%__?CONFIG_OUT_DIR%\%__?CONFIG_FILE%".
   exit /b 20
 ) >&2
 
