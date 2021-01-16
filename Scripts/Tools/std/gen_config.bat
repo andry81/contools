df81bddc9c3fda49af316dab025766f8cfe9273b
andry81 <andry@inbox.ru>
2021-01-16 21:48:35 +0000

    2021.01.17: * fixed: Scripts/Tools/std: load_config.bat: minor fixup * new: Scripts/Tools/std: gen_config.bat: placeholder replace functionality * changed: Scripts/Tools/std: load_config.bat: `-gen_config` flag to explicitly generate the configuration file, the configuration file does not generate anymore by default
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@532 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tools/std/gen_config.bat
+++ Scripts/Tools/std/gen_config.bat
@@ -2,38 +2,84 @@
 
 setlocal
 
+call "%%~dp0__init__.bat" || exit /b
+
+set "?~dp0=%~dp0"
+set "?~n0=%~n0"
+set "?~nx0=%~nx0"
+
+rem script flags
+set "SED_BARE_FLAGS="
+set "SED_REPLACE_FROM="
+set "SED_REPLACE_TO="
+
+:FLAGS_LOOP
+
+if defined SED_REPLACE_FROM set "SED_REPLACE_FROM=%SED_REPLACE_FROM:\=\\%"
+if defined SED_REPLACE_FROM set "SED_REPLACE_FROM=%SED_REPLACE_FROM:|=\|%"
+if defined SED_REPLACE_FROM set "SED_REPLACE_FROM=%SED_REPLACE_FROM:{=\{%"
+if defined SED_REPLACE_FROM set "SED_REPLACE_FROM=%SED_REPLACE_FROM:}=\}%"
+
+if defined SED_REPLACE_TO set "SED_REPLACE_TO=%SED_REPLACE_TO:\=\\%"
+if defined SED_REPLACE_TO set "SED_REPLACE_TO=%SED_REPLACE_TO:|=\|%"
+if defined SED_REPLACE_TO set "SED_REPLACE_TO=%SED_REPLACE_TO:{=\{%"
+if defined SED_REPLACE_TO set "SED_REPLACE_TO=%SED_REPLACE_TO:}=\}%"
+
+if defined SED_REPLACE_FROM if defined SED_REPLACE_TO set SED_BARE_FLAGS=%SED_BARE_FLAGS% -e "s|%SED_REPLACE_FROM%|%SED_REPLACE_TO%|mg"
+
+rem flags always at first
+set "FLAG=%~1"
+
+if defined FLAG ^
+if not "%FLAG:~0,1%" == "-" set "FLAG="
+
+if defined FLAG (
+  if "%FLAG%" == "-r" (
+    set "SED_REPLACE_FROM=%~2"
+    set "SED_REPLACE_TO=%~3"
+    shift
+    shift
+  ) else (
+    echo.%?~nx0%: error: invalid flag: %FLAG%
+  ) >&2
+
+  shift
+
+  rem read until no flags
+  goto FLAGS_LOOP
+)
+
 set "CONFIG_IN_DIR=%~1"
 set "CONFIG_OUT_DIR=%~2"
 set "CONFIG_FILE=%~3"
 
 if not defined CONFIG_IN_DIR (
-  echo.%~nx0: error: input config directory is not defined.
+  echo.%?~nx0%: error: input config directory is not defined.
   exit /b 1
 ) >&2
 
 if not defined CONFIG_OUT_DIR (
-  echo.%~nx0: error: output config directory is not defined.
+  echo.%?~nx0%: error: output config directory is not defined.
   exit /b 2
 ) >&2
 
-set "CONFIG_IN_DIR=%CONFIG_IN_DIR:\=/%"
-set "CONFIG_OUT_DIR=%CONFIG_OUT_DIR:\=/%"
-
-if "%CONFIG_IN_DIR:~-1%" == "/" set "CONFIG_IN_DIR=%CONFIG_IN_DIR:~0,-1%"
-if "%CONFIG_OUT_DIR:~-1%" == "/" set "CONFIG_OUT_DIR=%CONFIG_OUT_DIR:~0,-1%"
+for /F "eol= tokens=* delims=" %%i in ("%CONFIG_IN_DIR%\.") do set "CONFIG_IN_DIR=%%~fi"
+for /F "eol= tokens=* delims=" %%i in ("%CONFIG_OUT_DIR%\.") do set "CONFIG_OUT_DIR=%%~fi"
 
 if not exist "%CONFIG_IN_DIR%\" (
-  echo.%~nx0: error: input config directory does not exist: "%CONFIG_IN_DIR%".
+  echo.%?~nx0%: error: input config directory does not exist: "%CONFIG_IN_DIR%".
   exit /b 10
 ) >&2
 
 if not exist "%CONFIG_OUT_DIR%\" (
-  echo.%~nx0: error: output config directory does not exist: "%CONFIG_OUT_DIR%".
+  echo.%?~nx0%: error: output config directory does not exist: "%CONFIG_OUT_DIR%".
   exit /b 11
 ) >&2
 
 if not exist "%CONFIG_OUT_DIR%\%CONFIG_FILE%" ^
-if exist "%CONFIG_IN_DIR%/%CONFIG_FILE%.in" (
-  echo."%CONFIG_IN_DIR%/%CONFIG_FILE%.in" -^> "%CONFIG_OUT_DIR%/%CONFIG_FILE%"
-  type "%CONFIG_IN_DIR:/=\%\%CONFIG_FILE%.in" > "%CONFIG_OUT_DIR%/%CONFIG_FILE%"
+if exist "%CONFIG_IN_DIR%\%CONFIG_FILE%.in" (
+  echo."%CONFIG_IN_DIR%\%CONFIG_FILE%.in" -^> "%CONFIG_OUT_DIR%\%CONFIG_FILE%"
+  if defined SED_BARE_FLAGS (
+    type "%CONFIG_IN_DIR%\%CONFIG_FILE%.in" | "%CONTOOLS_GNUWIN32_ROOT%/bin/sed.exe" -r -b%SED_BARE_FLAGS% > "%CONFIG_OUT_DIR%\%CONFIG_FILE%"
+  ) else type "%CONFIG_IN_DIR%\%CONFIG_FILE%.in" > "%CONFIG_OUT_DIR%\%CONFIG_FILE%"
 )
