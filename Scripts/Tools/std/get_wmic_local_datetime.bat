c8e23c3d98058716b73d81140a41c92c5f3d8c67
andry81 <andry@inbox.ru>
2021-01-20 22:10:37 +0000

    2021.01.21: * changed: Scripts/Tools/std: get_wmic_*.bat: use direct path to the `wmic.exe`
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@540 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tools/std/get_wmic_local_datetime.bat
+++ Scripts/Tools/std/get_wmic_local_datetime.bat
@@ -9,7 +9,7 @@ type nul>nul
 rem drop return value
 set "RETURN_VALUE="
 
-for /F "usebackq eol= tokens=1,2 delims==" %%i in (`wmic path Win32_OperatingSystem get LocalDateTime /VALUE 2^>nul`) do if "%%i" == "LocalDateTime" set "RETURN_VALUE=%%j"
+for /F "usebackq eol= tokens=1,2 delims==" %%i in (`"%SystemRoot%\System32\wbem\wmic.exe" path Win32_OperatingSystem get LocalDateTime /VALUE 2^>nul`) do if "%%i" == "LocalDateTime" set "RETURN_VALUE=%%j"
 
 if defined RETURN_VALUE (
   set "RETURN_VALUE=%RETURN_VALUE:~0,18%"
