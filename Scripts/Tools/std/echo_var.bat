6f141b31da9b09259ff32e78fb0b195039e9a460
andry81 <andry@inbox.ru>
2021-02-10 06:09:56 +0000

    2021.02.10: * fixed: Scripts/Tools/std: echo_var.bat: accidental consume `!` and `^` characters
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@559 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tools/std/echo_var.bat
+++ Scripts/Tools/std/echo_var.bat
@@ -11,19 +11,12 @@ rem Load and replace a value quote characters by the \x01 character.
 call set "RETURN_VALUE=%%%~1:"=%%"
 
 rem recode quote and exclamation characters
-set "__ESC__=^"
-set __QUOT__=^"
-set "__EXCL__=!"
-set "RETURN_VALUE=%RETURN_VALUE:!=!__EXCL__!%"
-set "RETURN_VALUE=%RETURN_VALUE:^=!__ESC__!%"
-set "RETURN_VALUE=%RETURN_VALUE:=!__QUOT__!%"
+set __?QUOT__=^"
+set "__?EXCL__=!" & set "__?ESC__=^"
+set "RETURN_VALUE=%RETURN_VALUE:!=!__?EXCL__!%"
+set "RETURN_VALUE=%RETURN_VALUE:=!__?QUOT__!%"
+set "RETURN_VALUE=%RETURN_VALUE:^=!__?ESC__!%"
 
 rem safe echo
-setlocal ENABLEDELAYEDEXPANSION
-for /F "tokens=* delims=" %%i in ("!RETURN_VALUE!") do for /F "tokens=* delims=" %%j in ("%%i") do (
-  endlocal
-  endlocal
-  echo.%~2%%j%~3
-)
-
+setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!RETURN_VALUE!") do for /F "eol= tokens=* delims=" %%j in ("%%i") do endlocal & echo.%~2%%j%~3
 exit /b 0
