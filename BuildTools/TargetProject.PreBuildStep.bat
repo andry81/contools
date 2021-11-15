7261fe447674f98796a8b5ef0405bd3aac42f654
andry81 <andry@inbox.ru>
2021-11-15 14:40:54 +0000

    2021.11.15: * changed: _config, BuildTools/msys: `msys` binaries moved into externals * changed: BuildTools/TargetProject.PreBuildStep.bat: minor changes
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@729 699b1267-f50e-4533-8e1b-323e5631ef88

--- BuildTools/TargetProject.PreBuildStep.bat
+++ BuildTools/TargetProject.PreBuildStep.bat
@@ -19,7 +19,6 @@ if defined PROJECT_IN_SRC_DIR set "PROJECT_IN_SRC_DIR_PATH=%PROJECT_IN_SRC_DIR%\
 
 if not defined UNICODE_ENABLED set UNICODE_ENABLED=0
 
-set "BUILD_TOOLS_DIR=%SOLUTION_DIR%..\BuildTools"
 set "SOURCES_DIR=%SOLUTION_DIR%..\Utilities\src\%PROJECT_IN_SRC_DIR_PATH%"
 set "CONSOLE_HELP_FILE=%SOURCES_DIR%\help"
 
@@ -45,8 +44,8 @@ goto AUTOGEN_DEFINITIONS_LOOP
 if not defined APP_MODULE_NAME set "APP_MODULE_NAME=%PROJECT_NAME%"
 
 pushd "%SOURCES_DIR%" && (
-  if exist "%SOURCES_DIR%\help.def" "%BUILD_TOOLS_DIR%\msys\bin\autogen.exe"%AUTOGEN_BARE_FLAGS% -L "%SOURCES_DIR%" help.def
-  "%BUILD_TOOLS_DIR%\msys\bin\autogen.exe"%AUTOGEN_BARE_FLAGS% -L "%SOURCES_DIR%" version.def
+  if exist "%SOURCES_DIR%\help.def" call :CMD "%%CONTOOLS_AUTOGEN_ROOT%%\bin\autogen.exe"%%AUTOGEN_BARE_FLAGS%% -L "%%SOURCES_DIR%%" help.def
+  call :CMD "%%CONTOOLS_AUTOGEN_ROOT%%\bin\autogen.exe"%%AUTOGEN_BARE_FLAGS%% -L "%%SOURCES_DIR%%" version.def
   popd
 ) || exit /b 65534
 
@@ -56,10 +55,7 @@ if %UNICODE_ENABLED% NEQ 0 set THLIBAUTOCFG_CMD_LINE_FLAGS=%THLIBAUTOCFG_CMD_LIN
 set "THLIBAUTOCFG_CMD_LINE="
 if defined THLIBAUTOCFG_CMD_LINE_FLAGS set "THLIBAUTOCFG_CMD_LINE=-%THLIBAUTOCFG_CMD_LINE_FLAGS% "
 
-if exist "%CONSOLE_HELP_FILE%.txt" (
-  echo   "%CONSOLE_HELP_FILE%_inl.hpp"
-  call :CMD "%CONTOOLS_UTILITIES_BIN_ROOT%/contools/thlibautocfg.exe" -txt2c %THLIBAUTOCFG_CMD_LINE%"%CONSOLE_HELP_FILE%.txt" "%CONSOLE_HELP_FILE%_inl.hpp"
-)
+if exist "%CONSOLE_HELP_FILE%.txt" call :CMD "%CONTOOLS_UTILITIES_BIN_ROOT%/contools/thlibautocfg.exe" -txt2c %THLIBAUTOCFG_CMD_LINE%"%CONSOLE_HELP_FILE%.txt" "%CONSOLE_HELP_FILE%_inl.hpp"
 
 rem avoid output of this sequence: "error:"
 echo Last return code: %ERRORLEVEL%
