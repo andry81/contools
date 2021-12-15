2c6c2be2c4954a0bf0ae22b68dfd77fa0b5882df
andry81 <andry@inbox.ru>
2021-12-15 06:58:06 +0000

    2021.12.15: * changed: __init__: execution speed optimization
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@747 699b1267-f50e-4533-8e1b-323e5631ef88

--- __init__/__init__.bat
+++ __init__/__init__.bat
@@ -6,16 +6,16 @@ set "CONTOOLS_PROJECT_ROOT_INIT0_DIR=%~dp0"
 
 if not defined NEST_LVL set NEST_LVL=0
 
-if not defined CONTOOLS_PROJECT_ROOT                call :CANONICAL_PATH CONTOOLS_PROJECT_ROOT                "%%~dp0.."
-if not defined CONTOOLS_PROJECT_EXTERNALS_ROOT      call :CANONICAL_PATH CONTOOLS_PROJECT_EXTERNALS_ROOT      "%%CONTOOLS_PROJECT_ROOT%%/_externals"
+if not defined CONTOOLS_PROJECT_ROOT                call "%%~dp0canonical_path.bat" CONTOOLS_PROJECT_ROOT                "%%~dp0.."
+if not defined CONTOOLS_PROJECT_EXTERNALS_ROOT      call "%%~dp0canonical_path.bat" CONTOOLS_PROJECT_EXTERNALS_ROOT      "%%CONTOOLS_PROJECT_ROOT%%/_externals"
 
-if not defined PROJECT_OUTPUT_ROOT                  call :CANONICAL_PATH PROJECT_OUTPUT_ROOT                  "%%CONTOOLS_PROJECT_ROOT%%/_out"
-if not defined PROJECT_LOG_ROOT                     call :CANONICAL_PATH PROJECT_LOG_ROOT                     "%%CONTOOLS_PROJECT_ROOT%%/.log"
+if not defined PROJECT_OUTPUT_ROOT                  call "%%~dp0canonical_path.bat" PROJECT_OUTPUT_ROOT                  "%%CONTOOLS_PROJECT_ROOT%%/_out"
+if not defined PROJECT_LOG_ROOT                     call "%%~dp0canonical_path.bat" PROJECT_LOG_ROOT                     "%%CONTOOLS_PROJECT_ROOT%%/.log"
 
-if not defined CONTOOLS_PROJECT_INPUT_CONFIG_ROOT   call :CANONICAL_PATH CONTOOLS_PROJECT_INPUT_CONFIG_ROOT   "%%CONTOOLS_PROJECT_ROOT%%/_config"
-if not defined CONTOOLS_PROJECT_OUTPUT_CONFIG_ROOT  call :CANONICAL_PATH CONTOOLS_PROJECT_OUTPUT_CONFIG_ROOT  "%%PROJECT_OUTPUT_ROOT%%/config/contools"
+if not defined CONTOOLS_PROJECT_INPUT_CONFIG_ROOT   call "%%~dp0canonical_path.bat" CONTOOLS_PROJECT_INPUT_CONFIG_ROOT   "%%CONTOOLS_PROJECT_ROOT%%/_config"
+if not defined CONTOOLS_PROJECT_OUTPUT_CONFIG_ROOT  call "%%~dp0canonical_path.bat" CONTOOLS_PROJECT_OUTPUT_CONFIG_ROOT  "%%PROJECT_OUTPUT_ROOT%%/config/contools"
 
-if not defined CONTOOLS_ROOT                        call :CANONICAL_PATH CONTOOLS_ROOT                        "%%CONTOOLS_PROJECT_ROOT%%/Scripts/Tools"
+if not defined CONTOOLS_ROOT                        call "%%~dp0canonical_path.bat" CONTOOLS_ROOT                        "%%CONTOOLS_PROJECT_ROOT%%/Scripts/Tools"
 
 if not exist "%CONTOOLS_PROJECT_OUTPUT_CONFIG_ROOT%\" ( mkdir "%CONTOOLS_PROJECT_OUTPUT_CONFIG_ROOT%" || exit /b 10 )
 
@@ -38,14 +38,4 @@ if not exist "%PROJECT_LOG_ROOT%\" ( mkdir "%PROJECT_LOG_ROOT%" || exit /b 12 )
 
 if defined CHCP call "%%CONTOOLS_ROOT%%/std/chcp.bat" %%CHCP%%
 
-exit /b 0
-
-:CANONICAL_PATH
-setlocal DISABLEDELAYEDEXPANSION
-for /F "eol= tokens=* delims=" %%i in ("%~2\.") do set "RETURN_VALUE=%%~fi"
-rem set "RETURN_VALUE=%RETURN_VALUE:\=/%"
-(
-  endlocal
-  set "%~1=%RETURN_VALUE%"
-)
 exit /b 0
