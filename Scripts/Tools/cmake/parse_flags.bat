92c6603aaf1243c1b3ccd2eb463ccc7287cf94bb
andry81 <andry@inbox.ru>
2022-03-01 04:10:34 +0000

    2022.03.01: * new: Scripts/Tools/cmake: added `parse_flags.bat` script to partially parse cmake flags like `-T` and `-A` as a part of another script
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@785 699b1267-f50e-4533-8e1b-323e5631ef88

---
+++ Scripts/Tools/cmake/parse_flags.bat
@@ -0,0 +1,32 @@
+
+:FLAGS_LOOP
+
+rem flags always at first
+set "__?FLAG=%~1"
+
+if defined __?FLAG ^
+if not "%__?FLAG:~0,1%" == "-" set "__?FLAG="
+
+if defined __?FLAG (
+  if "%__?FLAG%" == "-T" (
+    set "CMAKE_GENERATOR_TOOLSET=%~2"
+    shift
+    set /A __?FLAG_SHIFT+=1
+  ) else if "%__?FLAG%" == "-A" (
+    set "CMAKE_GENERATOR_PLATFORM=%~2"
+    shift
+    set /A __?FLAG_SHIFT+=1
+  ) else if "%__?FLAG%" == "-I" (
+    set "CMAKE_GENERATOR_INSTANCE=%~2"
+    shift
+    set /A __?FLAG_SHIFT+=1
+  ) else (
+    exit /b 0
+  )
+
+  shift
+  set /A __?FLAG_SHIFT+=1
+
+  rem read until no flags
+  goto FLAGS_LOOP
+)
