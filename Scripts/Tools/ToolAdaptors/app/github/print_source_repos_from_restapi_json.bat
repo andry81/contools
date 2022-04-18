f84b3d19ce37b3cc5fbdacb3e8d6e01fe9e47882
andry81 <andry@inbox.ru>
2022-04-18 03:47:58 +0000

    2022.04.18: * new: Scripts/Tools/ToolAdaptors/app/github: `print_*_repos_from_restapi_json.bat` print script to print `html_url` only parameter from restapi json file with repositories list
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@808 699b1267-f50e-4533-8e1b-323e5631ef88

---
+++ Scripts/Tools/ToolAdaptors/app/github/print_source_repos_from_restapi_json.bat
@@ -0,0 +1,75 @@
+@echo off
+
+setlocal
+
+call "%%~dp0__init__\__init__.bat" || exit /b
+
+if %IMPL_MODE%0 NEQ 0 goto IMPL
+
+call "%%CONTOOLS_PROJECT_ROOT%%/__init__/declare_builtins.bat" %%0 %%*
+
+for %%i in (CONTOOLS_ROOT CONTOOLS_UTILITIES_BIN_ROOT) do (
+  if not defined %%i (
+    echo.%~nx0: error: `%%i` variable is not defined.
+    exit /b 255
+  ) >&2
+)
+
+call "%%CONTOOLS_ROOT%%/build/init_project_log.bat" "%%?~n0%%" || exit /b
+
+set "INIT_VARS_FILE=%PROJECT_LOG_DIR%\init.vars"
+
+rem register all environment variables
+set 2>nul > "%INIT_VARS_FILE%"
+
+"%CONTOOLS_UTILITIES_BIN_ROOT%/contools/callf.exe" ^
+  /ret-child-exit /pause-on-exit /tee-stdout "%PROJECT_LOG_FILE%" /tee-stderr-dup 1 ^
+  /v IMPL_MODE 1 /v INIT_VARS_FILE "%INIT_VARS_FILE%" ^
+  /ra "%%" "%%?01%%" /v "?01" "%%" ^
+  "${COMSPEC}" "/c \"@\"${?~f0}\" {*}\"" %* || exit /b
+
+exit /b 0
+
+:IMPL
+
+rem load initialization environment variables
+for /F "usebackq eol=# tokens=1,* delims==" %%i in ("%INIT_VARS_FILE%") do set "%%i=%%j"
+
+call "%%CONTOOLS_ROOT%%/std/allocate_temp_dir.bat" . "%%?~n0%%" || (
+  echo.%?~nx0%: error: could not allocate temporary directory: "%SCRIPT_TEMP_CURRENT_DIR%"
+  set LASTERROR=255
+  goto FREE_TEMP_DIR
+) >&2
+
+call :MAIN %%*
+set LASTERROR=%ERRORLEVEL%
+
+:FREE_TEMP_DIR
+rem cleanup temporary files
+call "%%CONTOOLS_ROOT%%/std/free_temp_dir.bat"
+
+exit /b %LASTERROR%
+
+:MAIN
+set "JSON_FILE=%~1"
+
+if not defined JSON_FILE (
+  echo.%?~n0%: error: JSON_FILE is not defined.
+  exit /b 255
+) >&2
+
+if not exist "%JSON_FILE%" (
+  echo.%?~n0%: error: JSON_FILE is not found: "%JSON_FILE%".
+  exit /b 255
+) >&2
+
+"%JQ_EXECUTABLE%" -c -r ".[] | select(.fork == false).html_url" "%JSON_FILE%" || exit /b 255
+
+exit /b 0
+
+:CMD
+echo.^>%*
+(
+  %*
+)
+exit /b
