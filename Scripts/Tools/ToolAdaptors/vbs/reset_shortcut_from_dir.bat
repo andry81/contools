822ff63486fc8beb7c824925abe1fbad2d78dcfe
andry81 <andry@inbox.ru>
2022-06-20 01:54:34 +0000

    2022.06.20: * new: Scripts/Tools/ToolAdaptors/vbs: added `reset_shortcut_from_dir.bat` script to reset shortcut by read/write it (does shortcut file shrink and cleanup)
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@825 699b1267-f50e-4533-8e1b-323e5631ef88

---
+++ Scripts/Tools/ToolAdaptors/vbs/reset_shortcut_from_dir.bat
@@ -0,0 +1,111 @@
+@echo off
+
+setlocal
+
+call "%%~dp0__init__\__init__.bat" || exit /b
+
+if %IMPL_MODE%0 NEQ 0 goto IMPL
+
+call "%%CONTOOLS_PROJECT_ROOT%%/__init__/declare_builtins.bat" %%0 %%*
+
+for %%i in (CONTOOLS_ROOT CONTOOLS_UTILITIES_BIN_ROOT) do (
+  if not defined %%i (
+    echo.%~nx0: error: `%%i` variable is not defined.
+    exit /b 255
+  ) >&2
+)
+
+call "%%CONTOOLS_ROOT%%/build/init_project_log.bat" "%%?~n0%%" || exit /b
+
+set "INIT_VARS_FILE=%PROJECT_LOG_DIR%\init.vars"
+
+rem register all environment variables
+set 2>nul > "%INIT_VARS_FILE%"
+
+"%CONTOOLS_UTILITIES_BIN_ROOT%/contools/callf.exe" ^
+  /ret-child-exit /pause-on-exit /tee-stdout "%PROJECT_LOG_FILE%" /tee-stderr-dup 1 ^
+  /v IMPL_MODE 1 /v INIT_VARS_FILE "%INIT_VARS_FILE%" ^
+  /ra "%%" "%%?01%%" /v "?01" "%%" ^
+  "${COMSPEC}" "/c \"@\"${?~f0}\" {*}\"" %* || exit /b
+
+exit /b 0
+
+:IMPL
+rem script flags
+set RESTORE_LOCALE=0
+set "FLAG_CHCP="
+
+:FLAGS_LOOP
+
+rem flags always at first
+set "FLAG=%~1"
+
+if defined FLAG ^
+if not "%FLAG:~0,1%" == "-" set "FLAG="
+
+if defined FLAG (
+  if "%FLAG%" == "-chcp" (
+    set "FLAG_CHCP=%~2"
+    shift
+  ) else (
+    echo.%?~nx0%: error: invalid flag: %FLAG%
+    exit /b -255
+  ) >&2
+
+  shift
+
+  rem read until no flags
+  goto FLAGS_LOOP
+)
+
+rem load initialization environment variables
+for /F "usebackq eol=# tokens=1,* delims==" %%i in ("%INIT_VARS_FILE%") do set "%%i=%%j"
+
+call "%%CONTOOLS_ROOT%%/std/allocate_temp_dir.bat" . "%%?~n0%%" || (
+  echo.%?~nx0%: error: could not allocate temporary directory: "%SCRIPT_TEMP_CURRENT_DIR%"
+  set LASTERROR=255
+  goto FREE_TEMP_DIR
+) >&2
+
+if defined FLAG_CHCP (
+  call "%%CONTOOLS_ROOT%%/std/chcp.bat" "%%FLAG_CHCP%%"
+  set RESTORE_LOCALE=1
+)
+
+call :MAIN %%1 %%2 %%3 %%4 %%5 %%6 %%7 %%8 %%9
+set LASTERROR=%ERRORLEVEL%
+
+rem restore locale
+if %RESTORE_LOCALE% NEQ 0 call "%%CONTOOLS_ROOT%%/std/restorecp.bat"
+
+:FREE_TEMP_DIR
+rem cleanup temporary files
+call "%%CONTOOLS_ROOT%%/std/free_temp_dir.bat"
+
+exit /b %LASTERROR%
+
+:MAIN
+set "LINKS_DIR=%~1"
+
+if defined LINKS_DIR if exist "%LINKS_DIR%\" goto LINKS_DIR_EXIST
+
+(
+  echo.%~nx0: error: LINKS_DIR does not exist: `%LINKS_DIR%`.
+  exit /b 255
+) >&2
+
+:LINKS_DIR_EXIST
+
+for /F "eol= tokens=* delims=" %%i in ("%LINKS_DIR%\.") do set "LINKS_DIR=%%~fi"
+
+for /F "usebackq eol= tokens=* delims=" %%i in (`dir /A:-D /B /S "%LINKS_DIR%\*.lnk" 2^>nul`) do (
+  set "LINK_FILE_PATH=%%i"
+  call :UPDATE_LINK
+)
+
+exit /b 0
+
+:UPDATE_LINK
+echo."%LINK_FILE_PATH%"
+
+"%SystemRoot%\System32\cscript.exe" //Nologo "%CONTOOLS_TOOL_ADAPTORS_ROOT%/vbs/update_shortcut.vbs" -- "%LINK_FILE_PATH%"
