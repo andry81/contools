1c1ad042a6c7011a3413dafc0eb08f637ad4d2df
andry81 <andry@inbox.ru>
2022-08-31 18:47:03 +0000

    2022.08.31: * fixed: Scripts/Tools/ToolAdaptors/app/github: `-from-cmd` parameter usage fixup, now `*` is used as `all` or `any`
    git-svn-id: svn+ssh://svn.code.sf.net/p/contools/contools/trunk@840 699b1267-f50e-4533-8e1b-323e5631ef88

--- Scripts/Tools/ToolAdaptors/app/github/.impl/update_skip_state.bat
+++ Scripts/Tools/ToolAdaptors/app/github/.impl/update_skip_state.bat
@@ -38,17 +38,21 @@ set "MATCH_PARAM0="
 set "MATCH_PARAM1="
 
 if defined FLAG_FROM_CMD_PARAM0 (
-  if defined PARAM0 (
+  if "%FLAG_FROM_CMD_PARAM0%" == "*" (
+    set MATCH_PARAM0=1
+  ) else if defined PARAM0 (
     if "%FLAG_FROM_CMD_PARAM0%" == "%PARAM0%" set MATCH_PARAM0=1
-  ) else set MATCH_PARAM0=1
+  )
 ) else if not defined PARAM0 set MATCH_PARAM0=1
 
 if not defined MATCH_PARAM0 exit /b 255
 
 if defined FLAG_FROM_CMD_PARAM1 (
-  if defined PARAM1 (
+  if "%FLAG_FROM_CMD_PARAM1%" == "*" (
+    set MATCH_PARAM1=1
+  ) else if defined PARAM1 (
     if "%FLAG_FROM_CMD_PARAM1%" == "%PARAM1%" set MATCH_PARAM1=1
-  ) else set MATCH_PARAM1=1
+  )
 ) else if not defined PARAM1 set MATCH_PARAM1=1
 
 if not defined MATCH_PARAM1 exit /b 255
