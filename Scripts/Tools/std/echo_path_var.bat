b3d4ed1ddc58ab5b7eb8e5009b471171e65baa9e
andry81 <andry@inbox.ru>
2023-04-06 16:23:53 +0300

    2023.04.06: * fixed: Scripts/Tools/std/echo_path_var.bat: `*`, `?` character parse workaround (left the globbing feature as is, use `^*` and `^?` instead to bypass the globbing) * fixed: Scripts/Tools/std/echo_*var.bat: unexisted variable returns code `255`, but empty one returns `1`

--- Scripts/Tools/std/echo_path_var.bat
+++ Scripts/Tools/std/echo_path_var.bat
@@ -12,13 +12,14 @@ if not defined __?PREFIX__ set "__?PREFIX__=%~2"
 if not defined __?SUFFIX__ set "__?SUFFIX__=%~3"
 
 if not defined __?VAR__ exit /b 255
+if not defined %__?VAR__% exit /b 1
 
 rem Escape specific separator characters by sequence of `$NN` characters:
 rem  1. `?` and `*` - globbing characters in the `for %%i in (...)` expression
 rem  2. `,`, ` `    - separator characters in the `for %%i in (...)` expression
 setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!%__?VAR__%:$=$00!") do ( endlocal & set "%__?VAR__%=%%i" )
 setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!%__?VAR__%:^*=$01!") do ( endlocal & set "%__?VAR__%=%%i" )
-setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!%__?VAR__%:?=$02!") do ( endlocal & set "%__?VAR__%=%%i" )
+setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!%__?VAR__%:^?=$02!") do ( endlocal & set "%__?VAR__%=%%i" )
 setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!%__?VAR__%: =$03!") do ( endlocal & set "%__?VAR__%=%%i" )
 rem escape tabulation character
 setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!%__?VAR__%:	=$04!") do ( endlocal & set "%__?VAR__%=%%i" )
@@ -29,8 +30,8 @@ setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!%__?
   for %%j in (%%i) do (
     set "__?LINE__=%%j"
     rem unescape
-    setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!__?LINE__:$01=*!") do ( endlocal & set "__?LINE__=%%i" )
-    setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!__?LINE__:$02=?!") do ( endlocal & set "__?LINE__=%%i" )
+    setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!__?LINE__:$01=^*!") do ( endlocal & set "__?LINE__=%%i" )
+    setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!__?LINE__:$02=^?!") do ( endlocal & set "__?LINE__=%%i" )
     setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!__?LINE__:$03= !") do ( endlocal & set "__?LINE__=%%i" )
     setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!__?LINE__:$04=	!") do ( endlocal & set "__?LINE__=%%i" )
     setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!__?LINE__:$05=,!") do ( endlocal & set "__?LINE__=%%i" )
--- Scripts/Tools/std/echo_var.bat
+++ Scripts/Tools/std/echo_var.bat
@@ -10,6 +10,7 @@ if not defined __?PREFIX__ set "__?PREFIX__=%~2"
 if not defined __?SUFFIX__ set "__?SUFFIX__=%~3"
 
 if not defined __?VAR__ exit /b 255
+if not defined %__?VAR__% exit /b 1
 
 rem safe echo
 setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!__?PREFIX__!!%__?VAR__%!!__?SUFFIX__!") do ( endlocal & echo.%%i)
