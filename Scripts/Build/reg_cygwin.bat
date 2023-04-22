972e938907aee22113c66424395f1fb9cdb9d46b
andry81 <andry@inbox.ru>
2023-04-22 17:48:35 +0300

    2023.04.22: * refactor: Utilities/bin/thomaspolaert: Thomas Polaert cecho utility is moved to external repository

--- Scripts/Build/reg_cygwin.bat
+++ Scripts/Build/reg_cygwin.bat
@@ -334,8 +334,8 @@ rem Environment will be restored automatically here
 exit /b
 
 :cecho
-if exist "%CONTOOLS_UTILITIES_BIN_ROOT%/thomaspolaert/cecho.exe" (
-  "%CONTOOLS_UTILITIES_BIN_ROOT%/thomaspolaert/cecho.exe" %*{#}{\n}
+if exist "%CONTOOLS_TPOLAERT_CECHO_ROOT%/cecho.exe" (
+  "%CONTOOLS_TPOLAERT_CECHO_ROOT%/cecho.exe" %*{#}{\n}
 ) else (
   echo.%*
 )
