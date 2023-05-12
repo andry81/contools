bd1462392ab4855b99b16ed1fe43b9daa75a722c
andry81 <andry@inbox.ru>
2023-05-12 08:06:57 +0300

    2023.05.12: * new: Scripts/Tools/cleanup: cleanup scripts * new: Scripts/Tools/exec/exec_callf_prefix.bat: script to execute callf as a prefix and run cleanup scripts on exit

---
+++ Scripts/Tools/exec/exec_callf_prefix.bat
@@ -0,0 +1,67 @@
+@echo off
+
+setlocal
+
+rem script flags
+set "CALLF_BARE_FLAGS="
+
+:FLAGS_LOOP
+
+rem flags always at first
+set "FLAG=%~1"
+
+if defined FLAG ^
+if not "%FLAG:~0,1%" == "-" set "FLAG="
+
+if defined FLAG (
+  if "%FLAG%" == "-init_vars_file" (
+    set CALLF_BARE_FLAGS=%CALLF_BARE_FLAGS% /v INIT_VARS_FILE "%INIT_VARS_FILE%"
+  ) else if "%FLAG%" == "-X" (
+    set CALLF_BARE_FLAGS=%CALLF_BARE_FLAGS% %2
+    shift
+  ) else if "%FLAG%" == "--" (
+    shift
+    set "FLAG="
+    goto FLAGS_LOOP_END
+  ) else (
+    set "FLAG="
+    goto FLAGS_LOOP_END
+  )
+
+  shift
+
+  rem read until no flags
+  goto FLAGS_LOOP
+)
+
+:FLAGS_LOOP_END
+
+if not defined COMSPECLNK set "COMSPECLNK=%COMSPEC%"
+
+rem variables escaping
+set "?~f0=%?~f0:{=\{%"
+set "COMSPECLNK=%COMSPECLNK:{=\{%"
+
+(
+  endlocal
+  "%CONTOOLS_UTILITIES_BIN_ROOT%/contools/callf.exe"%CALLF_BARE_FLAGS% ^
+    /ret-child-exit /tee-stdout "%PROJECT_LOG_FILE%" /tee-stderr-dup 1 ^
+    /no-expand-env /no-subst-pos-vars ^
+    /v IMPL_MODE 1 ^
+    /ra "%%" "%%?01%%" /v "?01" "%%" ^
+    "%COMSPECLNK%" "/c \"@\"%?~f0%\" {*}\"" %*
+)
+
+setlocal
+
+set LASTERROR=%ERRORLEVEL%
+
+if %NEST_LVL%0 EQU 0 (
+  call "%%CONTOOLS_ROOT%%/cleanup/cleanup_log.bat"
+  call "%%CONTOOLS_ROOT%%/cleanup/cleanup_init_vars.bat"
+)
+
+(
+  endlocal
+  exit /b %LASTERROR%
+)
