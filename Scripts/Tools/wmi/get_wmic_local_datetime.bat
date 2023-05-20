02a3b7227c97a33f1b05f02038364233153e62a9
andry81 <andry@inbox.ru>
2023-05-20 03:29:51 +0300

    2023.05.20: * changed: Scripts/Tools/wmi: minor details

--- Scripts/Tools/wmi/get_wmic_local_datetime.bat
+++ Scripts/Tools/wmi/get_wmic_local_datetime.bat
@@ -7,6 +7,18 @@ rem CAUTION:
 rem   In Windowx XP an elevated call under data protection flag will block the wmic tool!
 rem
 
+rem CAUTION:
+rem   The `wmic` executable has an issue with different output codepage:
+rem
+rem     https://stackoverflow.com/questions/55310573/wmic-command-in-batch-outputting-non-utf-8-text-files/55312341#55312341
+rem
+rem   The encoding of wmic's output depends on where the output is being sent
+rem     * If you send the output to a disk file by using a redirection operator
+rem       or the /output switch, wmic will use UCS-2 LE BOM
+rem     * If you send the output to the console or to a pipe, wmic will use OEM
+rem       codepage
+rem
+
 rem Drop last error level
 call;
 
