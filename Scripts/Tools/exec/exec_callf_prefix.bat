9d0481f4b93acefc06c9e15d34918b92fc985f29
andry81 <andry@inbox.ru>
2023-06-25 18:56:07 +0300

    2023.06.25: * new: Scripts/Tools/exec/exec_terminal_prefix.bat: moved from `tacklebar` project * changed: Scripts/Tools/exec/exec_callf_prefix.bat: added `-elevate` option to execute under Administrator privileges with single console * changed: Scripts/Tools/exec/exec_terminal_prefix.bat: merge with `exec_callf_prefix.bat` script * changed: Scripts/Tools/exec: exec_callf_prefix.bat, exec_terminal_prefix.bat: `FLAG_SHIFT` does not reset if already defined

--- Scripts/Tools/exec/exec_callf_prefix.bat
+++ Scripts/Tools/exec/exec_callf_prefix.bat
@@ -3,8 +3,10 @@
 setlocal
 
 rem script flags
-set FLAG_SHIFT=0
+if not defined FLAG_SHIFT set FLAG_SHIFT=0
+set FLAG_ELEVATED=0
 set "CALLF_BARE_FLAGS="
+set "CALLF_PROMOTE_PARENT_FLAGS="
 
 :FLAGS_LOOP
 
@@ -15,10 +17,22 @@ if defined FLAG ^
 if not "%FLAG:~0,1%" == "-" set "FLAG="
 
 if defined FLAG (
-  if "%FLAG%" == "-init_vars_file" (
+  if "%FLAG%" == "-elevate" (
+    set CALLF_BARE_FLAGS=%CALLF_BARE_FLAGS% ^
+/promote{ /load-parent-proc-init-env-vars /ret-child-exit } /promote-parent{%CALLF_PROMOTE_PARENT_FLAGS% /tee-stdout "%PROJECT_LOG_FILE%" /tee-stderr-dup 1 } ^
+/elevate{ /no-window /create-inbound-server-pipe-to-stdout "%~2_stdout_{pid}" /create-inbound-server-pipe-to-stderr "%~2_stderr_{pid}" ^
+}{ /attach-parent-console /reopen-stdout-as-client-pipe "%~2_stdout_{ppid}" /reopen-stderr-as-client-pipe "%~2_stderr_{ppid}" }
+    set FLAG_ELEVATED=1
+    shift
+    set /A FLAG_SHIFT+=1
+  ) else if "%FLAG%" == "-init_vars_file" (
     set CALLF_BARE_FLAGS=%CALLF_BARE_FLAGS% /v INIT_VARS_FILE "%INIT_VARS_FILE%"
   ) else if "%FLAG%" == "-X" (
     set CALLF_BARE_FLAGS=%CALLF_BARE_FLAGS% %2
+    shift
+    set /A FLAG_SHIFT+=1
+  ) else if "%FLAG%" == "-Y" (
+    set CALLF_PROMOTE_PARENT_FLAGS=%CALLF_PROMOTE_PARENT_FLAGS% %2
     shift
     set /A FLAG_SHIFT+=1
   ) else if "%FLAG%" == "--" (
@@ -40,6 +54,13 @@ if defined FLAG (
 
 :FLAGS_LOOP_END
 
+if %FLAG_ELEVATED% NEQ 0 goto SKIP_REGULAR_SETUP
+
+set CALLF_BARE_FLAGS=%CALLF_BARE_FLAGS% ^
+/ret-child-exit /tee-stdout "%PROJECT_LOG_FILE%" /tee-stderr-dup 1
+
+:SKIP_REGULAR_SETUP
+
 if not defined COMSPECLNK set "COMSPECLNK=%COMSPEC%"
 
 rem variables escaping
@@ -49,7 +70,6 @@ set "COMSPECLNK=%COMSPECLNK:{=\{%"
 (
   endlocal
   "%CONTOOLS_UTILITIES_BIN_ROOT%/contools/callf.exe"%CALLF_BARE_FLAGS% ^
-    /ret-child-exit /tee-stdout "%PROJECT_LOG_FILE%" /tee-stderr-dup 1 ^
     /no-expand-env /no-subst-pos-vars ^
     /v IMPL_MODE 1 ^
     /ra "%%" "%%?01%%" /v "?01" "%%" ^
---
+++ Scripts/Tools/exec/exec_terminal_prefix.bat
@@ -0,0 +1,136 @@
+@echo off
+
+setlocal
+
+set "?09=/"
+
+rem script flags
+if not defined FLAG_SHIFT set FLAG_SHIFT=0
+set "CALLF_BARE_FLAGS="
+
+:FLAGS_LOOP
+
+rem flags always at first
+set "FLAG=%~1"
+
+if defined FLAG ^
+if not "%FLAG:~0,1%" == "-" set "FLAG="
+
+if defined FLAG (
+  if "%FLAG%" == "-enable_msys_slash_escape" (
+    if %USE_MINTTY%0 NEQ 0 if %USE_MINTTY_ROOT_AS_MSYS_ROOT%0 NEQ 0 set "?09=//"
+  ) else if "%FLAG%" == "-log-stdin" (
+    set CALLF_BARE_FLAGS=%CALLF_BARE_FLAGS% %?09%tee-stdin "%PROJECT_LOG_FILE%" %?09%pipe-stdin-to-child-stdin
+  ) else if "%FLAG%" == "-log-conout" (
+    set CALLF_BARE_FLAGS=%CALLF_BARE_FLAGS% %?09%tee-stdout "%PROJECT_LOG_FILE%" %?09%tee-stderr-dup 1
+  ) else if "%FLAG%" == "-X" (
+    set CALLF_BARE_FLAGS=%CALLF_BARE_FLAGS% %2
+    shift
+    set /A FLAG_SHIFT+=1
+  ) else if "%FLAG%" == "--" (
+    shift
+    set "FLAG="
+    set /A FLAG_SHIFT+=1
+    goto FLAGS_LOOP_END
+  ) else (
+    set "FLAG="
+    goto FLAGS_LOOP_END
+  )
+
+  shift
+  set /A FLAG_SHIFT+=1
+
+  rem read until no flags
+  goto FLAGS_LOOP
+)
+
+:FLAGS_LOOP_END
+
+
+if %FLAG_USE_X64%0 NEQ 0 set CALLF_BARE_FLAGS= %CALLF_BARE_FLAGS% %?09%disable-wow64-fs-redir
+
+if not defined COMSPECLNK set "COMSPECLNK=%COMSPEC%"
+
+rem variables escaping
+set "?~f0=%?~f0:{=\{%"
+set "COMSPECLNK=%COMSPECLNK:{=\{%"
+
+if %USE_MINTTY%0 EQU 0 goto SKIP_USE_MINTTY
+
+(
+  endlocal
+  start "" /I /B /WAIT %MINTTY_TERMINAL_PREFIX% -e "%CONTOOLS_UTILITIES_BIN_ROOT%/contools/callf.exe"%CALLF_BARE_FLAGS% ^
+    %?09%disable-ctrl-signals %?09%detach-inherited-console-on-wait %?09%ret-child-exit %?09%no-expand-env %?09%no-subst-pos-vars ^
+    %?09%v IMPL_MODE 1 %?09%v INIT_VARS_FILE "%INIT_VARS_FILE%" ^
+    %?09%ra "%%" "%%?01%%" %?09%v "?01" "%%" ^
+    %?09%shift-%FLAG_SHIFT% ^
+    "%COMSPECLNK%" "%?09%c \"@\"%?~f0%\" {*}\"" %*
+)
+
+setlocal
+
+set LASTERROR=%ERRORLEVEL%
+
+if %NEST_LVL% EQU 0 (
+  call "%%CONTOOLS_ROOT%%/cleanup/cleanup_log.bat"
+  call "%%CONTOOLS_ROOT%%/cleanup/cleanup_init_vars.bat"
+)
+
+exit /b %LASTERROR%
+
+:SKIP_USE_MINTTY
+
+if %USE_CONEMU%0 EQU 0 goto SKIP_USE_CONEMU
+
+if /i "%CONEMU_INTERACT_MODE%" == "attach" %CONEMU_CMDLINE_ATTACH_PREFIX%
+
+if /i not "%CONEMU_INTERACT_MODE%" == "run" goto SKIP_USE_CONEMU
+
+(
+  endlocal
+  %CONEMU_CMDLINE_RUN_PREFIX% "%CONTOOLS_UTILITIES_BIN_ROOT%/contools/callf.exe"%CALLF_BARE_FLAGS% ^
+    /load-parent-proc-init-env-vars /detach-inherited-console-on-wait ^
+    /disable-ctrl-signals /attach-parent-console /ret-child-exit /no-expand-env /no-subst-pos-vars ^
+    /v IMPL_MODE 1 /v INIT_VARS_FILE "%INIT_VARS_FILE%" ^
+    /ra "%%" "%%?01%%" /v "?01" "%%" ^
+    /shift-%FLAG_SHIFT% ^
+    "%COMSPECLNK%" "/c \"@\"%?~f0%\" {@}\"" -cur_console:n %*
+)
+
+setlocal
+
+set LASTERROR=%ERRORLEVEL%
+
+if %NEST_LVL% EQU 0 (
+  call "%%CONTOOLS_ROOT%%/cleanup/cleanup_log.bat"
+  call "%%CONTOOLS_ROOT%%/cleanup/cleanup_init_vars.bat"
+)
+
+exit /b %LASTERROR%
+
+:SKIP_USE_CONEMU
+
+(
+  endlocal
+  "%CONTOOLS_UTILITIES_BIN_ROOT%/contools/callf.exe"%CALLF_BARE_FLAGS% ^
+    /load-parent-proc-init-env-vars /detach-inherited-console-on-wait /wait-child-first-time-timeout 300 ^
+    /disable-ctrl-signals /attach-parent-console /ret-child-exit /no-expand-env /no-subst-pos-vars ^
+    /v IMPL_MODE 1 /v INIT_VARS_FILE "%INIT_VARS_FILE%" ^
+    /ra "%%" "%%?01%%" /v "?01" "%%" ^
+    /shift-%FLAG_SHIFT% ^
+    "%COMSPECLNK%" "/c \"@\"%?~f0%\" {*}\"" %*
+)
+
+setlocal
+
+set LASTERROR=%ERRORLEVEL%
+
+if %NEST_LVL% EQU 0 (
+  call "%%CONTOOLS_ROOT%%/cleanup/cleanup_log.bat"
+  call "%%CONTOOLS_ROOT%%/cleanup/cleanup_init_vars.bat"
+)
+
+(
+  endlocal
+  exit /b %LASTERROR%
+)
