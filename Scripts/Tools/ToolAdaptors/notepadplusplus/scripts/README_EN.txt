dac69bccfc2745a91fc61ca2dca5b3eac249f17a
andry81 <andry@inbox.ru>
2023-10-08 07:33:50 +0300

    2023.10.10: * new: Scripts/Tools/ToolAdaptors/notepadplusplus/scripts/tacklebar/libs/npplib.py: added `-z -append` command line options to append files from a list file to existing instance instead of open a new instance * new: Scripts/Tools/ToolAdaptors/notepadplusplus/scripts/tacklebar/libs/npplib.py: added `-z -restore_single_instance` command line options to restore instance window show state after files append * new: Scripts/Tools/ToolAdaptors/notepadplusplus/scripts/tacklebar/libs/npplib.py: added `-z --child_cmdline_len_limit -z <limit>` command line options to limit maximum child instance command line length to append files * new: Scripts/Tools/ToolAdaptors/notepadplusplus/scripts/tacklebar/libs/npplib.py: added `-z -no_exit_after_append` command line options to not exit the instance after files append * changed: Scripts/Tools/ToolAdaptors/notepadplusplus/scripts/README_EN.txt: readme update

--- Scripts/Tools/ToolAdaptors/notepadplusplus/scripts/README_EN.txt
+++ Scripts/Tools/ToolAdaptors/notepadplusplus/scripts/README_EN.txt
@@ -1,5 +1,5 @@
 * README_EN.txt
-* 2020.11.24
+* 2023.10.10
 * contools/notepadplusplus
 
 1. DESCRIPTION
@@ -34,6 +34,9 @@ Now each time when the Notepad++ starts it will call to `startup.py` script.
 -------------------------------------------------------------------------------
 3. SCRIPTS
 -------------------------------------------------------------------------------
+* `/tacklebar/libs/npplib.py` -
+
+  main library script.
 
 * `/tacklebar/reopen_all_files.py` -
 
@@ -63,10 +66,49 @@ All other scripts can be directly used from the Notepad++ Plugins menu.
 Examples of the Notepad++ extra command line:
 
 >
-notepad++.exe -nosession -multiInst -openSession -z -from_utf16 -z --open_from_file_list -z "<utf-16le-with-bom-paths-list-file>"
+notepad++.exe -nosession -multiInst -z -from_utf16 -z --open_from_file_list -z "<utf-16le-with-bom-paths-list-file>"
 
 >
-notepad++.exe -nosession -multiInst -openSession -z -from_utf16le -z --open_from_file_list -z "<utf-16le-without-bom-paths-list-file>"
+notepad++.exe -nosession -multiInst -z -from_utf16le -z --open_from_file_list -z "<utf-16le-without-bom-paths-list-file>"
+
+Additionsl command line arguments:
+
+`-z --open_short_path_if_gt_limit -z 258`
+
+  Translates each path into a short DOS path if greater than the limit.
+  Can workaround long path files open, which Notepad++ does not support.
+
+`-z -append`
+
+  Append mode.
+  Runs Notepad++ as a launcher instance to delegate files opening to child
+  Notepad++ process(es). If a Notepad++ instance already has been running, then
+  appends files to existing Notepad++ instance, otherwise opens a new window
+  instance. If files has appended to existing Notepad++ instance, then after
+  that the launcher does by default auto close.
+
+`-z -restore_single_instance`
+
+  Has meaning in the append mode, when the Notepad++ instance does auto close
+  at the end. If there were no other instances, then the Notepad++ does restore
+  the window show state (for example, unminimizes).
+  Useful in case when user want to hide the window blinking.
+
+`-z --child_cmdline_len_limit -z 4096`
+
+  Has meaning in the append mode.
+  Reduces maximum command line length limit which is 32767 as by default.
+  The launcher Notepad++ instance builds each command line until this limit and
+  only after runs it.
+  To append each file by a separate child Notepad++ process you can set it to
+  1.
+
+`-z -no_exit_after_append`
+
+  Do not auto close the launcher Notepad++ instance in case of append mode.
+  Useful to debug the launcher on Python errors from the console.
+
+For the rest options see the `npplib.py` script file.
 
 -------------------------------------------------------------------------------
 4. AUTHOR
--- Scripts/Tools/ToolAdaptors/notepadplusplus/scripts/tacklebar/libs/npplib.py
+++ Scripts/Tools/ToolAdaptors/notepadplusplus/scripts/tacklebar/libs/npplib.py
@@ -27,9 +27,11 @@ def clear_readonly_flag_from_all_files():
   active_file = notepad.getCurrentFilename()
 
   num_cleared = 0
+  file_index = len(all_files)
 
   for f in reversed(all_files):
-    print("  - " + f)
+    file_index -= 1
+    print('  [{}] {}'.format(file_index, f))
     notepad.activateFile(f[0])
     notepad.menuCommand(MENUCOMMAND.EDIT_CLEARREADONLY)
     num_cleared += 1
@@ -52,7 +54,7 @@ def reopen_all_files():
   num_reopened = 0
 
   for f in all_files:
-    print("  - " + f[0])
+    print('  [{}] {}'.format(num_reopened, f[0]))
     notepad.open(f[0])
     num_reopened += 1
 
@@ -65,6 +67,49 @@ def reopen_all_files():
   print()
   print('* Number of reopened paths: ' + str(num_reopened))
   print()
+
+def process_notepadpp_windows(restore_single_instance, out_params):
+  import ctypes
+  from ctypes import wintypes
+
+  class InParams:
+    current_pid = 0
+
+  user32 = ctypes.windll.user32
+  kernel32 = ctypes.windll.kernel32
+
+  WNDENUMPROC = ctypes.WINFUNCTYPE(wintypes.BOOL, wintypes.HWND, wintypes.LPARAM)
+  SW_RESTORE = wintypes.DWORD(9)
+
+  in_params = InParams()
+
+  in_params.current_pid = kernel32.GetCurrentProcessId()
+  process_id = wintypes.DWORD()
+
+  wnd_class_name_buf_size = 256
+  wnd_class_name_buf = ctypes.create_unicode_buffer(256)
+
+  def enum_notepadpp_windows(hwnd, lparam):
+    user32.GetClassNameW(hwnd, wnd_class_name_buf, wnd_class_name_buf_size)
+    #wnd_class_name_buf[255] = 0 # string terminator character in case of truncation
+    if u'Notepad++' in wnd_class_name_buf.value:
+      out_params.num_instances += 1
+      user32.GetWindowThreadProcessId(hwnd, ctypes.byref(process_id))
+      if process_id.value == in_params.current_pid:
+        out_params.notepadpp_hwnd = hwnd
+    return True # continue enumerating
+
+  user32.EnumWindows(WNDENUMPROC(enum_notepadpp_windows), 0)
+
+  if restore_single_instance:
+    print('process_notepadpp_windows:')
+    print('  - pid: ' + str(in_params.current_pid))
+    print('  - num instances: ' + str(out_params.num_instances))
+    if out_params.notepadpp_hwnd and not out_params.num_instances > 1: # single instance
+      print('  - instance restored')
+      user32.ShowWindow(out_params.notepadpp_hwnd, SW_RESTORE)
+    else:
+      print('  - instance NOT restored')
 
 def process_extra_command_line():
   import os, io, shlex
@@ -84,10 +129,19 @@ def process_extra_command_line():
   open_path_len_limit = 0 # open as is
   next_arg_is_open_path_limit = False
 
+  child_cmdline_len_limit = 32767 # use default limit - 32767
+  next_arg_is_child_cmdline_len_limit = False
+
   chdir_path = ''
   next_arg_is_chdir_path = False
 
+  do_append = False
   do_reopen_all_files = False
+  do_restore_single_instance = False
+
+  is_multi_instance = False
+
+  no_exit_after_append = False
 
   for arg in cmdline_list:
     if arg == '-z':
@@ -103,6 +157,12 @@ def process_extra_command_line():
       open_path_len_limit = max(int(arg), 255) # 255 is builtin minimum
       next_arg_is_open_path_limit = False
       print('--open_short_path_if_gt_limit ' + str(open_path_len_limit))
+    elif next_arg_is_child_cmdline_len_limit:
+      # 1 is builtin minimum, means: use one file to open per each child Notepad++ command line
+      # 32767 is builtin maximum for Win32 CreateProcess
+      child_cmdline_len_limit = min(max(int(arg), 1), 32767)
+      next_arg_is_child_cmdline_len_limit = False
+      print('--child_cmdline_len_limit ' + str(child_cmdline_len_limit))
     else:
       if arg == '-from_utf8':
         from_utf8 = True
@@ -116,20 +176,94 @@ def process_extra_command_line():
         next_arg_is_file_list_path = True
       elif arg == '--open_short_path_if_gt_limit':
         next_arg_is_open_path_limit = True
+      elif arg == '--child_cmdline_len_limit':
+        next_arg_is_child_cmdline_len_limit = True
       elif arg == '--chdir':
         next_arg_is_chdir_path = True
       elif arg == '-reopen_all_files':
         do_reopen_all_files = True
+      elif arg == '-restore_single_instance':
+        do_restore_single_instance = True
+      elif arg == '-append':
+        do_append = True
+      elif arg == '-multiInst':
+        is_multi_instance = True
+      elif arg == '-no_exit_after_append':
+        no_exit_after_append = True
 
-  num_opened = 0
+  # append and restore has meaning ONLY in multi-instance mode
+  if not is_multi_instance:
+    do_append = False
+    do_restore_single_instance = False
+  # restore has meaning ONLY in the minimized show state used in the append mode
+  elif not do_append:
+    do_restore_single_instance = False
+
+  num_processed_paths = 0
+  num_success_paths = 0
+
+  # True - open files in current Notepad++ instance
+  # False - open file in a child Notepad++ instance
+  do_open_inplace = True
 
   if chdir_path:
     os.chdir(chdir_path)
     print('--chdir: ' + chdir_path)
 
   if open_from_file_list_path:
-    if open_path_len_limit > 0:
+    if do_append or open_path_len_limit > 0:
       import ctypes
+
+    # construct child command line
+    if do_append:
+      class OutParams:
+        notepadpp_hwnd = None
+        num_instances = 0
+
+      out_params = OutParams()
+
+      # process Notepad++ instances as standalone windows
+      process_notepadpp_windows(do_restore_single_instance, out_params)
+
+      if out_params.notepadpp_hwnd and not out_params.num_instances > 1: # single instance
+        is_single_instance = True
+      else:
+        is_single_instance = False
+        do_open_inplace = False
+
+        # prepare child Notepad++ command line to pass all being opened files into it to append to existing Notepad++ instance (except this instance)
+        child_cmdline_prefix_list = list()
+        child_cmdline_file_list = list()
+
+        next_arg_ignore = False
+
+        # filter out all `-z` parameters to leave only Notepad++ builtin parameters except several specific
+        for arg in cmdline_list:
+          if next_arg_ignore:
+            next_arg_ignore = False
+            continue
+
+          if arg == '-z':
+            next_arg_ignore = True
+            continue
+
+          if next_arg_is_file_list_path:
+            open_from_file_list_path = str(arg)
+            next_arg_is_file_list_path = False
+          elif next_arg_is_chdir_path:
+            chdir_path = str(arg)
+            next_arg_is_chdir_path = False
+          elif next_arg_is_open_path_limit:
+            open_path_len_limit = max(int(arg), 255) # 255 is builtin minimum
+            next_arg_is_open_path_limit = False
+            print('--open_short_path_if_gt_limit ' + str(open_path_len_limit))
+          else:
+            if arg == '-nosession':
+              continue
+            if arg == '-multiInst':
+              continue
+
+            child_cmdline_prefix_list.append(arg)
 
     print('--open_from_file_list:')
 
@@ -164,6 +298,7 @@ def process_extra_command_line():
     # CAUTION:
     #   The `win32api.GetShortPathName` awaits ascii format and unicode variant is not implemented in Python 2.7.
     #   So we must use `ctype` module to directly call `GetShowrtPathNameW` instead.
+    #
     #   For details:
     #     https://mail.python.org/pipermail/python-win32/2006-May/004697.html
     #
@@ -181,19 +316,14 @@ def process_extra_command_line():
     #     >     using the default encoding (ascii). Splat.
     #     >
     #     >     Looks like you need a GetShortPathNameW() but it's not implemented.
-    #     >     Raise it as an issue on the pywin32 sourceforge bug register. Tell
-    #     >     Mark
-    #     >     I sent you :-)
-    #     >
-    #     >     It may be possible to fake up your default encoding to say cp1252 BUT
-    #     >     take the advice of anyone who screams "Don't do that!" and in any
-    #     >     case
-    #     >     this wouldn't help you with a Russian, Chinese, etc etc filename.
+    #     >     ...
     #     >
     #     >     Another thought: try using ctypes.
     #     >
 
     file_path_unc_prefix_utf16le = u'\\\\?\\'.encode('utf-16-le', errors = 'ignore') # a constant
+
+    kernel32 = ctypes.windll.kernel32
 
     for line in file_strings:
       file_path = line.strip()
@@ -201,33 +331,137 @@ def process_extra_command_line():
         file_path_decoded = file_path
         if recode_to_utf8:
           file_path = file_path_decoded.encode('utf-8', errors = 'ignore')
-        print("  - " + file_path)
+        print('  [{}] ({}) {}'.format(num_processed_paths, len(file_path), file_path))
 
         # 255 is builtin minimum
         if open_path_len_limit <= 255 or open_path_len_limit >= len(file_path):
-          notepad.open(file_path)
+          if do_open_inplace:
+            notepad.open(file_path)
+          else:
+            child_cmdline_file_list.append(file_path)
+
+          num_success_paths += 1
         else:
           file_path_unc_utf16le = file_path_unc_prefix_utf16le + file_path_decoded.encode('utf-16-le', errors = 'ignore')
-          file_path_buf = ctypes.create_unicode_buffer(len(file_path_unc_utf16le) + 1)
+          file_path_buf_size = len(file_path_unc_utf16le) + 1 # in characters
+          file_path_buf = ctypes.create_unicode_buffer(file_path_buf_size) # plus terminator character
 
           # ISSUE:
           #   Tests showed `ctypes.windll.kernel32.GetShortPathNameW` can return 0 time to time on a long path file for no reason.
           #
-          if ctypes.windll.kernel32.GetShortPathNameW(file_path_unc_utf16le, file_path_buf, ctypes.sizeof(file_path_buf)):
-            print("    - " + str(file_path_buf.value))
-            notepad.open(str(file_path_buf.value))
+          if kernel32.GetShortPathNameW(file_path_unc_utf16le, file_path_buf, file_path_buf_size):
+            file_path = str(file_path_buf.value)
+
+            print('    - ({}) {}'.format(len(file_path), file_path))
+
+            if do_open_inplace:
+              notepad.open(str(file_path_buf.value))
+            else:
+              child_cmdline_file_list.append(str(file_path_buf.value))
+
+            num_success_paths += 1
           else:
-            print("    - path may not opened: `ctypes.windll.kernel32.GetShortPathNameW` call is failed.")
+            print("    - path may be is not opened: `ctypes.windll.kernel32.GetShortPathNameW` call is failed.")
+
             # fall back to long path
-            notepad.open(file_path)
+            if do_open_inplace:
+              notepad.open(file_path)
+            # ISSUE:
+            #   We have to skip append into a child command line, because in that case Notepad++ does not open the entire command line
+            #   including not long file path arguments if at least one argument is a long file path!
+            #
+            #else:
+            #  child_cmdline_file_list.append(file_path)
 
           #file_path_buf = None
 
-        num_opened += 1
+        num_processed_paths += 1
 
   print()
-  print('* Number of opened paths: ' + str(num_opened))
+  print('* Number of success paths: {} of {}'.format(str(num_success_paths), str(num_processed_paths)))
   print()
+
+  if not do_open_inplace:
+    import sys
+
+    print('executing child subprocess:')
+
+    if len(child_cmdline_file_list):
+      import subprocess
+
+      # each command line must be not longer than a command line length limit
+      cmdline_len_max = child_cmdline_len_limit
+
+      # count accumulated command line length respective to white space characters
+      white_space_chars = ' \t'
+
+      child_subprocess_prefix_cmdline_list = [sys.argv[0]] + child_cmdline_prefix_list[1:]
+      child_subprocess_prefix_cmdline_len = 0
+
+      def cmdline_len(arg_list):
+        cmdline_len = 0
+        arg_index = 0
+
+        for arg in arg_list:
+          if len(arg):
+            arg_has_white_space_char = any(elem in arg for elem in white_space_chars)
+            cmdline_len += len(arg) + (2 if arg_has_white_space_char else 0)
+          else: # empty argument
+            cmdline_len += 2
+
+          if arg_index:
+            cmdline_len += 1 # space between arguments
+
+          arg_index += 1
+
+        return cmdline_len
+
+      child_subprocess_prefix_cmdline_len = cmdline_len(child_subprocess_prefix_cmdline_list)
+
+      while len(child_cmdline_file_list):
+        file_path = child_cmdline_file_list.pop(0)
+        file_path_list = [file_path]
+
+        child_subprocess_prev_cmdline_list = child_subprocess_prefix_cmdline_list + file_path_list
+        child_subprocess_prev_cmdline_len = child_subprocess_prefix_cmdline_len + 1 + cmdline_len(file_path_list)
+
+        if len(child_cmdline_file_list):
+          file_path = child_cmdline_file_list[0]
+
+          child_subprocess_next_cmdline_list = child_subprocess_prev_cmdline_list + [file_path]
+          child_subprocess_next_cmdline_len = child_subprocess_prev_cmdline_len + 1 + cmdline_len([file_path])
+
+          while cmdline_len_max >= child_subprocess_next_cmdline_len:
+            child_cmdline_file_list.pop(0)
+
+            file_path_list.append(file_path)
+
+            child_subprocess_prev_cmdline_list = child_subprocess_next_cmdline_list
+            child_subprocess_prev_cmdline_len = child_subprocess_next_cmdline_len
+
+            if len(child_cmdline_file_list):
+              file_path = child_cmdline_file_list[0]
+
+              child_subprocess_next_cmdline_list = child_subprocess_prev_cmdline_list + [file_path]
+              child_subprocess_next_cmdline_len = child_subprocess_prev_cmdline_len + 1 + cmdline_len([file_path])
+            else:
+              break
+
+        print('  - ' + str(child_subprocess_prefix_cmdline_list))
+        print('    command line length: ' + str(child_subprocess_prev_cmdline_len))
+
+        file_index = 0
+
+        for file_path in file_path_list:
+          print('    [{}] ({}) {}'.format(file_index, len(file_path), file_path))
+          file_index += 1
+
+        subprocess.call(child_subprocess_prev_cmdline_list, stdin = None, stdout = None, stderr = None, shell = False)
+    else:
+      print('  - None')
+
+    if not no_exit_after_append:
+      sys.exit(0)
 
   if do_reopen_all_files:
     reopen_all_files()
