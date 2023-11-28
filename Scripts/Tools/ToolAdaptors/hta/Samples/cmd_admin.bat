91589d378b1657fc84714dc73e9854a0baaf08e4
andry81 <andry@inbox.ru>
2023-11-28 19:34:59 +0300

    2023.11.28: * new: Scripts/Tools/ToolAdaptors/hta/Samples/cmd_admin.bat: example of a batch script to use `mshta` to self execute a batch script in elevated environment if started in not elevated environment

---
+++ Scripts/Tools/ToolAdaptors/hta/Samples/cmd_admin.bat
@@ -0,0 +1,24 @@
+@echo off
+
+rem Description:
+rem  Example of a batch script to use `mshta` to self execute a batch script in elevated environment if started in not elevated environment.
+
+setlocal
+
+if %IMPL_MODE%0 NEQ 0 goto IMPL
+net session >nul 2>&1 && goto IMPL
+
+:ELEVATE
+set IMPL=1
+rem CAUTION: ShellExecute does not wait a child process close!
+start /WAIT /B "" mshta vbscript:Close(CreateObject("Shell.Application").ShellExecute("%COMSPEC%","/c @call ""%~f0""","","runas",True))
+pause
+exit /b
+
+:IMPL
+net session >nul 2>&1 || (
+  echo.%~nx0: error: process must be elevated before continue.
+  exit /b 255
+) >&2
+
+pause
