1ea7ed04ea39fe2f20335132eb3d8ad2506db4ec
andry81 <andry@inbox.ru>
2024-01-18 21:22:00 +0400

    2024.01.18: * new: Scripts/Tools/filesys/read_path_props.bat: script wrapper to read a path property values

---
+++ Scripts/Tools/filesys/read_path_props.bat
@@ -0,0 +1,23 @@
+@echo off
+
+rem Description:
+rem   Script reads a path property values.
+rem   OS: Windows XP+
+
+rem drop return value
+set "RETURN_VALUE="
+
+rem Drop last error level
+call;
+
+setlocal
+
+call "%%~dp0__init__.bat" || exit /b
+
+rem CAUTION:
+rem   `for /F` does not return a command error code
+for /F "usebackq eol= tokens=* delims=" %%i in (`@"%%SystemRoot%%\System32\cscript.exe" //nologo "%%CONTOOLS_TOOL_ADAPTORS_ROOT%%/vbs/read_path_props.vbs" %*`) do set "RETURN_VALUE=%%i"
+
+if defined RETURN_VALUE ( endlocal & set "RETURN_VALUE=%RETURN_VALUE%" & exit /b 0 )
+
+exit /b 1
