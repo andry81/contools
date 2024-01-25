bd08de6a76f74c3243c83218e75757726d0339aa
andry81 <andry@inbox.ru>
2024-01-25 06:09:26 +0400

    2024.01.25: * changed: Scripts/Tools/encoding/prepend_bom_to_utf_file.bat: execution speed improvements

--- Scripts/Tools/encoding/prepend_bom_to_utf_file.bat
+++ Scripts/Tools/encoding/prepend_bom_to_utf_file.bat
@@ -3,8 +3,8 @@
 rem Author:   Andrey Dibrov (andry at inbox dot ru)
 
 rem Description:
-rem   Script prepends UTF BOM sequence to an input text file if file does not
-rem   have it yet.
+rem   Script prepends UTF BOM sequence to a text file if file does not
+rem   have it yet and output the result into another text file.
 
 setlocal
 
@@ -15,17 +15,16 @@ call "%%CONTOOLS_PROJECT_ROOT%%/__init__/declare_builtins.bat" %%0 %%* || exit /
 rem script flags
 set RESTORE_LOCALE=0
 
-call "%%CONTOOLS_ROOT%%/std/allocate_temp_dir.bat" . "%%?~n0%%"
+set "SCRIPT_TEMP_CURRENT_DIR=%TEMP%\%?~n0%.%RANDOM%-%RANDOM%"
 
 call :MAIN %%*
 set LASTERROR=%ERRORLEVEL%
 
+if exist "%SCRIPT_TEMP_CURRENT_DIR%\*" rmdir /S /Q "%SCRIPT_TEMP_CURRENT_DIR%" >nul 2>nul
+
 :EXIT_MAIN
 rem restore locale
 if %RESTORE_LOCALE% NEQ 0 call "%%CONTOOLS_ROOT%%/std/restorecp.bat"
-
-rem cleanup temporary files
-call "%%CONTOOLS_ROOT%%/std/free_temp_dir.bat"
 
 exit /b %LASTERROR%
 
@@ -59,30 +58,51 @@ if defined FLAG (
 set "INPUT_FILE_PATH=%~1"
 set "BOM_FILE_TOKEN=%~2"
 set "OUTPUT_FILE_PATH=%~3"
-set "OUTPUT_FILE_DIR=%~dp3"
 
-if not exist "%INPUT_FILE_PATH%" (
-  echo.%~nx0%: error: INPUT_FILE_PATH does not exist: "%INPUT_FILE_PATH%".
-  exit /b 1
+if not defined INPUT_FILE_PATH (
+  echo.%~nx0%: error: INPUT_FILE_PATH is not defined.
+  exit /b 255
+) >&2
+
+if not defined BOM_FILE_TOKEN (
+  echo.%~nx0%: error: BOM_FILE_TOKEN is not defined.
+  exit /b 255
+) >&2
+
+if not defined OUTPUT_FILE_PATH (
+  echo.%~nx0%: error: OUTPUT_FILE_PATH is not defined.
+  exit /b 255
 ) >&2
 
 set "BOM_FILE_PATH=%CONTOOLS_ROOT%/encoding/boms/%BOM_FILE_TOKEN%.bin"
 
+for /f "eol= tokens=* delims=" %%i in ("%INPUT_FILE_PATH%\.") do ( set "INPUT_FILE_PATH=%%~fi" )
+for /f "eol= tokens=* delims=" %%i in ("%BOM_FILE_PATH%\.") do ( set "BOM_FILE_PATH=%%~fi" )
+for /f "eol= tokens=* delims=" %%i in ("%OUTPUT_FILE_PATH%\.") do ( set "OUTPUT_FILE_DIR=%%~dpi" & set "OUTPUT_FILE_PATH=%%~fi" )
+
+if not exist "%INPUT_FILE_PATH%" (
+  echo.%~nx0%: error: INPUT_FILE_PATH does not exist: "%INPUT_FILE_PATH%".
+  exit /b 255
+) >&2
+
 if not exist "%BOM_FILE_PATH%" (
   echo.%~nx0%: error: BOM_FILE_TOKEN token file does not exist: "%BOM_FILE_PATH%".
-  exit /b 2
+  exit /b 255
 ) >&2
 
-if not exist "%OUTPUT_FILE_DIR%" (
+if not exist "%OUTPUT_FILE_DIR%\*" (
   echo.%~nx0%: error: OUTPUT_FILE_DIR does not exist: "%OUTPUT_FILE_DIR%".
-  exit /b 3
+  exit /b 255
 ) >&2
 
-set "INPUT_FILE_PATH=%~f1"
-set "OUTPUT_FILE_PATH=%~f3"
+if /i "%INPUT_FILE_PATH%" == "%OUTPUT_FILE_PATH%" (
+  echo.%~nx0%: error: INPUT_FILE_PATH and OUTPUT_FILE_PATH must be different: "%INPUT_FILE_PATH%".
+  exit /b 1
+) >&2
 
-set "INTERMEDIATE_FILE_TMP=%SCRIPT_TEMP_CURRENT_DIR%\intermediate_file.txt"
 set "INPUT_FILE_BOM_PREFIX_TMP=%SCRIPT_TEMP_CURRENT_DIR%\input_file_bom_prefix.bin"
+
+mkdir "%SCRIPT_TEMP_CURRENT_DIR%" >nul 2>nul
 
 if defined FLAG_CHCP (
   call "%%CONTOOLS_ROOT%%/std/chcp.bat" "%%FLAG_CHCP%%"
@@ -90,7 +110,15 @@ if defined FLAG_CHCP (
 ) else call "%%CONTOOLS_ROOT%%/std/getcp.bat"
 
 rem an UTF16LE file automatically checked for the BOM by the copy utility
-if /i "%BOM_FILE_TOKEN%" == "fffe" goto COPY_AS_TEXT_FILES
+if /i "%BOM_FILE_TOKEN%" == "fffe" (
+  rem copy as text files
+  copy "%BOM_FILE_PATH%" /A + "%INPUT_FILE_PATH%" /A "%OUTPUT_FILE_PATH%" /B /Y >nul 2>nul || (
+    echo.%~nx0%: error: could not copy to the output file (1^): "%OUTPUT_FILE_PATH%".
+    exit /b 2
+  ) >&2
+
+  exit /b
+)
 
 if not "%CURRENT_CP%" == "437" (
   rem to avoid an unicode code page
@@ -113,7 +141,7 @@ if defined INPUT_FILE_BOM_STR call set "INPUT_FILE_BOM_STR=%%INPUT_FILE_BOM_STR:
 
 if not defined INPUT_FILE_BOM_STR goto IGNORE_BOM_CHECK
 
-for /F "eol= tokens=* delims=" %%i in ("%INPUT_FILE_BOM_STR%") do (echo.|set /P __DUMMY__=%%i) > "%INPUT_FILE_BOM_PREFIX_TMP%"
+for /F "eol= tokens=* delims=" %%i in ("%INPUT_FILE_BOM_STR%") do set /P =%%i<nul > "%INPUT_FILE_BOM_PREFIX_TMP%"
 "%SystemRoot%\System32\fc.exe" "%INPUT_FILE_BOM_PREFIX_TMP%" "%BOM_FILE_PATH%" > nul
 if %ERRORLEVEL% NEQ 0 goto IGNORE_BOM_CHECK
 
@@ -122,12 +150,10 @@ if %RESTORE_LOCALE% GTR 1 (
   set /A RESTORE_LOCALE-=1
 )
 
-if /i not "%INPUT_FILE_PATH%" == "%OUTPUT_FILE_PATH%" (
-  call :COPY_FILE "%%INPUT_FILE_PATH%%" "%%OUTPUT_FILE_PATH%%" >nul 2>nul || (
-    echo.%~nx0%: error: could not copy to the output file: "%OUTPUT_FILE_PATH%".
-    exit /b 10
-  ) >&2
-)
+copy "%INPUT_FILE_PATH%" "%OUTPUT_FILE_PATH%" /B /Y >nul 2>nul || (
+  echo.%~nx0%: error: could not copy to the output file (2^): "%OUTPUT_FILE_PATH%".
+  exit /b 2
+) >&2
 
 exit /b 0
 
@@ -138,42 +164,10 @@ if %RESTORE_LOCALE% GTR 1 (
   set /A RESTORE_LOCALE-=1
 )
 
-:COPY_AS_BIN_FILES
-
-call :COPY_TWO_BIN_FILES "%%BOM_FILE_PATH%%" "%%INPUT_FILE_PATH%%" "%%INTERMEDIATE_FILE_TMP%%" >nul 2>nul || (
-  echo.%~nx0%: error: could not copy to the intermediate file: "%INTERMEDIATE_FILE_TMP%".
-  exit /b 20
-) >&2
-
-call :COPY_FILE "%%INTERMEDIATE_FILE_TMP%%" "%%OUTPUT_FILE_PATH%%" >nul 2>nul || (
-  echo.%~nx0%: error: could not copy to the output file: "%OUTPUT_FILE_PATH%".
-  exit /b 21
+rem copy as binary files
+copy "%BOM_FILE_PATH%" /B + "%INPUT_FILE_PATH%" /B "%OUTPUT_FILE_PATH%" /B /Y >nul 2>nul || (
+  echo.%~nx0%: error: could not copy to the output file (3^): "%OUTPUT_FILE_PATH%".
+  exit /b 2
 ) >&2
 
 exit /b
-
-:COPY_AS_TEXT_FILES
-
-call :COPY_TWO_TEXT_FILES "%%BOM_FILE_PATH%%" "%%INPUT_FILE_PATH%%" "%%INTERMEDIATE_FILE_TMP%%" >nul 2>nul || (
-  echo.%~nx0%: error: could not copy to the intermediate file: "%INTERMEDIATE_FILE_TMP%".
-  exit /b 30
-) >&2
-
-call :COPY_FILE "%%INTERMEDIATE_FILE_TMP%%" "%%OUTPUT_FILE_PATH%%" >nul 2>nul || (
-  echo.%~nx0%: error: could not copy to the output file: "%OUTPUT_FILE_PATH%".
-  exit /b 31
-) >&2
-
-exit /b
-
-:COPY_FILE
-copy "%~f1" "%~f2" /B /Y || exit /b
-exit /b 0
-
-:COPY_TWO_TEXT_FILES
-copy "%~f1" /A + "%~f2" /A "%~f3" /B /Y || exit /b
-exit /b 0
-
-:COPY_TWO_BIN_FILES
-copy "%~f1" /B + "%~f2" /B "%~f3" /B /Y || exit /b
-exit /b 0
