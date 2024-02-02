c0fe0d69189209dd9914d4497eeee2d5971e3b54
andry81 <andry@inbox.ru>
2024-02-02 18:23:05 +0300

    2024.02.02: * fixed: Scripts/Tools/ToolAdaptors/sh/detect_find.sh: missed trim of trailing line feed characters * fixed: Scripts/Tools/ToolAdaptors/sh/detect_find.sh: paths insensitive comparison

--- Scripts/Tools/ToolAdaptors/sh/detect_find.sh
+++ Scripts/Tools/ToolAdaptors/sh/detect_find.sh
@@ -2,10 +2,22 @@
 
 SHELL_FIND=find
 
-# detect `find.exe` in Windows behind `%SYSTEMROOT%\System32\find.exe`
+# NOTE:
+#   The `${path,,}` or `${path^^}` form has issues:
+#     1. Does not handle a unicode string case conversion correctly (unicode characters translation in words).
+#     2. Supported in Bash 4+.
+
+# detect `find.exe` in Windows behind `$SYSTEMROOT\System32\find.exe`
 if which where >/dev/null 2>&1; then
-  for path in `where find 2>/dev/null`; do
-    case "$path" in
+  local old_shopt="$(shopt -p nocasematch)" # read state before change
+  if [[ "$old_shopt" != 'shopt -s nocasematch' ]]; then
+    shopt -s nocasematch
+  else
+    unset old_shopt
+  fi
+
+  IFS=$'\r\n'; for path in `where find 2>/dev/null`; do # IFS - with trim trailing line feeds
+    case "$path" in # with case insensitive comparison
       "$SYSTEMROOT"\\*) ;;
       "$WINDIR"\\*) ;;
       *)
@@ -14,4 +26,8 @@ if which where >/dev/null 2>&1; then
         ;;
     esac
   done
+
+  if [[ -n "$old_shopt" ]]; then
+    eval $old_shopt
+  fi
 fi
