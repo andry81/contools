11a9f9da3cd7d9881e812f19cbce27e23334b1d6
andry81 <andry@inbox.ru>
2024-03-12 20:33:55 +0300

    2024.03.12: * changed: Scripts/Tools/std/trim_var.bat: reverted to previous implementation as ~x2 faster

--- Scripts/Tools/std/trim_var.bat
+++ Scripts/Tools/std/trim_var.bat
@@ -10,12 +10,22 @@ if not defined %~1 exit /b 0
 
 setlocal DISABLEDELAYEDEXPANSION
 
-setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!%~1!") do endlocal & set "RETURN_VALUE=%%i"
+rem Load and replace a value quote characters by the \x01 character.
+call set "RETURN_VALUE=%%%~1:"=%%"
 
 call "%%~dp0.trim_var/trim_var.trim_value_left.bat" || exit /b
 if not defined RETURN_VALUE endlocal & ( if "%~2" == "" ( set "%~1=" ) else set "%~2=" ) & exit /b 0
 call "%%~dp0.trim_var/trim_var.trim_value_right.bat" || exit /b
 if not defined RETURN_VALUE endlocal & ( if "%~2" == "" ( set "%~1=" ) else set "%~2=" ) & exit /b 0
 
-setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!RETURN_VALUE!") do endlocal & endlocal & if not "%~2" == "" ( set "%~2=%%i" ) else set "%~1=%%i"
+rem recode quote and exclamation characters
+set "__ESC__=^"
+set __QUOT__=^"
+set "__EXCL__=!"
+set "RETURN_VALUE=%RETURN_VALUE:!=!__EXCL__!%"
+set "RETURN_VALUE=%RETURN_VALUE:^=!__ESC__!%"
+set "RETURN_VALUE=%RETURN_VALUE:=!__QUOT__!%"
+
+rem safe set
+setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!RETURN_VALUE!") do for /F "eol= tokens=* delims=" %%j in ("%%i") do endlocal & endlocal & if not "%~2" == "" ( set "%~2=%%j" ) else set "%~1=%%j"
 exit /b 0
