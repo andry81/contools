532cfd98f29a679986dbc629dee97d9d5fa90483
andry81 <andry@inbox.ru>
2024-03-20 18:46:59 +0300

    2024.03.20: * new: Scripts/Tools/filesys/read_shortcut_target_path.bat: added `TARGET_PATH_STDOUT_FILE` and `TARGET_PATH_STDERR_FILE` variables usage to avoid files creation/deletion

--- Scripts/Tools/filesys/read_shortcut_target_path.bat
+++ Scripts/Tools/filesys/read_shortcut_target_path.bat
@@ -63,16 +63,22 @@ if defined FLAG (
 
 :FLAGS_LOOP_END
 
+set "TARGET_PATH_TEMP_STDOUT_FILE="
+set "TARGET_PATH_TEMP_STDERR_FILE="
+
 rem CAUTION:
 rem   We must use temporary file with BOM header to retain the Unicode encoding.
 rem
-set "TARGET_PATH_TEMP_STDOUT_FILE=%TEMP%\read_shortcut_target_path.stdout.%RANDOM%-%RANDOM%.txt"
-set "TARGET_PATH_TEMP_STDERR_FILE=%TEMP%\read_shortcut_target_path.stderr.%RANDOM%-%RANDOM%.txt"
+if not defined TARGET_PATH_STDOUT_FILE set "TARGET_PATH_TEMP_STDOUT_FILE=%TEMP%\read_shortcut_target_path.stdout.%RANDOM%-%RANDOM%.txt"
+if not defined TARGET_PATH_STDERR_FILE set "TARGET_PATH_TEMP_STDERR_FILE=%TEMP%\read_shortcut_target_path.stderr.%RANDOM%-%RANDOM%.txt"
+
+if not defined TARGET_PATH_STDOUT_FILE set "TARGET_PATH_STDOUT_FILE=%TARGET_PATH_TEMP_STDOUT_FILE%"
+if not defined TARGET_PATH_STDERR_FILE set "TARGET_PATH_STDERR_FILE=%TARGET_PATH_TEMP_STDERR_FILE%"
 
 call :MAIN %%*
 
-del /F /Q "%TARGET_PATH_TEMP_STDOUT_FILE%" >nul 2>nul
-del /F /Q "%TARGET_PATH_TEMP_STDERR_FILE%" >nul 2>nul
+if defined TARGET_PATH_TEMP_STDOUT_FILE del /F /Q "%TARGET_PATH_TEMP_STDOUT_FILE%" >nul 2>nul
+if defined TARGET_PATH_TEMP_STDERR_FILE del /F /Q "%TARGET_PATH_TEMP_STDERR_FILE%" >nul 2>nul
 
 if defined RETURN_VALUE ( endlocal & set "RETURN_VALUE=%RETURN_VALUE%" & exit /b 0 )
 
@@ -80,20 +86,20 @@ exit /b 1
 
 :MAIN
 rem UTF-16LE BOM
-copy "%CONTOOLS_ROOT%\encoding\boms\fffe.bin" "%TARGET_PATH_TEMP_STDOUT_FILE%" /B /Y >nul 2>nul
-copy "%CONTOOLS_ROOT%\encoding\boms\fffe.bin" "%TARGET_PATH_TEMP_STDERR_FILE%" /B /Y >nul 2>nul
+copy "%CONTOOLS_ROOT%\encoding\boms\fffe.bin" "%TARGET_PATH_STDOUT_FILE%" /B /Y >nul 2>nul
+copy "%CONTOOLS_ROOT%\encoding\boms\fffe.bin" "%TARGET_PATH_STDERR_FILE%" /B /Y >nul 2>nul
 rem UTF-8 BOM
-rem set /P =﻿<nul > "%TARGET_PATH_TEMP_STDOUT_FILE%"
+rem set /P =﻿<nul > "%TARGET_PATH_STDOUT_FILE%"
 
 (
   if %FLAG_EXTENDED_PROPERTY% EQU 0 (
     call "%%CONTOOLS_ROOT%%/std/callshift.bat" -skip 5 "%%FLAG_SHIFT%%" "%%SystemRoot%%\System32\cscript.exe" //NOLOGO //U "%%CONTOOLS_TOOL_ADAPTORS_ROOT%%/vbs/read_shortcut.vbs" -p TargetPath -- %%* || exit /b
   ) else call "%%CONTOOLS_ROOT%%/std/callshift.bat" -skip 7 "%%FLAG_SHIFT%%" "%%SystemRoot%%\System32\cscript.exe" //NOLOGO //U "%%CONTOOLS_TOOL_ADAPTORS_ROOT%%/vbs/read_path_props.vbs" -v -x -lr -- LinkTarget %%* || exit /b
-) >> "%TARGET_PATH_TEMP_STDOUT_FILE%" 2>> "%TARGET_PATH_TEMP_STDERR_FILE%"
+) >> "%TARGET_PATH_STDOUT_FILE%" 2>> "%TARGET_PATH_STDERR_FILE%"
 
 rem NOTE: `type` respects UTF-16LE file with BOM header
-type "%TARGET_PATH_TEMP_STDERR_FILE%" >&2
+type "%TARGET_PATH_STDERR_FILE%" >&2
 
 if %FLAG_EXTENDED_PROPERTY% EQU 0 (
-  for /F "usebackq eol= tokens=1,* delims==" %%i in (`@type "%TARGET_PATH_TEMP_STDOUT_FILE%"`) do set "RETURN_VALUE=%%j"
+  for /F "usebackq eol= tokens=1,* delims==" %%i in (`@type "%TARGET_PATH_STDOUT_FILE%"`) do set "RETURN_VALUE=%%j"
 ) else for /F "usebackq eol= tokens=* delims=" %%i in (`@type "%TARGET_PATH_TEMP_STDOUT_FILE%"`) do set "RETURN_VALUE=%%i"
