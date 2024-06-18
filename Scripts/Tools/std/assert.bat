b78bb05c1805c4b8bb105db86ad16e1f686b9336
andry81 <andry@inbox.ru>
2024-06-18 18:34:01 +0300

    2024.06.18: * new: Scripts/Tools/std/assert.bat: assertion script with an outter script execution stop

---
+++ Scripts/Tools/std/assert.bat
@@ -0,0 +1,18 @@
+@echo off
+@if 0 %*
+
+rem Description:
+rem   Stops an outer script execution with the message:
+rem     `The syntax of the command is incorrect.` if `%1` is empty, otherwise
+rem     `%1 was unexpected at this time.` if not.
+rem   Does not change the error level.
+
+rem Examples:
+rem   1. >assert.bat
+rem      The syntax of the command is incorrect.
+rem
+rem   2. >assert.bat a b c
+rem      a was unexpected at this time.
+rem
+rem   3. >assert.bat "a b c"
+rem      "a b c" was unexpected at this time.
