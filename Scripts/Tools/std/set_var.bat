a0629ac2e9c21dbb73b0958e34b6782dbf2a9a00
andry81 <andry@inbox.ru>
2024-06-18 22:07:24 +0300

    2024.06.18: * new: Scripts/Tools/std/set_var.bat: script to set variable from another variable without error level change

---
+++ Scripts/Tools/std/set_var.bat
@@ -0,0 +1,19 @@
+@echo off
+
+rem USAGE:
+rem   set_var.bat <outvar> <var>
+
+rem Description:
+rem   Sets variable into another variable.
+rem   Does not change the error level.
+
+rem CAUTION:
+rem   The delayed expansion feature must be disabled before this script call: `setlocal DISABLEDELAYEDEXPANSION`, otherwise
+rem   the `!` character will be expanded.
+rem
+
+(
+  if defined %~2 (
+    setlocal ENABLEDELAYEDEXPANSION & for /F "eol= tokens=* delims=" %%i in ("!%~2!") do endlocal & set "%~1=%%i"
+  ) else set "%~1="
+) & exit /b %ERRORLEVEL%
