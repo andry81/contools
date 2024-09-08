007478640f24f65ebcbe36fa1422b3dc53ca2fea
andry81 <andry@inbox.ru>
2024-09-08 19:09:54 +0300

    2024.09.08: * fixed: Scripts/Tests/bench/batscripts: minor fixup

--- Scripts/Tests/bench/batscripts/test_std_encode__22_sys_chars_exe_cmdline.bat
+++ Scripts/Tests/bench/batscripts/test_std_encode__22_sys_chars_exe_cmdline.bat
@@ -30,7 +30,7 @@ set /A TIME_SECS=%TIMEDIFF% / 1000
 set /A TIME_MSECS=%TIMEDIFF% %% 1000
 
 if "%TIME_MSECS:~2,1%" == "" set "TIME_MSECS=0%TIME_MSECS%"
-if "%TIME_MSECS:~1,1%" == "" set "TIME_MSECS=0%TIME_MSECS%"
+if "%TIME_MSECS:~2,1%" == "" set "TIME_MSECS=0%TIME_MSECS%"
 
 echo Time spent: %TIME_SECS%.%TIME_MSECS% secs
 echo.
