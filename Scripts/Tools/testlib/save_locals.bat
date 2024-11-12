f27772fac767da100fc40310bf9f4959aebcfab2
andry81 <andry@inbox.ru>
2024-11-13 00:42:16 +0300

    2024.11.13: * changed: Scripts/Tools/testlib/save_locals.bat: minor optimization based on the suggestion in the issue #3 (`Batch FOR-Loops EOL relies on a rare character`)

--- Scripts/Tools/testlib/save_locals.bat
+++ Scripts/Tools/testlib/save_locals.bat
@@ -7,18 +7,10 @@ type nul > "%TEST_SCRIPT_RETURN_VALUES_FILE_PATH%"
 
 for %%i in (TESTLIB__NEST_LVL TESTLIB__OVERALL_PASSED_TESTS TESTLIB__OVERALL_TESTS) do (
   (echo.%%i) >> "%TEST_SCRIPT_RETURN_VARS_FILE_PATH%"
-  setlocal ENABLEDELAYEDEXPANSION
-  if defined %%i for /F "tokens=* delims="eol^= %%j in ("!%%i!") do (
-    endlocal
-    (echo.%%j) >> "%TEST_SCRIPT_RETURN_VALUES_FILE_PATH%"
-  ) else (echo.) >> "%TEST_SCRIPT_RETURN_VALUES_FILE_PATH%"
+  setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%j in (""!%%i!"") do endlocal & (echo.%%~j) >> "%TEST_SCRIPT_RETURN_VALUES_FILE_PATH%"
 )
 
 if exist "%~1" for /F "usebackq tokens=* delims="eol^= %%i in ("%~1") do (
   (echo.%%i) >> "%TEST_SCRIPT_RETURN_VARS_FILE_PATH%"
-  setlocal ENABLEDELAYEDEXPANSION
-  if defined %%i for /F "tokens=* delims="eol^= %%j in ("!%%i!") do (
-    endlocal
-    (echo.%%j) >> "%TEST_SCRIPT_RETURN_VALUES_FILE_PATH%"
-  ) else (echo.) >> "%TEST_SCRIPT_RETURN_VALUES_FILE_PATH%"
+  setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%j in (""!%%i!"") do endlocal & (echo.%%~j) >> "%TEST_SCRIPT_RETURN_VALUES_FILE_PATH%"
 )
