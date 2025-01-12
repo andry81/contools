5fc90925f09d13e1ebcd79643ec1d036dfe90221
andry81 <andry@inbox.ru>
2025-01-12 19:22:52 +0300

    2025.01.12: * changed: Scripts/Tools/std/get_windows_version.bat: improved parse, removed temporary file usage

--- Scripts/Tools/std/get_windows_version.bat
+++ Scripts/Tools/std/get_windows_version.bat
@@ -2,25 +2,17 @@
 
 setlocal
 
-if defined SCRIPT_TEMP_CURRENT_DIR (
-  set "__?VER_TEMP_FILE=%SCRIPT_TEMP_CURRENT_DIR%\%~n0.%RANDOM%-%RANDOM%.txt"
-) else set "__?VER_TEMP_FILE=%TEMP%\%~n0.%RANDOM%-%RANDOM%.txt"
-
-ver 2>nul > "%__?VER_TEMP_FILE%"
-
 rem CAUTION:
 rem   In Windowx XP an elevated call under data protection flag will block the wmic tool, so we have to use `ver` command instead!
 rem
 set "WINDOWS_VER_STR="
-for /F "usebackq tokens=1,2,* delims=[]" %%i in ("%__?VER_TEMP_FILE%") do for /F "tokens=1,2,* delims= " %%l in ("%%j") do set "WINDOWS_VER_STR=%%m"
+for /F "usebackq tokens=1,* delims=[" %%i in (`@ver 2^>nul`) do for /F "tokens=1,* delims=]" %%k in ("%%j") do set "WINDOWS_VER_STR=%%k"
 
-del /F /Q /A:-D "%__?VER_TEMP_FILE%" >nul 2>nul
-
-set "__?VER_TEMP_FILE="
+setlocal ENABLEDELAYEDEXPANSION & for /F "usebackq tokens=* delims="eol^= %%i in ('"!WINDOWS_VER_STR:* =!"') do endlocal & set "WINDOWS_VER_STR=%%~i"
 
 set WINDOWS_MAJOR_VER=0
 set WINDOWS_MINOR_VER=0
-for /F "tokens=1,2,* delims=."eol^= %%i in ("%WINDOWS_VER_STR%") do ( set "WINDOWS_MAJOR_VER=%%i" & set "WINDOWS_MINOR_VER=%%j" )
+for /F "tokens=1,2,* delims=."eol^= %%i in ("%WINDOWS_VER_STR%") do set "WINDOWS_MAJOR_VER=%%i" & set "WINDOWS_MINOR_VER=%%j"
 
 set WINDOWS_X64_VER=0
 if defined PROCESSOR_ARCHITEW6432 ( set "WINDOWS_X64_VER=1" ) else if /i not "%PROCESSOR_ARCHITECTURE%" == "x86" set WINDOWS_X64_VER=1
