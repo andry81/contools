4232d2518a924ca8d2f8c24584afb6c1b50b3e69
andry81 <andry@inbox.ru>
2025-01-13 18:55:38 +0300

    2025.01.13: * fixed: Scripts/Tools/std/canonical_path_if_def.bat: check variable's value after existence of a variable

--- Scripts/Tools/std/canonical_path_if_def.bat
+++ Scripts/Tools/std/canonical_path_if_def.bat
@@ -7,12 +7,12 @@ if "%~1" == "" (
   exit /b 255
 ) >&2
 
+if not defined %~1 exit /b 0
+
 if "%~2" == "" (
   echo.%~nx0: error: variable's value is not defined: "%~1".
   exit /b 255
 ) >&2
-
-if not defined %~1 exit /b 0
 
 for /F "tokens=* delims="eol^= %%i in ("%~2\.") do set "RETURN_VALUE=%%~fi"
 rem set "RETURN_VALUE=%RETURN_VALUE:\=/%"
