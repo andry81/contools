35cf81606b8fbaa9e95c0101ff29af1bcf87a67e
andry81 <andry@inbox.ru>
2025-01-22 04:46:53 +0300

    2025.01.22: * new: Scripts/Tools/ToolAdaptors/hta/cmd_admin.bat: script runs `cmd.exe` under UAC promotion using `mshta.exe` executable * new: Scripts/Tools/ToolAdaptors/hta/Samples/mshta_cmd_elevate.bat: batch script example for the uniform variant of a command line as a single argument for the `mshta.exe` executable and other cases * changed: Scripts/Tools/ToolAdaptors/hta/Samples/cmd_admin.bat: removed as does not support nested quotes

---
+++ Scripts/Tools/ToolAdaptors/hta/Samples/mshta_cmd_elevate.bat
@@ -0,0 +1,51 @@
+@echo off
+
+setlocal DISABLEDELAYEDEXPANSION
+
+call "%%~dp0..\..\__init__\__init__.bat" || exit /b
+
+rem Uniform variant of a command line as a single argument for the `mshta.exe`
+rem executable and other cases.
+rem You must take care about escaping of nested quotes and Windows Batch
+rem control characters.
+
+rem NOTE:
+rem   `ExecuteGlobal` is used as a workaround, because the `mshta.exe` first
+rem    argument must not be used with the surrounded quotes.
+
+rem Windows Batch compatible command line with escapes (`"""` is a single nested `"`, `""""""` is a double nested `"` and so on).
+set "__QARG0__=vbscript:ExecuteGlobal("""Close(CreateObject(""""""Shell.Application"""""").ShellExecute(""""""%COMSPEC%"""""", """"""/k @cd """"""""""""%CD%"""""""""""" ^& %CD:~0,2% ^& """"""""""""%CONTOOLS_UTILITIES_BIN_ROOT%/contools/printargs.exe"""""""""""" """"""""""""123 456"""""""""""""""""", """""""""""", """"""runas"""""", True))""")"
+
+rem Command line variant for the Windows Batch `echo` command
+
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!__QARG0__!") do endlocal & ^
+echo.^>%%i
+
+echo.---
+
+rem Command line variant for the executable with C runtime command line parser
+
+rem translate escapes into C runtime command line escapes
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!__QARG0__:""""""""""""=\\\\\\\"!"") do endlocal & set "?.=%%i"
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!?.:~0,-1!") do endlocal & set "?.=%%i"
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!?.:""""""=\\\"!"") do endlocal & set "?.=%%i"
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!?.:~0,-1!") do endlocal & set "?.=%%i"
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!?.:"""^=\"!") do endlocal & set "?.=%%i"
+
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!?.!") do endlocal & ^
+start /B /WAIT "" "%CONTOOLS_UTILITIES_BIN_ROOT%/contools/printargs.exe" "%%i"
+
+echo.---
+
+rem Command line variant for `mshta.exe` executable
+
+rem translate C runtime command line escapes into `mshta.exe` (vbs) escapes (`""` is a single nested `"`, `""""` is a double nested `"` and so on)
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!?.:\\\\\\\"^=""""!"") do endlocal & set "?.=%%i"
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!?.:~0,-1!") do endlocal & set "?.=%%i"
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!?.:\\\"^=""!"") do endlocal & set "?.=%%i"
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!?.:~0,-1!") do endlocal & set "?.=%%i"
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!?.:\"^="!") do endlocal & set "?.=%%i"
+
+set ?.
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!?.!") do endlocal & ^
+start /B /WAIT "" "%SystemRoot%\System32\mshta.exe" %%i
