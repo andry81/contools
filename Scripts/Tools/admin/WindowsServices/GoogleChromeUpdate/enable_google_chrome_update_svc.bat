023eca8dee3dc405c70c6a85e5928915ef51eb58
andry81 <andry@inbox.ru>
2025-01-24 04:48:13 +0300

    2025.01.24: * fixed: Scripts/Tools/*.bat: elevation execution fixup

--- Scripts/Tools/admin/WindowsServices/GoogleChromeUpdate/enable_google_chrome_update_svc.bat
+++ Scripts/Tools/admin/WindowsServices/GoogleChromeUpdate/enable_google_chrome_update_svc.bat
@@ -1,37 +1,64 @@
 @echo off
 
-setlocal
+setlocal DISABLEDELAYEDEXPANSION
+
+set /A ELEVATED+=0
 
 if %IMPL_MODE%0 NEQ 0 goto IMPL
-call :IS_ADMIN_ELEVATED && goto MAIN
+call :IS_ADMIN_ELEVATED && goto ELEVATED
 
-:ELEVATE
-set IMPL=1
-rem CAUTION: ShellExecute does not wait a child process close!
-start /B /WAIT "" "%SystemRoot%\System32\mshta.exe" vbscript:Close^(CreateObject^("Shell.Application").ShellExecute^("%COMSPEC%"^,"/c @call ""%~f0"" %* & pause"^,""^,"runas"^,True))
-exit /b
+goto ELEVATE
 
 rem CAUTIOM:
 rem   Windows 7 has an issue around the `find.exe` utility and code page 65001.
 rem   We use `findstr.exe` instead of `find.exe` to workaround it.
 rem
-rem   Details: https://superuser.com/questions/557387/pipe-not-working-in-cmd-exe-on-windows-7/1869422#1869422
+rem   Based on: https://superuser.com/questions/557387/pipe-not-working-in-cmd-exe-on-windows-7/1869422#1869422
 
 :IS_ADMIN_ELEVATED
-if exist "%SystemRoot%\System32\whoami.exe" "%SystemRoot%\System32\whoami.exe" /groups | "%SystemRoot%\System32\findstr.exe" /L "S-1-16-12288" >nul 2>nul && exit /b
-if exist "%SystemRoot%\System32\fltmc.exe" "%SystemRoot%\System32\fltmc.exe" >nul 2>nul && exit /b
-if exist "%SystemRoot%\System64\openfiles.exe" "%SystemRoot%\System64\openfiles.exe" >nul 2>nul && exit /b
-if exist "%SystemRoot%\System32\openfiles.exe" "%SystemRoot%\System32\openfiles.exe" >nul 2>nul && exit /b
+if exist "%SystemRoot%\System32\whoami.exe" "%SystemRoot%\System32\whoami.exe" /groups | "%SystemRoot%\System32\findstr.exe" /L "S-1-16-12288" >nul 2>nul & exit /b
+if exist "%SystemRoot%\System32\fltmc.exe" "%SystemRoot%\System32\fltmc.exe" >nul 2>nul & exit /b
+if exist "%SystemRoot%\System64\openfiles.exe" "%SystemRoot%\System64\openfiles.exe" >nul 2>nul & exit /b
+if exist "%SystemRoot%\System32\openfiles.exe" "%SystemRoot%\System32\openfiles.exe" >nul 2>nul & exit /b
 if exist "%SystemRoot%\System32\config\system" exit /b 0
 exit /b 255
 
+:ELEVATE
+rem Based on:
+rem   `Uniform variant of a command line as a single argument for the `mshta.exe` executable and other cases` :
+rem   https://github.com/andry81/contools/discussions/11
+
+rem Windows Batch compatible command line with escapes (`"""` is a single nested `"`, `""""""` is a double nested `"` and so on).
+set ?.=set "IMPL_MODE=1" ^& "%~f0" %* ^& pause
+
+rem translate Windows Batch compatible escapes into escape placeholders
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!?.:$=$0!") do endlocal & set "?.=%%i"
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!?.:"""^=$2!"") do endlocal & set "?.=%%i"
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!?.:~0,-1!") do endlocal & set "?.=%%i"
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!?.:"^=$1!"") do endlocal & set "?.=%%i"
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!?.:~0,-1!") do endlocal & set "?.=%%i"
+
+rem translate escape placeholders into `mshta.exe` (vbs) escapes (`""` is a single nested `"`, `""""` is a double nested `"` and so on)
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!?.:$2=""""""""!") do endlocal & set "?.=%%i"
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!?.:$1=""""!") do endlocal & set "?.=%%i"
+setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in ("!?.:$0=$!") do endlocal & set "?.=%%i"
+
+rem CAUTION: ShellExecute does not wait a child process close!
+rem NOTE: `ExecuteGlobal` is used as a workaround, because the `mshta.exe` first argument must not be used with the surrounded quotes
+start /B /WAIT "" "%SystemRoot%\System32\mshta.exe" vbscript:ExecuteGlobal("Close(CreateObject(""Shell.Application"").ShellExecute(""%COMSPEC%"", ""/c @%?.%"", """", ""runas"", True))")
+exit /b
+
+:ELEVATED
+set ELEVATED=1
+
 :IMPL
-call :IS_ADMIN_ELEVATED || (
+if %ELEVATED% EQU 0 call :IS_ADMIN_ELEVATED || (
   echo.%~nx0: error: process must be elevated before continue.
   exit /b 255
 ) >&2
 
-:MAIN
+echo.^>sc config gupdate start= demand
 sc config gupdate start= demand
 
+echo.^>sc config gupdatem start= demand
 sc config gupdatem start= demand
--- Scripts/Tools/std/is_admin_elevated.bat
+++ Scripts/Tools/std/is_admin_elevated.bat
@@ -9,10 +9,10 @@ rem   https://www.robvanderwoude.com/battech_elevation.php
 
 setlocal
 
-if exist "%SystemRoot%\System32\whoami.exe" "%SystemRoot%\System32\whoami.exe" /groups | "%SystemRoot%\System32\findstr.exe" /L "S-1-16-12288" >nul 2>nul && exit /b
-if exist "%SystemRoot%\System32\fltmc.exe" "%SystemRoot%\System32\fltmc.exe" >nul 2>nul && exit /b
-if exist "%SystemRoot%\System64\openfiles.exe" "%SystemRoot%\System64\openfiles.exe" >nul 2>nul && exit /b
-if exist "%SystemRoot%\System32\openfiles.exe" "%SystemRoot%\System32\openfiles.exe" >nul 2>nul && exit /b
+if exist "%SystemRoot%\System32\whoami.exe" "%SystemRoot%\System32\whoami.exe" /groups | "%SystemRoot%\System32\findstr.exe" /L "S-1-16-12288" >nul 2>nul & exit /b
+if exist "%SystemRoot%\System32\fltmc.exe" "%SystemRoot%\System32\fltmc.exe" >nul 2>nul & exit /b
+if exist "%SystemRoot%\System64\openfiles.exe" "%SystemRoot%\System64\openfiles.exe" >nul 2>nul & exit /b
+if exist "%SystemRoot%\System32\openfiles.exe" "%SystemRoot%\System32\openfiles.exe" >nul 2>nul & exit /b
 if exist "%SystemRoot%\System32\config\system" exit /b 0
 exit /b 255
 
@@ -20,4 +20,4 @@ rem CAUTIOM:
 rem   Windows 7 has an issue around the `find.exe` utility and code page 65001.
 rem   We use `findstr.exe` instead of `find.exe` to workaround it.
 rem
-rem   Details: https://superuser.com/questions/557387/pipe-not-working-in-cmd-exe-on-windows-7/1869422#1869422
+rem   Based on: https://superuser.com/questions/557387/pipe-not-working-in-cmd-exe-on-windows-7/1869422#1869422
