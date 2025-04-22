76366e21830d9452650e88aacf7bf23b030eb237
andry81 <andry@inbox.ru>
2025-04-23 02:38:06 +0300

    2025.04.23: * new: Scripts/Tests/bench/batscripts/test__echo*.bat: bench tests * refactor: Scripts/Tests/bench/batscripts: test__call*.bat, test__all.bat: tests refactor

--- Scripts/Tests/bench/batscripts/test__all.bat
+++ Scripts/Tests/bench/batscripts/test__all.bat
@@ -7,5 +7,8 @@ call "%%~dp0__init__/__init__.bat" || exit /b
 
 for %%i in ("%TESTS_PROJECT_ROOT%\test__*.bat") do (
   set "SCRIPT_FILE=%%i"
-  call "%%CONTOOLS_ROOT%%/std/if_.bat" not "%%SCRIPT_FILE:*\test__all.bat=%%" == "" && call "%%CONTOOLS_ROOT%%/std/call.bat" "%%SCRIPT_FILE%%"
+  call "%%CONTOOLS_ROOT%%/std/if_.bat" ^
+    not "%%SCRIPT_FILE:*\test__all.bat=%%" == "" ^
+    if not "%%SCRIPT_FILE:*\test__call.bat=%%" == "" ^
+    if not "%%SCRIPT_FILE:*\test__echo.bat=%%" == "" && call "%%CONTOOLS_ROOT%%/std/call.bat" "%%SCRIPT_FILE%%"
 )
---
+++ Scripts/Tests/bench/batscripts/test__echo__07_plus.bat
@@ -0,0 +1,41 @@
+@echo off
+
+setlocal
+
+call "%%~dp0__init__/__init__.bat" || exit /b
+
+echo.^>%~nx0
+
+set "TEMP_FILE=%TEMP%\@type-%RANDOM%-%RANDOM%.txt"
+
+setlocal DISABLEDELAYEDEXPANSION
+
+set "BEGIN_TIME=%TIME%"
+
+(
+  for /L %%i in (1,1,10000) do (
+    echo+Hello world!
+  )
+) >nul
+
+call "%%CONTOOLS_ROOT%%/timediff.bat" "%%BEGIN_TIME%%" "%%TIME%%"
+
+(
+  endlocal
+  set "TIMEDIFF=%TIMEDIFF%"
+)
+
+set /A TIMEDIFF/=10
+
+set /A TIME_SECS=%TIMEDIFF% / 1000
+set /A TIME_MSECS=%TIMEDIFF% %% 1000
+
+if "%TIME_MSECS:~2,1%" == "" set "TIME_MSECS=0%TIME_MSECS%"
+if "%TIME_MSECS:~2,1%" == "" set "TIME_MSECS=0%TIME_MSECS%"
+
+echo Time spent: %TIME_SECS%.%TIME_MSECS% msecs
+echo.
+
+del /F /Q /A:-D "%TEMP_FILE%" >nul 2>nul
+
+exit /b 0
