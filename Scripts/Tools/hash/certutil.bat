9e7343d58e66372287653d97b82ecfab20583513
andry81 <andry@inbox.ru>
2025-04-29 19:24:35 +0300

    2025.04.29: * new: Scripts/Tools/hash/certutil.bat: `certutil.exe` wrapper script for a file hash calculation

---
+++ Scripts/Tools/hash/certutil.bat
@@ -0,0 +1,14 @@
+@echo off
+
+rem USAGE:
+rem   certutil.bat <file> [<algorithm>]
+
+rem Description:
+rem   Certutil wrapper script.
+
+rem <algorithm>:
+rem   MD2 MD4 MD5 SHA1 SHA256 SHA384 SHA512
+
+setlocal
+
+"%SystemRoot%\System32\certutil.exe" -hashfile %*
