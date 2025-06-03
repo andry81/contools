054d8a06c7a2d83e1dba004656717a296dc405c4
andry81 <andry@inbox.ru>
2025-06-03 09:50:41 +0300

    2025.06.03: * new: Scripts/Tests/bench/batscripts/test__echo_!exclusive: exclusive `echo` bench tests

---
+++ Scripts/Tests/bench/batscripts/test__echo_!exclusive/test__echo__01_print_10K.bat
@@ -0,0 +1,20 @@
+@echo off
+
+setlocal
+
+call "%%~dp0__init__/__init__.bat" || exit /b
+
+echo;^>%~nx0
+
+setlocal DISABLEDELAYEDEXPANSION
+
+call "%%CONTOOLS_ROOT%%/time/begin_time.bat"
+
+for /L %%i in (1,1,10000) do echo %%i
+
+call "%%CONTOOLS_ROOT%%/time/end_time.bat" 10
+
+echo Time spent: %TIME_INTS%.%TIME_FRACS% msecs
+echo;
+
+exit /b 0
