d844b815a17e1d63e61251ab6400459a42688e2d
andry81 <andry@inbox.ru>
2025-06-06 15:54:44 +0300

    2025.06.06: * fixed: Scripts/Tools/*.bat: Windows XP Administrator privileges elevation code fixup * changed: ISSUES.txt: issues update

--- Scripts/Tools/ToolAdaptors/hta/cmd_admin_system.bat
+++ Scripts/Tools/ToolAdaptors/hta/cmd_admin_system.bat
@@ -73,18 +73,24 @@ rem with locals drop
 setlocal ENABLEDELAYEDEXPANSION & for /F "tokens=* delims="eol^= %%i in (""!PSEXEC!" -i -s -d "!COMSPEC!" !?.!") do endlocal & endlocal & %%i
 exit /b
 
-rem CAUTIOM:
+rem CAUTION:
 rem   Windows 7 has an issue around the `find.exe` utility and code page 65001.
 rem   We use `findstr.exe` instead of `find.exe` to workaround it.
 rem
 rem   Based on: https://superuser.com/questions/557387/pipe-not-working-in-cmd-exe-on-windows-7/1869422#1869422
 
+rem CAUTION:
+rem   In Windowx XP an elevated call under data protection flag will block the wmic tool, so we have to use `ver` command instead!
+
 :IS_ADMIN_ELEVATED
-if exist "%SystemRoot%\System32\whoami.exe" "%SystemRoot%\System32\whoami.exe" /groups | "%SystemRoot%\System32\findstr.exe" /L "S-1-16-12288" >nul 2>nul & exit /b
-if exist "%SystemRoot%\System32\fltmc.exe" "%SystemRoot%\System32\fltmc.exe" >nul 2>nul & exit /b
-if exist "%SystemRoot%\System64\openfiles.exe" "%SystemRoot%\System64\openfiles.exe" >nul 2>nul & exit /b
-if exist "%SystemRoot%\System32\openfiles.exe" "%SystemRoot%\System32\openfiles.exe" >nul 2>nul & exit /b
-if exist "%SystemRoot%\System32\config\system" exit /b 0
+set "WINDOWS_VER_STR=" & set "WINDOWS_MAJOR_VER=0" & for /F "usebackq tokens=1,2,* delims=[]" %%i in (`@ver 2^>nul`) do set "WINDOWS_VER_STR=%%j"
+if not defined WINDOWS_VER_STR goto SKIP_VER
+setlocal ENABLEDELAYEDEXPANSION & for /F "usebackq tokens=* delims="eol^= %%i in ('"!WINDOWS_VER_STR:* =!"') do endlocal & set "WINDOWS_VER_STR=%%~i"
+for /F "tokens=1,2,* delims=."eol^= %%i in ("%WINDOWS_VER_STR%") do set "WINDOWS_MAJOR_VER=%%i"
+:SKIP_VER
+if %WINDOWS_MAJOR_VER% GEQ 6 (
+  if exist "%SystemRoot%\System32\where.exe" "%SystemRoot%\System32\whoami.exe" /groups | "%SystemRoot%\System32\findstr.exe" /L "S-1-16-16384" >nul 2>nul & exit /b
+) else if exist "%SystemRoot%\System32\fltmc.exe" "%SystemRoot%\System32\fltmc.exe" >nul 2>nul & exit /b
 exit /b 255
 
 :CALL_ELEVATE
