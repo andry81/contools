75740214801b45918f072a25f161678904c9fbe6
andry81 <andry@inbox.ru>
2025-07-07 00:51:40 +0300

    2025.07.07: * new: scripts/tests/bench/batscripts/test__read: script bench tests

---
+++ scripts/tests/bench/batscripts/test__read/test__read__12_by_set_using_tmp_file_w_del.bat
@@ -0,0 +1,34 @@
+@echo off
+
+setlocal
+
+call "%%~dp0__init__/__init__.bat" || exit /b
+
+echo;^>%~nx0
+
+setlocal DISABLEDELAYEDEXPANSION
+
+call "%%CONTOOLS_ROOT%%/std/canonical_path.bat" TEMP_DIR  "%%TEST_DATA_TEMP_ROOT%%/%%~n0"
+call "%%CONTOOLS_BUILD_TOOLS_ROOT%%/mkdir_if_notexist.bat" "%%TEMP_DIR%%" >nul || exit /b
+
+call "%%CONTOOLS_ROOT%%/time/begin_time.bat"
+
+setlocal ENABLEDELAYEDEXPANSION
+
+for /L %%i in (1,1,1000) do (
+  set "TEMP_FILE=%TEMP_DIR%\tmp-!RANDOM!-!RANDOM!.txt"
+  echo !RANDOM!-!RANDOM! > "!TEMP_FILE!"
+  set /P VALUE=<"!TEMP_FILE!"
+  del /F /Q /A:-D "!TEMP_FILE!" >nul 2>nul
+)
+
+endlocal
+
+call "%%CONTOOLS_ROOT%%/time/end_time.bat" 1
+
+rmdir /S /Q "%TEMP_DIR%"
+
+echo Time spent: %TIME_INTS%.%TIME_FRACS% msecs
+echo;
+
+exit /b 0
---
+++ scripts/tests/bench/batscripts/test__read/test__read__21_by_for_file_using_exec_to_tmp_file_no_del.bat
@@ -0,0 +1,33 @@
+@echo off
+
+setlocal
+
+call "%%~dp0__init__/__init__.bat" || exit /b
+
+echo;^>%~nx0
+
+setlocal DISABLEDELAYEDEXPANSION
+
+call "%%CONTOOLS_ROOT%%/std/canonical_path.bat" TEMP_DIR  "%%TEST_DATA_TEMP_ROOT%%/%%~n0"
+call "%%CONTOOLS_BUILD_TOOLS_ROOT%%/mkdir_if_notexist.bat" "%%TEMP_DIR%%" >nul || exit /b
+
+call "%%CONTOOLS_ROOT%%/time/begin_time.bat"
+
+setlocal ENABLEDELAYEDEXPANSION
+
+for /L %%i in (1,1,1000) do (
+  set "TEMP_FILE=%TEMP_DIR%\tmp-!RANDOM!-!RANDOM!.txt"
+  "%SystemRoot%\system32\chcp.com" > "!TEMP_FILE!"
+  for /F "usebackq tokens=* delims="eol^= %%i in ("!TEMP_FILE!") do set "VALUE=%%i"
+)
+
+endlocal
+
+call "%%CONTOOLS_ROOT%%/time/end_time.bat" 1
+
+rmdir /S /Q "%TEMP_DIR%"
+
+echo Time spent: %TIME_INTS%.%TIME_FRACS% msecs
+echo;
+
+exit /b 0
