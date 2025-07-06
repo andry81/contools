1d37ea91fcc165ca8345fd111ad27877cb8cbfc1
andry81 <andry@inbox.ru>
2025-07-07 02:32:38 +0300

    2025.07.07: * changed: scripts/tests/bench/batscripts/test__read/test__read__23_by_for_exec_to_var.bat: test speedup

--- scripts/tests/bench/batscripts/test__read/test__read__23_by_for_exec_to_var.bat
+++ scripts/tests/bench/batscripts/test__read/test__read__23_by_for_exec_to_var.bat
@@ -10,13 +10,13 @@ setlocal DISABLEDELAYEDEXPANSION
 
 call "%%CONTOOLS_ROOT%%/time/begin_time.bat"
 
-for /L %%i in (1,1,1000) do (
+for /L %%i in (1,1,100) do (
   for /F "usebackq tokens=* delims="eol^= %%i in (`@"%SystemRoot%\system32\chcp.com"`) do set "VALUE=%%i"
 )
 
 call "%%CONTOOLS_ROOT%%/time/end_time.bat" 1
 
-echo Time spent: %TIME_INTS%.%TIME_FRACS% msecs
+echo Time spent: %TIME_INTS%%TIME_FRACS:~0,1%.%TIME_FRACS:~1%0 msecs
 echo;
 
 exit /b 0
