6df703ed53107310a718263ca0575a6949781416
andry81 <andry@inbox.ru>
2025-07-07 20:18:23 +0300

    2025.07.07: * changed: scripts/tests/unit/batscripts/**/init.bat: switched to `setshift.bat` script to read the rest of a test command line

--- scripts/tests/unit/batscripts/test_xml/.test_xml__00_sed/init.bat
+++ scripts/tests/unit/batscripts/test_xml/.test_xml__00_sed/init.bat
@@ -1,18 +1,8 @@
 @echo off
 
 set "TEST_DATA_DIR=%TEST_SCRIPT_FILE_NAME%/%~1"
-shift
 
-set "TEST_DATA_SED_CMD_LINE="
-:TEST_DATA_SED_CMD_LINE_LOOP
-if "%~1" == "" goto TEST_DATA_SED_CMD_LINE_LOOP_END
-
-set TEST_DATA_SED_CMD_LINE=%TEST_DATA_SED_CMD_LINE%%1 
-shift
-
-goto TEST_DATA_SED_CMD_LINE_LOOP
-
-:TEST_DATA_SED_CMD_LINE_LOOP_END
+call "%%CONTOOLS_ROOT%%/std/setshift.bat" 1 TEST_DATA_SED_CMD_LINE %%*
 
 call "%%CONTOOLS_ROOT%%/std/allocate_temp_dir.bat" . "%%TEST_SCRIPT_FILE_NAME%%" "" "%%TEST_DATA_TEMP_ROOT%%" || exit /b
 
--- scripts/tests/unit/batscripts/test_xml/.test_xml__10_filter_xpath_list_by_xpath_list/init.bat
+++ scripts/tests/unit/batscripts/test_xml/.test_xml__10_filter_xpath_list_by_xpath_list/init.bat
@@ -2,19 +2,8 @@
 
 set "TEST_DATA_FILE_IN=%~1"
 set "TEST_DATA_FILE_REF_DIR=%~2"
-shift
-shift
 
-set "TEST_DATA_CMD_LINE="
-:TEST_DATA_CMD_LINE_LOOP
-if "%~1" == "" goto TEST_DATA_CMD_LINE_LOOP_END
-
-set TEST_DATA_CMD_LINE=%TEST_DATA_CMD_LINE%%1 
-shift
-
-goto TEST_DATA_CMD_LINE_LOOP
-
-:TEST_DATA_CMD_LINE_LOOP_END
+call "%%CONTOOLS_ROOT%%/std/setshift.bat" 2 TEST_DATA_CMD_LINE %%*
 
 call "%%CONTOOLS_ROOT%%/std/allocate_temp_dir.bat" . "%%TEST_SCRIPT_FILE_NAME%%" "" "%%TEST_DATA_TEMP_ROOT%%" || exit /b
 
