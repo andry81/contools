64f88d8957537727de063d23385f9ea0c6b835e0
andry81 <andry@inbox.ru>
2025-08-08 04:03:17 +0300

    2025.08.08: * new: scripts/tests/bench/bat/test_encoding: bench tests for `unix2dos.bat` and `dos2unix.bat` scripts

---
+++ scripts/tests/bench/bat/test_encoding/test_encoding__01_unix2dos.bat
@@ -0,0 +1,22 @@
+@echo off
+
+setlocal
+
+call "%%~dp0__init__/__init__.bat" || exit /b
+
+echo;^>%~nx0
+
+setlocal DISABLEDELAYEDEXPANSION
+
+call "%%CONTOOLS_ROOT%%/time/begin_time.bat"
+
+(
+  for /L %%i in (1,1,10) do call "%%CONTOOLS_ROOT%%/encoding/unix2dos.bat" -fix-tail-lr "%%TEST_DATA_IN_ROOT%%/test_encoding__unix2dos/test_01.txt"
+) >nul
+
+call "%%CONTOOLS_ROOT%%/time/end_time.bat" 10
+
+echo Time spent: %TIME_INTS%.%TIME_FRACS% secs
+echo;
+
+exit /b 0
---
+++ scripts/tests/bench/bat/test_encoding/test_encoding__12_dos2unixs_no_fix_tail_lr.bat
@@ -0,0 +1,22 @@
+@echo off
+
+setlocal
+
+call "%%~dp0__init__/__init__.bat" || exit /b
+
+echo;^>%~nx0
+
+setlocal DISABLEDELAYEDEXPANSION
+
+call "%%CONTOOLS_ROOT%%/time/begin_time.bat"
+
+(
+  for /L %%i in (1,1,10) do call "%%CONTOOLS_ROOT%%/encoding/dos2unix.bat" "%%TEST_DATA_IN_ROOT%%/test_encoding__dos2unix/test_01.txt"
+) >nul
+
+call "%%CONTOOLS_ROOT%%/time/end_time.bat" 10
+
+echo Time spent: %TIME_INTS%.%TIME_FRACS% secs
+echo;
+
+exit /b 0
