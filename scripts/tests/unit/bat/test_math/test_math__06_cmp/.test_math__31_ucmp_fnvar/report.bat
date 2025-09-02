5c9ef044e512e9a8c6e1504528562df719e3b504
andry81 <andry@inbox.ru>
2025-09-02 06:11:02 +0300

    2025.09.02: * fixed: scripts: tools/testlib, tests/unit/bat: tests order number

--- scripts/tests/unit/bat/test_math/test_math__06_cmp/.test_math__31_ucmp_fnvar/report.bat
+++ scripts/tests/unit/bat/test_math/test_math__06_cmp/.test_math__31_ucmp_fnvar/report.bat
@@ -9,11 +9,11 @@ if not defined LVAR set "L=<undef>"
 if not defined RVAR set "R=<undef>"
 
 if !TEST_LAST_ERROR! NEQ 0 (
-  echo;FAILED: !TESTLIB__OVERALL_TESTS!.!TESTLIB__CURRENT_TESTS!: RETREF=`!RETREF!` RET=`!TEST_IMPL_ERROR!` OP=`!OP!` L=`!L!` R=`!R!`
+  echo;FAILED: !TESTLIB__TEST_ORDER_NUMBER!: RETREF=`!RETREF!` RET=`!TEST_IMPL_ERROR!` OP=`!OP!` L=`!L!` R=`!R!`
   echo;
   exit /b 0
 )
 
-echo;PASSED: !TESTLIB__OVERALL_TESTS!.!TESTLIB__CURRENT_TESTS!: RET=`!TEST_IMPL_ERROR!` OP=`!OP!` L=`!L!` R=`!R!`
+echo;PASSED: !TESTLIB__TEST_ORDER_NUMBER!: RET=`!TEST_IMPL_ERROR!` OP=`!OP!` L=`!L!` R=`!R!`
 
 exit /b 0
