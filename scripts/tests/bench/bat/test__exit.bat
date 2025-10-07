36c6a9a0c28e6995a1930e40a359c3d365e70cba
andry81 <andry@inbox.ru>
2025-10-07 04:11:27 +0300

    2025.10.07: * fixed: scripts/tests/bench/bat/test__exit.bat: execution fixup * new: scripts/tests/bench/bat/test__exit: more tests * refactor: scripts/tests/bench/bat/test__exit: minor refactor

--- scripts/tests/bench/bat/test__exit.bat
+++ scripts/tests/bench/bat/test__exit.bat
@@ -4,7 +4,7 @@ setlocal
 
 call "%%~dp0__init__/__init__.bat" || exit /b
 
-for %%i in ("%TESTS_PROJECT_ROOT%\%~n0\test__exit_b__*.bat") do (
+for %%i in ("%TESTS_PROJECT_ROOT%\%~n0\test__exit__*.bat") do (
   set "SCRIPT_FILE=%%i"
   call "%%CONTOOLS_ROOT%%/std/call.bat" "%%SCRIPT_FILE%%"
 )
