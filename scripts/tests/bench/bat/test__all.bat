d5cd2a59eba309ae2e7e0ca09d9c16742d4120a4
andry81 <andry@inbox.ru>
2025-10-30 06:04:25 +0300

    2025.10.30: * fixed: scripts/tests/bench/bat: test_all.bat, test__all.bat: missed to exclude exclusive tests

--- scripts/tests/bench/bat/test__all.bat
+++ scripts/tests/bench/bat/test__all.bat
@@ -6,8 +6,11 @@ call "%%~dp0__init__/script_init.bat" %%0 %%* || exit /b
 if %IMPL_MODE%0 EQU 0 exit /b
 
 for %%i in ("%TESTS_PROJECT_ROOT%\test__*.bat") do (
+  set "SCRIPT_NAME=%%~ni"
   set "SCRIPT_FILE=%%i"
   call "%%CONTOOLS_ROOT%%/std/if_.bat" ^
-    not "%%SCRIPT_FILE:*\%~nx0=%%" == "" if "%%SCRIPT_FILE:*\test___=%%" == "%%SCRIPT_FILE%%" ^
+    "%%SCRIPT_NAME:!=%%" == "%%SCRIPT_NAME%%" ^
+    if not "%%SCRIPT_FILE:*\%~nx0=%%" == "" ^
+    if "%%SCRIPT_FILE:*\test___=%%" == "%%SCRIPT_FILE%%" ^
       && call "%%CONTOOLS_ROOT%%/std/call.bat" "%%SCRIPT_FILE%%"
 )
--- scripts/tests/bench/bat/test_all.bat
+++ scripts/tests/bench/bat/test_all.bat
@@ -6,8 +6,11 @@ call "%%~dp0__init__/script_init.bat" %%0 %%* || exit /b
 if %IMPL_MODE%0 EQU 0 exit /b
 
 for %%i in ("%TESTS_PROJECT_ROOT%\test_*.bat") do (
+  set "SCRIPT_NAME=%%~ni"
   set "SCRIPT_FILE=%%i"
   call "%%CONTOOLS_ROOT%%/std/if_.bat" ^
-    not "%%SCRIPT_FILE:*\%~nx0=%%" == "" if "%%SCRIPT_FILE:*\test__=%%" == "%%SCRIPT_FILE%%" ^
+    "%%SCRIPT_NAME:!=%%" == "%%SCRIPT_NAME%%" ^
+    if not "%%SCRIPT_FILE:*\%~nx0=%%" == "" ^
+    if "%%SCRIPT_FILE:*\test__=%%" == "%%SCRIPT_FILE%%" ^
       && call "%%CONTOOLS_ROOT%%/std/call.bat" "%%SCRIPT_FILE%%"
 )
