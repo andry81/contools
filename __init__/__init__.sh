47bac08361e6e2fd17cc7ae391e93e68c5782143
andry81 <andry@inbox.ru>
2026-02-04 03:19:09 +0300

    2026.02.04: * changed: *.sh: switched to portable version of shebang line

--- __init__/__init__.sh
+++ __init__/__init__.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Script can be ONLY included by "source" command.
 [[ -n "$BASH" && (-z "$BASH_LINENO" || BASH_LINENO[0] -gt 0) && (-z "$CONTOOLS_PROJECT_ROOT_INIT0_DIR" || "$CONTOOLS_PROJECT_ROOT_INIT0_DIR" != "$CONTOOLS_PROJECT_ROOT") ]] || return 0 || exit 0 # exit to avoid continue if the return can not be called
