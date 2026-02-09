9ce743a6cfef9948701f27eeacc6c13812dcfe24
andry81 <andry@inbox.ru>
2026-02-09 08:33:03 +0300

    2026.02.09: * fixed: tests: inappropriate default behavior of the `fc.exe`, by default it does ignore line returns difference, switched to always use the binary mode comparison * fixed: tests: line endings fixup * changed: ISSUES.txt: issues update

--- scripts/tests/unit/bat/test_xml/.test_xml__10_filter_xpath_list_by_xpath_list/impl.bat
+++ scripts/tests/unit/bat/test_xml/.test_xml__10_filter_xpath_list_by_xpath_list/impl.bat
@@ -7,7 +7,7 @@
 if not exist "%TEST_TEMP_DATA_OUT_FILE%" set "TEST_LAST_ERROR=21" & goto EXIT
 if not exist "%TEST_DATA_REF_FILE%" set "TEST_LAST_ERROR=22" & goto EXIT
 
-"%SystemRoot%\System32\fc.exe" "%TEST_TEMP_DATA_OUT_FILE%" "%TEST_DATA_REF_FILE%" >nul || set "TEST_LAST_ERROR=23"
+"%SystemRoot%\System32\fc.exe" /B "%TEST_TEMP_DATA_OUT_FILE%" "%TEST_DATA_REF_FILE%" >nul || set "TEST_LAST_ERROR=23"
 
 :EXIT
 exit /b %TEST_LAST_ERROR%
