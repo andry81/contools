780ae6d6c27f4803c4c310a0e15b9e7e0fc5a639
andry81 <andry@inbox.ru>
2026-03-17 10:46:24 +0300

    2026.03.17: * fixed: scripts/tools/std/is_dir_exist.bat: execution fixup * changed: scripts/tools/std/is_dir_exist.bat: changed return codes to -1 - exist but not a directory, 1 - not exist

--- scripts/tools/std/is_dir_exist.bat
+++ scripts/tools/std/is_dir_exist.bat
@@ -32,11 +32,9 @@ if "%~1" == "" (
 
 for /F "tokens=* delims="eol^= %%i in ("%~1\.") do set "FILE_PATH=%%~fi"
 
-if not exist "\\?\%FILE_PATH%\*" exit /b 1
-
 for /F "tokens=* delims="eol^= %%i in ("\\?\%FILE_PATH%") do set "FILE_PATH_ATTR=%%~ai"
-if not defined FILE_PATH_ATTR exit /b 255
+if not defined FILE_PATH_ATTR exit /b 1
 
-if /i "%FILE_PATH_ATTR:d=%" == "%FILE_PATH_ATTR%" exit /b 2
+if /i "%FILE_PATH_ATTR:d=%" == "%FILE_PATH_ATTR%" exit /b -1
 
 exit /b 0
