5850e2358bcae85def2beb87c797125426d8b727
andry81 <andry@inbox.ru>
2026-03-19 14:02:51 +0300

    2026.03.19: * change: scripts/tools/std/get_windows_version.bat: added `WINDOWS_X32_VER` and full bitness detection OS vs `cmd.exe`

--- scripts/tools/std/get_windows_version.bat
+++ scripts/tools/std/get_windows_version.bat
@@ -2,14 +2,10 @@
 
 setlocal
 
-set "WINDOWS_VER_STR="
-set "WINDOWS_MAJOR_VER="
-set "WINDOWS_MINOR_VER="
-set "WINDOWS_X64_VER="
-set "COMSPEC_X64_VER="
+set "WINDOWS_VER_STR=" & set "WINDOWS_MAJOR_VER=" & set "WINDOWS_MINOR_VER=" & set "WINDOWS_X64_VER=" & set "COMSPEC_X64_VER="
 
 rem CAUTION:
-rem   In Windowx XP an elevated call under data protection flag will block the wmic tool, so we have to use `ver` command instead!
+rem   In Windows XP an elevated call under data protection flag will block the wmic tool, so we have to use `ver` command instead!
 
 for /F "usebackq tokens=1,2,* delims=[]" %%i in (`@ver 2^>nul`) do set "WINDOWS_VER_STR=%%j"
 
@@ -17,18 +13,26 @@ if not defined WINDOWS_VER_STR goto EXIT
 
 setlocal ENABLEDELAYEDEXPANSION & for /F "usebackq tokens=* delims="eol^= %%i in ('"!WINDOWS_VER_STR:* =!"') do endlocal & set "WINDOWS_VER_STR=%%~i"
 
-set WINDOWS_MAJOR_VER=0
-set WINDOWS_MINOR_VER=0
+set /A "WINDOWS_MAJOR_VER=0", "WINDOWS_MINOR_VER=0"
 for /F "tokens=1,2,* delims=."eol^= %%i in ("%WINDOWS_VER_STR%") do set "WINDOWS_MAJOR_VER=%%i" & set "WINDOWS_MINOR_VER=%%j"
 
-set WINDOWS_X64_VER=0
-if defined PROCESSOR_ARCHITEW6432 ( set "WINDOWS_X64_VER=1" ) else if /i not "%PROCESSOR_ARCHITECTURE%" == "x86" set WINDOWS_X64_VER=1
+set /A "WINDOWS_X64_VER=0", "WINDOWS_X32_VER=0"
+if defined PROCESSOR_ARCHITEW6432 (
+  set /A "WINDOWS_X64_VER=1", "WINDOWS_X32_VER=1"
+) else if /i "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+  set /A "WINDOWS_X64_VER=1"
+) else if /i "%PROCESSOR_ARCHITECTURE%" == "x86" (
+  set /A "WINDOWS_X32_VER=1"
+)
+
+rem `SysWOW64` is visible from both 32-bit and 64-bit `cmd.exe`
+if not defined PROCESSOR_ARCHITEW6432 if exist "%SystemRoot%\SysWOW64\*" set /A "WINDOWS_X32_VER=1", "WINDOWS_X64_VER=1"
 
 rem CAUTION:
-rem   Specific case for Windows XP x64 SP2, where both PROCESSOR_ARCHITECTURE and PROCESSOR_ARCHITEW6432 are equal to AMD64 for 32-bit cmd.exe process!
+rem   Specific case for Windows XP x64 SP2, where both PROCESSOR_ARCHITECTURE and PROCESSOR_ARCHITEW6432 can be are equal to AMD64 for 32-bit `cmd.exe` process!
 
-set COMSPEC_X64_VER=0
-if /i not "%PROCESSOR_ARCHITECTURE%" == "x86" if not defined PROCESSOR_ARCHITEW6432 set COMSPEC_X64_VER=1
+set /A "COMSPEC_X64_VER=0"
+if /i not "%PROCESSOR_ARCHITECTURE%" == "x86" if not defined PROCESSOR_ARCHITEW6432 set /A "COMSPEC_X64_VER=1"
 
 :EXIT
 (
@@ -37,6 +41,7 @@ if /i not "%PROCESSOR_ARCHITECTURE%" == "x86" if not defined PROCESSOR_ARCHITEW6
   set "WINDOWS_MAJOR_VER=%WINDOWS_MAJOR_VER%"
   set "WINDOWS_MINOR_VER=%WINDOWS_MINOR_VER%"
   set "WINDOWS_X64_VER=%WINDOWS_X64_VER%"
+  set "WINDOWS_X32_VER=%WINDOWS_X32_VER%"
   set "COMSPEC_X64_VER=%COMSPEC_X64_VER%"
 )
 
