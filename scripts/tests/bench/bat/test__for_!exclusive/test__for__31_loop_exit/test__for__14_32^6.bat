6de4c8f1ad8fe05938e0de80df13d2092f3899d1
andry81 <andry@inbox.ru>
2026-04-23 02:08:33 +0300

    2026.04.23: * new: scripts/tests/bench/bat/test__for_!exclusive/test__for__31_loop_exit: bench tests for `for` loop exit

---
+++ scripts/tests/bench/bat/test__for_!exclusive/test__for__31_loop_exit/test__for__14_32^6.bat
@@ -0,0 +1,29 @@
+@echo off
+
+setlocal
+
+call "%%~dp0__init__/__init__.bat" || exit /b
+
+set "?~nx0=%~nx0"
+
+call "%%CONTOOLS_ROOT%%/std/echo_var.bat" ?~nx0 ">"
+
+setlocal DISABLEDELAYEDEXPANSION
+
+call "%%CONTOOLS_ROOT%%/time/begin_time.bat"
+
+for /L %%i in (1,1,1000) do call :TEST
+goto END
+
+:TEST
+for /L %%# in (1,1,32) do for /L %%# in (1,1,32) do for /L %%# in (1,1,32) do ^
+for /L %%# in (1,1,32) do for /L %%# in (1,1,32) do for /L %%# in (1,1,32) do exit /b 0
+
+:END
+
+call "%%CONTOOLS_ROOT%%/time/end_time.bat" 1
+
+echo Time spent: %TIME_INTS%.%TIME_FRACS% msecs
+echo;
+
+exit /b 0
