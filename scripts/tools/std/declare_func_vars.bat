2c84a519549e4033040a54002bf54ed1d7c9a339
andry81 <andry@inbox.ru>
2026-04-25 18:37:10 +0300

    2026.04.25: * new: scripts/tools/std/declare_func_vars.bat: `Terminate Batch Job` skip code

--- scripts/tools/std/declare_func_vars.bat
+++ scripts/tools/std/declare_func_vars.bat
@@ -5,4 +5,8 @@ for /L %%# in (1,1,16) do for /L %%# in (1,1,16) do for /L %%# in (1,1,16) do fo
 for /L %%# in (1,1,16) do for /L %%# in (1,1,16) do for /L %%# in (1,1,16) do for /L %%# in (1,1,16) do ^
 for %%# in (%%) do for %%# in (%%##) do
 
+rem `Terminate Batch Job` skip code
+set @CMD_SKIP_TERMINATE_BATCH_JOB="%%SystemRoot%%\System32\cmd.exe" /S /c "@if %%ERRORLEVEL%% EQU -1073741510 (exit 9009) else exit %%ERRORLEVEL%%"
+set :CALL_CMD_SKIP_TERMINATE_BATCH_JOB=call %%@CMD_SKIP_TERMINATE_BATCH_JOB%%
+
 exit /b 0
