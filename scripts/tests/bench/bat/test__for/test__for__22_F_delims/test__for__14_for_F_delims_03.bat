97214070c8385c77e76e406bba2068c00268c3f7
andry81 <andry@inbox.ru>
2026-05-08 15:54:21 +0300

    2026.05.08: * changed: scripts/tests/bench/bat/test__for: moved from exclusion group

---
+++ scripts/tests/bench/bat/test__for/test__for__22_F_delims/test__for__14_for_F_delims_03.bat
@@ -0,0 +1,20 @@
+@echo off
+
+setlocal
+
+call "%%~dp0__init__/__init__.bat" || exit /b
+
+echo;^>%~nx0
+
+setlocal DISABLEDELAYEDEXPANSION
+
+call "%%CONTOOLS_ROOT%%/time/begin_time.bat"
+
+for /L %%i in (1,1,100000) do for /F "tokens=* delims=;.,"eol^= %%j in ("012,345,678,901,234,567,890") do rem
+
+call "%%CONTOOLS_ROOT%%/time/end_time.bat" 100
+
+echo Time spent: %TIME_INTS%.%TIME_FRACS% msecs
+echo;
+
+exit /b 0
---
+++ scripts/tests/bench/bat/test__for/test__for__31_loop_exit.bat
@@ -0,0 +1,12 @@
+@echo off
+
+setlocal
+
+call "%%~dp0__init__/__init__.bat" || exit /b
+
+echo;^>%~nx0
+
+for %%i in ("%~dpn0\test__for__*.bat") do (
+  set "SCRIPT_FILE=%%i"
+  call "%%SCRIPT_FILE%%"
+)
