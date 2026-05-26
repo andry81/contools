47c361c30e9c3b400f8cbfc1565b14b9e6ab6cd7
andry81 <andry@inbox.ru>
2026-05-26 10:02:58 +0300

    2026.05.26: * fixed: scripts/tools/std/pause.bat: execution fixup * changed: scripts/tools/std/pause.bat: code improvement

--- scripts/tools/std/pause.bat
+++ scripts/tools/std/pause.bat
@@ -1,7 +1,7 @@
 @echo off & goto DOC_END
 
 rem USAGE:
-rem   pause.bat [-vbs] [-chcp <code-page>]
+rem   pause.bat [-chcp <code-page>]
 
 rem Description:
 rem   Sets the code page to the last known (`%LAST_CP%`) after a code page
@@ -29,27 +29,13 @@ rem     https://github.com/andry81/contools/discussions/37
 
 setlocal & set "LAST_ERROR=%ERRORLEVEL%"
 
-set "CHCP_FILE="
-if exist "%SystemRoot%\System32\chcp.com" set "CHCP_FILE=%SystemRoot%\System32\chcp.com"
-if not defined CHCP_FILE if exist "%SystemRoot%\System64\chcp.com" set "CHCP_FILE=%SystemRoot%\System64\chcp.com"
-
-if not defined CHCP_FILE (
-  if exist "%SystemRoot%\System32\timeout.exe" ( "%SystemRoot%\System32\timeout.exe" /T -1 ) else pause
-  exit /b %LAST_ERROR%
-)
-
 set "?~dp0=%~dp0"
-set "?~n0=%~n0"
 
 rem script names call stack
 if defined ?~ ( set "?~=%?~%-^>%~nx0" ) else if defined ?~nx0 ( set "?~=%?~nx0%-^>%~nx0" ) else set "?~=%~nx0"
 
-set "?~nx0=%~nx0"
-
 rem script flags
 set "FLAG_CHCP="
-
-:FLAGS_LOOP
 
 rem flags always at first
 set "FLAG=%~1"
@@ -57,24 +43,16 @@ set "FLAG=%~1"
 if defined FLAG ^
 if not "%FLAG:~0,1%" == "-" set "FLAG="
 
+if defined FLAG if "%FLAG%" == "-chcp" set "FLAG_CHCP=%~2" & shift & shift & call set "FLAG=%%~1"
+
 if defined FLAG (
-  if "%FLAG%" == "-chcp" (
-    set "FLAG_CHCP=%~2"
-    shift
-  ) else (
-    echo;%?~%: error: invalid flag: %FLAG%
-  ) >&2
-
-  shift
-
-  rem read until no flags
-  goto FLAGS_LOOP
-)
+  echo;%?~%: error: invalid flag: %FLAG%
+) >&2
 
 if defined FLAG_CHCP (
-  call "%%CONTOOLS_ROOT%%/std/chcp.bat" %%FLAG_CHCP%%
+  call "%%?~dp0%%chcp.bat" %%FLAG_CHCP%%
   if exist "%SystemRoot%\System32\timeout.exe" ( "%SystemRoot%\System32\timeout.exe" /T -1 ) else pause
-  call "%%CONTOOLS_ROOT%%/std/restorecp.bat"
+  call "%%?~dp0%%restorecp.bat"
   exit /b %LAST_ERROR%
 )
 
