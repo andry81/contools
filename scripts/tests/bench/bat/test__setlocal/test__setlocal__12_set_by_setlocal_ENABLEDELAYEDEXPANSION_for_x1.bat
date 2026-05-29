afbf87ed8f7b981033d14edf5fa22e17c8aca137
andry81 <andry@inbox.ru>
2026-05-29 08:19:42 +0300

    2026.05.29: * new: scripts/tests/bench/bat/test__setlocal: more bench tests

---
+++ scripts/tests/bench/bat/test__setlocal/test__setlocal__12_set_by_setlocal_ENABLEDELAYEDEXPANSION_for_x1.bat
@@ -0,0 +1,32 @@
+@echo off
+
+setlocal
+
+call "%%~dp0__init__/__init__.bat" || exit /b
+
+echo;^>%~nx0
+
+set VAR=1234567890
+
+call "%%CONTOOLS_ROOT%%/time/begin_time.bat"
+
+for /L %%i in (1,1,1000) do ^
+setlocal ENABLEDELAYEDEXPANSION & ^
+set "VAR=!VAR!" & ^
+set "VAR=!VAR!" & ^
+set "VAR=!VAR!" & ^
+set "VAR=!VAR!" & ^
+set "VAR=!VAR!" & ^
+set "VAR=!VAR!" & ^
+set "VAR=!VAR!" & ^
+set "VAR=!VAR!" & ^
+set "VAR=!VAR!" & ^
+set "VAR=!VAR!" & ^
+for /F "usebackq tokens=* delims="eol^= %%i in ('"!VAR!"') do endlocal & set "VAR=%%~i"
+
+call "%%CONTOOLS_ROOT%%/time/end_time.bat" 1
+
+echo Time spent: %TIME_INTS%.%TIME_FRACS% msecs
+echo;
+
+exit /b 0
