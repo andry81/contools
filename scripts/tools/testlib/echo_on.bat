1af2956c29b0bafd54f335e0d2ef82800dbe5c04
andry81 <andry@inbox.ru>
2026-06-21 19:04:28 +0300

    2026.06.21: * refactor: scripts/tools/testlib/echo_*.bat: minor refactor

--- scripts/tools/testlib/echo_on.bat
+++ scripts/tools/testlib/echo_on.bat
@@ -6,3 +6,7 @@ rem   Calls `echo on` with empty prompt and a command call at the end.
 rem   Previous prompt is saved in `TESTLIB__PREV_PROMPT` variable.
 rem   To reset the prompt use either `setlocal` before the call and `endlocal`
 rem   after, or `echo_off.bat` script.
+rem
+rem   Enables to echo a test commands out to the console as is until the
+rem   `echo_off.bat` script (with the `@` prefix before the `call` if required)
+rem   is called.
