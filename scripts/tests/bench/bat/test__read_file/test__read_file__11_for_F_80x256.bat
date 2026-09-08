68f6c728674104eda0879fc7882cd7f2f80b0558
andry81 <andry@inbox.ru>
2026-09-08 13:30:01 +0300

    2026.09.03: * new: scripts/tests/bench/bat/test__read_file.bat: read a file bench tests

---
+++ scripts/tests/bench/bat/test__read_file/test__read_file__11_for_F_80x256.bat
@@ -0,0 +1,26 @@
+@echo off
+
+setlocal
+
+call "%%~dp0__init__/__init__.bat" || exit /b
+
+echo;^>%~nx0
+
+setlocal DISABLEDELAYEDEXPANSION
+
+call "%%CONTOOLS_ROOT%%/std/canonical_path.bat" TEST_READ_DIR  "%%TEST_DATA_IN_ROOT%%/test_read_file"
+
+call "%%CONTOOLS_ROOT%%/time/begin_time.bat"
+
+cd "%TEST_READ_DIR%"
+
+for /L %%i in (1,1,100) do (
+  for /F "usebackq tokens=* delims="eol^= %%j in ("test_file_80x256.txt ") do break
+)
+
+call "%%CONTOOLS_ROOT%%/time/end_time.bat" 1
+
+echo Time spent: %TIME_INTS%.%TIME_FRACS% msecs
+echo;
+
+exit /b 0
